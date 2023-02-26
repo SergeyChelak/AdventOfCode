609e5436dc30f47b5d0a9fd336edad4ee9781692
Sergey Chelak <sergey.chelak@gmail.com>
2023-02-26 16:46:00 +0200

    loading and parsing data for aoc2015/day 06

--- src/aoc2015/day_06.rs
+++ src/aoc2015/day_06.rs
@@ -3,15 +3,76 @@ use crate::file_utils::*;
 
 use std::io;
 
+enum Command {
+    TurnOn,
+    TurnOff,
+    Toggle
+}
+
+impl Command {
+    fn parse(s: &str) -> Self {
+        if s.starts_with("turn on") {
+            Self::TurnOn
+        } else if s.starts_with("turn off") {
+            Self::TurnOff
+        } else if s.starts_with("toggle") {
+            Self::Toggle
+        } else {
+            panic!("Unexpected command {s}")
+        }
+    }
+}
+
+struct Coordinate(usize, usize);
+
+impl Coordinate {
+    fn parse(s: &str) -> Self {
+        let items: Vec<&str> = s.split(",").collect();
+        let x = items[0].parse::<usize>()
+                .expect("Wrong x value, expected usize");
+        let y = items[1].parse::<usize>()
+                .expect("Wrong y value, expected usize");
+        Self(x, y)
+    }
+}
+
+struct Instruction {
+    command: Command,
+    from: Coordinate,
+    to: Coordinate,
+}
+
+impl Instruction {
+    fn from_str(s: &str) -> Self {
+        let tokens: Vec<&str> = s.split(" ").collect();
+        let from_str = tokens[tokens.len() - 3];
+        let to_str = tokens[tokens.len() - 1];
+        Self {
+            command: Command::parse(s),
+            from: Coordinate::parse(from_str),
+            to: Coordinate::parse(to_str)
+        }
+    }
+}
+
 pub struct AoC2015_06 {
-    // place required fields here
+    input: Vec<Instruction>,
 }
 
 impl AoC2015_06 {
     pub fn new() -> io::Result<Self> {
         Ok(Self {
-            // initialize solution
+            input: Self::load_input()?
         })
+    }
+
+    fn load_input() -> io::Result<Vec<Instruction>> {
+        Ok(
+            read_file_as_lines("input/aoc2015_06")?
+                .iter()
+                .map(|line| Instruction::from_str(line))
+                .collect()
+        )
     }
 
 }
@@ -32,7 +93,8 @@ mod test {
     use super::*;
 
     #[test]
-    fn aoc015_06_input_load_test() -> io::Result<()> {
+    fn aoc2015_06_input_load_test() -> io::Result<()> {
+        assert!(AoC2015_06::new()?.input.len() > 0);
         Ok(())
     }
 
