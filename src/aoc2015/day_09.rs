d0a5823cda00a5c7d4adcf5c5579d99421c0af58
Sergey Chelak <sergey.chelak@gmail.com>
2023-02-28 21:33:05 +0200

    aoc2015/day 09 load & parse input

--- src/aoc2015/day_09.rs
+++ src/aoc2015/day_09.rs
@@ -1,17 +1,63 @@
 use crate::solution::Solution;
 use crate::file_utils::*;
 
+use std::collections::HashMap;
 use std::io;
 
+struct Distance(String, String, usize);
+type CityId = HashMap<String, usize>;
+type Graph = HashMap<(usize, usize), usize>;
+
 pub struct AoC2015_09 {
-    // place required fields here
+    cities: CityId,
+    graph: Graph,
 }
 
 impl AoC2015_09 {
     pub fn new() -> io::Result<Self> {
+        let lines = read_file_as_lines("input/aoc2015_09")?;
+        let input = Self::parse_input(&lines);
         Ok(Self {
-            // initialize solution
+            cities: input.0,
+            graph: input.1,
         })
+    }
+
+    fn parse_input(lines: &Vec<String>) -> (CityId, Graph) {
+        let distances = lines.iter()
+            .map(|s| Self::parse_line(&s))
+            .collect::<Vec<Distance>>();
+        let mut cities = CityId::new();
+        let mut graph = Graph::new();
+        for dist in distances {
+            let from = Self::get_city_id(&dist.0, &mut cities);
+            let to = Self::get_city_id(&dist.1, &mut cities);
+            graph.insert((from, to), dist.2);
+            graph.insert((to, from), dist.2);
+        }
+        (cities, graph)
+    }
+
+    fn parse_line(s: &str) -> Distance {
+        let equation = s.split(" = ").collect::<Vec<&str>>();
+        assert_eq!(equation.len(), 2, "Incorrect input string");
+        let weight = equation.last()
+            .unwrap()
+            .parse::<usize>()
+            .expect("Integer value expected after =");
+        let cities = equation[0].split(" to ").collect::<Vec<&str>>();
+        assert_eq!(cities.len(), 2, "Incorrect input string");
+        Distance(cities[0].to_string(), cities[1].to_string(), weight)
+    }
+
+    fn get_city_id(city: &str, map: &mut CityId) -> usize {
+        let next_id = map.len();
+        if let Some(id) = map.get(city) {
+            *id
+        } else {
+            map.insert(city.to_string(), next_id);
+            next_id
+        }
     }
 }
 
@@ -33,6 +79,9 @@ mod test {
 
     #[test]
     fn aoc2015_09_input_load_test() -> io::Result<()> {
+        let sol = AoC2015_09::new()?;
+        assert!(sol.cities.len() > 0, "Cities mapping not loaded");
+        assert!(sol.graph.len() > 0, "Graph is empty");
         Ok(())
     }
 
