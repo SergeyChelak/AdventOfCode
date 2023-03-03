d128a37e8463c91a2534c8c128e8ee9665412831
Sergey Chelak <sergey.chelak@gmail.com>
2023-03-03 21:49:15 +0200

    solved 2015/12 part #2

--- src/aoc2015/day_12.rs
+++ src/aoc2015/day_12.rs
@@ -7,17 +7,25 @@ use serde_json::*;
 
 type Json = Map<String, Value>;
 
-fn sum_in_object(json: &Json) -> i64 {
-    json.iter()
-        .fold(0, |acc, (_, value)| acc + sum_in_value(value))
+fn sum_in_object(json: &Json, skip: &str) -> i64 {
+    let skip_count = json.iter()
+        .map(|(_, v)| v)
+        .filter(|v| *v == skip)
+        .count();
+    if skip_count == 0 {
+        json.iter()
+            .fold(0, |acc, (_, value)| acc + sum_in_value(value, skip))
+    } else {
+        0
+    }
 }
 
 
-fn sum_in_value(value: &Value) -> i64 {
+fn sum_in_value(value: &Value, skip: &str) -> i64 {
     match value {
         Value::Number(number) => number.as_i64().unwrap_or(0),
-        Value::Object(map) => sum_in_object(map),
-        Value::Array(items) => items.iter().fold(0, |acc, v| acc + sum_in_value(v)),
+        Value::Object(map) => sum_in_object(map, skip),
+        Value::Array(items) => items.iter().fold(0, |acc, v| acc + sum_in_value(v, skip)),
         _ => 0
     }
 }
@@ -37,12 +45,14 @@ impl AoC2015_12 {
 
 impl Solution for AoC2015_12 {
     fn part_one(&self) -> String {
-        sum_in_object(&self.json)
+        sum_in_object(&self.json, "")
             .to_string()
     }
 
-    // fn part_two(&self) -> String {
-    // }
+    fn part_two(&self) -> String {
+        sum_in_object(&self.json, "red")
+            .to_string()
+    }
 
     fn description(&self) -> String {
         "AoC 2015/Day 12: JSAbacusFramework.io".to_string()
@@ -64,7 +74,7 @@ mod test {
     fn aoc2015_12_correctness() -> io::Result<()> {
         let sol = AoC2015_12::new()?;
         assert_eq!(sol.part_one(), "111754");
-        assert_eq!(sol.part_two(), "");
+        assert_eq!(sol.part_two(), "65402");
         Ok(())
     }
 }
