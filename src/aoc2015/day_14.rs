f23fa668171d513358674058af7dee6ebbdd9b45
Sergey Chelak <sergey.chelak@gmail.com>
2023-03-04 19:15:23 +0200

    solved 2015/14 part #1

--- src/aoc2015/day_14.rs
+++ src/aoc2015/day_14.rs
@@ -6,10 +6,20 @@ use std::str::FromStr;
 use std::num::ParseIntError;
 
 struct Reindeer {
-    name: String,
+    //name: String,
     speed: i32,
     fly_time: i32,
     rest_time: i32
+}
+
+impl Reindeer {
+    fn distance(&self, time: i32) -> i32 {
+        let interval = self.fly_time + self.rest_time;
+        let count = time / interval;
+        let mut dist = self.speed * count * self.fly_time;
+        dist += (time % interval).min(self.fly_time) * self.speed;
+        dist
+    }
 }
 
 impl FromStr for Reindeer {
@@ -17,13 +27,13 @@ impl FromStr for Reindeer {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let items = s.split(" ").collect::<Vec<&str>>();
-        let name = items[0].to_string();
+        // let name = items[0].to_string();
         let speed = items[3].parse::<i32>()?;
         let fly_time = items[6].parse::<i32>()?;
         let rest_time = items[items.len() - 2].parse::<i32>()?;
         Ok(
             Self {
-                name,
+                //name,
                 speed,
                 fly_time,
                 rest_time,
@@ -53,11 +63,17 @@ impl AoC2015_14 {
 
 impl Solution for AoC2015_14 {
     fn part_one(&self) -> String {
-        todo!()
+        let time = 2503;
+        self.reindeers.iter()
+            .map(|r| r.distance(time))
+            .max()
+            .expect("Not found")
+            .to_string()
     }
 
-    // fn part_two(&self) -> String {
-    // }
+    fn part_two(&self) -> String {
+        todo!()
+    }
 
     fn description(&self) -> String {
         "AoC 2015/Day 14: Reindeer Olympics".to_string()
@@ -78,8 +94,49 @@ mod test {
     #[test]
     fn aoc2015_14_correctness() -> io::Result<()> {
         let sol = AoC2015_14::new()?;
-        assert_eq!(sol.part_one(), "");
+        assert_eq!(sol.part_one(), "2655");
         assert_eq!(sol.part_two(), "");
         Ok(())
+    }
+
+    #[test]
+    fn aoc2015_14_distance_case1() {
+        let reindeer = Reindeer {
+            speed: 8,
+            fly_time: 8,
+            rest_time: 53
+        };
+        assert_eq!(reindeer.distance(1), 8);
+        assert_eq!(reindeer.distance(2), 16);
+        assert_eq!(reindeer.distance(8), 64);
+        assert_eq!(reindeer.distance(10), 64);
+        assert_eq!(reindeer.distance(53), 64);
+        assert_eq!(reindeer.distance(62), 72);
+    }
+
+    #[test]
+    fn aoc2015_14_distance_case2() {
+        let reindeer = Reindeer {
+            speed: 14,
+            fly_time: 10,
+            rest_time: 127
+        };
+        assert_eq!(reindeer.distance(1), 14);
+        assert_eq!(reindeer.distance(11), 140);
+        assert_eq!(reindeer.distance(12), 140);
+        assert_eq!(reindeer.distance(1000), 1120);
+    }
+
+    #[test]
+    fn aoc2015_14_distance_case3() {
+        let reindeer = Reindeer {
+            speed: 16,
+            fly_time: 11,
+            rest_time: 162
+        };
+        assert_eq!(reindeer.distance(1), 16);
+        assert_eq!(reindeer.distance(10), 160);
+        assert_eq!(reindeer.distance(12), 176);
+        assert_eq!(reindeer.distance(1000), 1056);
     }
 }
