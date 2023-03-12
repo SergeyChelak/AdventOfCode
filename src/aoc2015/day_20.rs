6e8670147e884ce6d40ba50a8eb3c4d94837d82b
Sergey Chelak <sergey.chelak@gmail.com>
2023-03-12 16:11:34 +0200

    solved 2015/20 part #2

--- src/aoc2015/day_20.rs
+++ src/aoc2015/day_20.rs
@@ -24,12 +24,41 @@ fn gifts(n: usize) -> usize {
     count
 }
 
+fn gifts_limit(n: usize) -> usize {
+    let mut count = 10usize;
+    for i in 2..=n {
+        if n % i == 0 && n / i < 51 {
+            count += i * 11;
+        }
+    }
+    count
+}
+
 impl Solution for AoC2015_20 {
-    fn part_one(&self) -> String {
+    // fn part_one(&self) -> String {
+    //     let mut num = 1usize;
+    //     loop {
+    //         let amount = gifts(num);
+    //         println!("# {num} gifts: {amount}");
+    //         if amount > self.input {
+    //             break;
+    //         } else {
+    //             let k = self.input / amount;
+    //             if k > 10 {
+    //                 num *= 2;
+    //             } else {
+    //                 num += 1;
+    //             }
+    //         }
+    //     }
+    //     num.to_string()
+    // }
+
+    fn part_two(&self) -> String {
         let mut num = 1usize;
         loop {
-            let amount = gifts(num);
-            println!("# {num} gifts: {amount}");
+            let amount = gifts_limit(num);
+            // println!("# {num} gifts: {amount}");
             if amount > self.input {
                 break;
             } else {
@@ -44,9 +73,6 @@ impl Solution for AoC2015_20 {
         num.to_string()
     }
 
-    // fn part_two(&self) -> String {
-    // }
-
     fn description(&self) -> String {
         "AoC 2015/Day 20: Infinite Elves and Infinite Houses".to_string()
     }
@@ -60,7 +86,7 @@ mod test {
     fn aoc2015_20_correctness() -> io::Result<()> {
         let sol = AoC2015_20::new()?;
         assert_eq!(sol.part_one(), "786240");
-        assert_eq!(sol.part_two(), "");
+        assert_eq!(sol.part_two(), "831600");
         Ok(())
     }
 }
