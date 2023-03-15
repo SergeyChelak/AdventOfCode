9cfd89ee4b283adb633d4f0cd897392936f53869
Sergey Chelak <sergey.chelak@gmail.com>
2023-03-16 00:26:03 +0200

    added battle logic

--- src/aoc2015/day_22.rs
+++ src/aoc2015/day_22.rs
@@ -2,31 +2,163 @@ use crate::solution::Solution;
 
 use std::io;
 
-struct Player {
-    hit_points: i32,
-    damage: i32,
-    armor: i32,
-    mana: i32,
+#[derive(Copy, Clone)]
+enum Spell {
+    MagicMissile,
+    Drain,
+    Shield,
+    Poison,
+    Recharge
 }
 
-impl Player {
-    fn boss() -> Self {
-        // Hit Points: 58
-        // Damage: 9
-        todo!()
-    }
-
-    fn player() -> Self {
-        Self {
-            hit_points: 50,
-            damage: 0,
-            armor: 0,
-            mana: 500,
+impl Spell {
+    fn cost(&self) -> i32 {
+        match self {
+            Self::MagicMissile => 53,
+            Self::Drain => 73,
+            Self::Shield => 113,
+            Self::Poison => 173,
+            Self::Recharge => 229,
         }
     }
 
-    fn make_turn(&self, other: &mut Self) {
-        //
+    fn initial_duration(&self) -> i32 {
+        match self {
+            Self::MagicMissile => 1,
+            Self::Drain => 1,
+            Self::Shield => 6,
+            Self::Poison => 6,
+            Self::Recharge => 5,
+        }
+    }
+
+    fn is_always_active(&self) -> bool {
+        match self {
+            Self::Poison | Self::Recharge => true,
+            _ => false
+        }
+    }
+}
+
+struct Effect {
+    duration: i32,
+    spell: Spell
+}
+
+impl Effect {
+    fn with_spell(spell: Spell) -> Self {
+        let duration = spell.initial_duration();
+        Self {
+            spell,
+            duration,
+        }
+    }
+
+    fn is_active(&self) -> bool {
+        self.duration > 0
+    }
+
+    fn cast(&mut self, wizard: &mut Wizard, boss: &mut Boss) {
+        match self.spell {
+            Spell::Poison => boss.hit_points -= 3,
+            Spell::Recharge => wizard.mana += 101,
+            _ => ()
+        }
+    }
+
+    fn activate(&self, wizard: &mut Wizard, boss: &mut Boss) {
+        match self.spell {
+            Spell::MagicMissile => boss.hit_points -= 4,
+            Spell::Drain => {
+                boss.hit_points -= 2;
+                wizard.hit_points += 2;
+            },
+            Spell::Shield => wizard.armor += 7,
+            _ => ()
+        }
+    }
+
+    fn decrease(&mut self, wizard: &mut Wizard) {
+        self.duration -= 1;
+        if self.is_active() {
+            return;
+        }
+        match self.spell {
+            Spell::Shield => wizard.armor -= 7,
+            _ => ()
+        }
+    }
+}
+
+struct Boss {
+    hit_points: i32,
+    damage: i32
+}
+
+impl Boss {
+    fn new() -> Self {
+        Self {
+            hit_points: 58,
+            damage: 9,
+        }
+    }
+
+    fn is_alive(&self) -> bool {
+        self.hit_points > 0    
+    }
+}
+
+struct Wizard {
+    hit_points: i32,
+    mana: i32,
+    armor: i32,
+}
+
+impl Wizard {
+    fn new() -> Self {
+        Self {
+            hit_points: 50,
+            mana: 500,
+            armor: 0,
+        }
+    }
+
+    fn is_alive(&self) -> bool {
+        self.hit_points > 0 && self.mana > 0   
+    }
+}
+
+fn perform_battle(spells: &Vec<Spell>) {
+    let mut wizard = Wizard::new();
+    let mut boss = Boss::new();
+    let mut timer = 0;
+    let mut ptr = 0;
+    let mut active_effects: Vec<Effect> = Vec::new();
+    while wizard.is_alive() || boss.is_alive() {
+        if timer % 2 == 0 && ptr < spells.len () {
+            let spell = spells[ptr];
+            ptr += 1;
+            if spell.cost() <= wizard.mana {
+                wizard.mana -= spell.cost();
+                let effect = Effect::with_spell(spell);
+                effect.activate(&mut wizard, &mut boss);            
+                active_effects.push(effect);
+            }
+        }
+        active_effects.iter_mut()
+            .for_each(|effect| effect.cast(&mut wizard, &mut boss));        
+        if timer % 2 != 0 {
+            let damage = (boss.damage - wizard.armor).min(1);
+            wizard.hit_points -= damage;
+        }
+        timer += 1;
+        active_effects.iter_mut()
+            .for_each(|e| e.decrease(&mut wizard));
+
+        active_effects = active_effects.into_iter()
+            .filter(|ef| ef.is_active())
+            .collect();
+
     }
 }
 
