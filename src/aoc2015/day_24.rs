7bde10cfb503c4a580a69bc51f9a4bf12b4706af
Sergey Chelak <sergey.chelak@gmail.com>
2023-03-25 22:13:28 +0200

    solved 2015 day 24

--- src/aoc2015/day_24.rs
+++ src/aoc2015/day_24.rs
@@ -17,61 +17,59 @@ impl AoC2015_24 {
             input
         })
     }
-}
 
-fn layout_boxes(weight: &Vec<usize>, target: usize) -> Option<(usize, usize)> {
-    let mut tab: Vec<Option<(usize, usize)>> = vec![None; target + 1];
-    for &w in weight {
-        if w <= target {
-            tab[w] = Some((1, w));
-        }
-    }
-    for i in 1..=target {
-        if let Some((count, quantum_entanglement)) = tab[i] {
-            for &w in weight {
-                let idx = i + w;
-                if idx > target {
-                    continue;
-                }
-                let next_cnt = 1 + count;
-                let next_qe = quantum_entanglement * w;
-                tab[idx] = if let Some((c, q)) = tab[idx] {
-                    if c > next_cnt {
-                        Some((next_cnt, next_qe))
-                    } else if c == next_cnt {
-                        Some((c, next_qe.min(q)))
-                    } else {
-                        tab[idx]
-                    }
-                } else {
-                    Some((next_cnt, next_qe))
-                }
+    fn search(&self, value: usize, position: usize, target: usize, items: &mut Vec<usize>, output: &mut (usize, usize)) {
+        if value < target {
+            for i in position..self.input.len() {
+                let val = self.input[i];
+                items.push(val);
+                self.search(value + val, i + 1, target, items, output);
+                items.pop();
+            }
+        } else if value == target {
+            let cur_cnt = items.len();
+            let (cnt, qe) = *output;
+            let cur_qe = items.iter().fold(1usize, |acc, v| acc.saturating_mul(*v));
+            if cur_cnt < cnt {
+                *output = (cur_cnt, cur_qe);
+            } else if cur_cnt == cnt {
+                *output = (cur_cnt, cur_qe.min(qe));
             }
         }
     }
-    tab[target]
-}
 
-impl Solution for AoC2015_24 {
-    fn part_one(&self) -> String {
-        let sum: usize = self.input.iter().sum();
-        if sum % 3 == 0 {
-            let target = sum / 3;
-            if let Some((_, qe)) = layout_boxes(&self.input, target) {
-                qe.to_string()
-            } else {
-                "Can't align boxes to get target weight"
-                .to_string()    
-            }
-            
+    fn accommodate_boxes(&self, trunks: usize) -> Option<usize> {
+        let sum: usize = self.input.iter().sum();        
+        if sum % trunks == 0 {
+            let target = sum / trunks;
+            let mut output = (usize::MAX, usize::MAX);
+            self.search(0, 0, target, &mut Vec::new(), &mut output);
+            Some(output.1)
+        } else {
+            None
+        }
+    }
+
+    fn format_output(&self, value: Option<usize>) -> String {
+        if let Some(value) = value {
+            value.to_string()
         } else {
             "Can't divide input weights with equal parts"
                 .to_string()
         }
     }
+}
 
-    // fn part_two(&self) -> String {
-    // }
+impl Solution for AoC2015_24 {
+    fn part_one(&self) -> String {                
+        let result = self.accommodate_boxes(3);
+        self.format_output(result)
+    }
+
+    fn part_two(&self) -> String {
+        let result = self.accommodate_boxes(4);
+        self.format_output(result)
+    }
 
     fn description(&self) -> String {
         "AoC 2015/Day 24: It Hangs in the Balance".to_string()
@@ -92,19 +90,8 @@ mod test {
     #[test]
     fn aoc2015_24_correctness() -> io::Result<()> {
         let sol = AoC2015_24::new()?;
-        assert_eq!(sol.part_one(), "");
-        assert_eq!(sol.part_two(), "");
+        assert_eq!(sol.part_one(), "10723906903");
+        assert_eq!(sol.part_two(), "74850409");
         Ok(())
-    }
-
-    #[test]
-    fn aoc2015_24_layout_boxes() {
-        let weights = vec![1, 2, 3, 4, 5, 7, 8, 9, 10, 11];
-        let sum: usize = weights.iter().sum();
-        assert_eq!(sum % 3, 0);
-        let target = sum / 3;
-        let (count, qe) = layout_boxes(&weights, target).unwrap();
-        assert_eq!(count, 2);
-        assert_eq!(qe, 99);
     }
 }
