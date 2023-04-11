5f8b91cc6a7b135bfc861d099b28c663a936bcdd
Sergey Chelak <sergey.chelak@gmail.com>
2023-04-11 23:30:31 +0300

    solution style aligned

--- src/aoc2016/day_02.rs
+++ src/aoc2016/day_02.rs
@@ -3,22 +3,6 @@ use crate::utils::*;
 
 use std::io;
 
-fn get_matrix_number(from: i32, s: &str) -> i32 {
-    let from = from - 1;
-    let pos = (from / 3, from % 3);
-    let (r, c) = s.chars()
-        .fold(pos, |(r, c), ch| {
-            match ch {
-                'L' => (r, 0.max(c - 1)),
-                'R' => (r, 2.min(c + 1)),
-                'U' => (0.max(r - 1), c),
-                'D' => (2.min(r + 1), c),
-                _ => panic!("Unexpected char {ch}")
-            }
-        });
-    r * 3 + c + 1
-}
-
 pub struct AoC2016_02 {
     lines: Vec<String>
 }
@@ -34,11 +18,21 @@ impl AoC2016_02 {
 
 impl Solution for AoC2016_02 {
     fn part_one(&self) -> String {
-        let mut prev = 5;
+        let mut pos = (1, 1);
         let mut output = String::new();
         for line in &self.lines {
-            let val = get_matrix_number(prev, line);
-            prev = val;
+            let (r, c) = line.chars()
+                .fold(pos, |(r, c), ch| {
+                match ch {
+                    'L' => (r, 0.max(c - 1)),
+                    'R' => (r, 2.min(c + 1)),
+                    'U' => (0.max(r - 1), c),
+                    'D' => (2.min(r + 1), c),
+                    _ => panic!("Unexpected char {ch}")
+                }
+            });            
+            pos = (r,c);
+            let val = r * 3 + c + 1;
             output.push_str(&val.to_string());
         }
         output
@@ -103,22 +97,24 @@ mod test {
 
     #[test]
     fn aoc2016_02_demo_case1() {
-        assert_eq!(get_matrix_number(5, "ULL"), 1);
-        assert_eq!(get_matrix_number(1, "RRDDD"), 9);
-        assert_eq!(get_matrix_number(9, "LURDL"), 8);
-        assert_eq!(get_matrix_number(8, "UUUUD"), 5);
+        let sol = example_input();
+        assert_eq!(sol.part_one(), "1985");
     }
 
     #[test]
     fn aoc2016_02_demo_case2() {
-        let sol = AoC2016_02 {
+        let sol = example_input();
+        assert_eq!(sol.part_two(), "5DB3");
+    }
+
+    fn example_input() -> AoC2016_02 {
+        AoC2016_02 {
             lines: vec! [
                 String::from("ULL"),
                 String::from("RRDDD"),
                 String::from("LURDL"),
                 String::from("UUUUD"),
             ]
-        };
-        assert_eq!(sol.part_two(), "5DB3");
+        }
     }
 }
