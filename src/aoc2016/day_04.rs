0feed923b70d8550895362c1c96fd52070e23a5e
Sergey Chelak <sergey.chelak@gmail.com>
2023-04-12 21:55:52 +0300

    solved 2016/04 part 2

--- src/aoc2016/day_04.rs
+++ src/aoc2016/day_04.rs
@@ -17,7 +17,8 @@ impl RoomCode {
         let name = components[..last_idx]
                 .iter()
                 .map(|s| s.to_string())
-                .collect::<String>();
+                .collect::<Vec<String>>()
+                .join("-");
         let checksum_compound = components[last_idx];
         let split_index = checksum_compound.find('[').expect("Input string should contain [ .. ]");
         let (sector_id, checksum) = checksum_compound.split_at(split_index);
@@ -32,6 +33,7 @@ impl RoomCode {
         let mut freq = [0i32; 26];
         let offset = 'a' as u8 as usize;
         for ch in self.encrypted_name.chars() {
+            if ch == '-' { continue; }
             let index = ch as u8 as usize - offset;
             freq[index] += 1;
         }
@@ -63,6 +65,25 @@ impl RoomCode {
     fn is_valid_checksum(&self) -> bool {
         self.stored_checksum == self.calc_checksum()
     }
+
+    fn decrypt_name(&self) -> String {
+        let offset = 'a' as u8;
+        let mut value = self.encrypted_name.clone();
+        for _ in 0..self.sector_id {
+            value = value
+                .chars()
+                .map(|ch| match ch {
+                    'a'..='z' => {
+                        let code = (ch as u8 - offset + 1) % 26;
+                        (code + offset) as char
+                    },
+                    ' ' | '-' => ' ',
+                    _ => ch,
+                })
+                .collect();
+        }
+        value
+    }
 }
 
 pub struct AoC2016_04 {
@@ -91,8 +112,15 @@ impl Solution for AoC2016_04 {
             .to_string()
     }
 
-    // fn part_two(&self) -> String {
-    // }
+    fn part_two(&self) -> String {
+        self.input
+            .iter()
+            .filter(|&code| code.is_valid_checksum())
+            .filter(|&code| code.decrypt_name() == "northpole object storage")
+            .map(|code| code.sector_id.to_string())
+            .take(1)
+            .collect::<String>()
+    }
 
     fn description(&self) -> String {
         "AoC 2016/Day 4: Security Through Obscurity".to_string()
@@ -114,14 +142,14 @@ mod test {
     fn aoc2016_04_correctness() -> io::Result<()> {
         let sol = AoC2016_04::new()?;
         assert_eq!(sol.part_one(), "409147");
-        assert_eq!(sol.part_two(), "");
+        assert_eq!(sol.part_two(), "991");
         Ok(())
     }
 
     #[test]
     fn aoc2016_04_room_decode() {
         let room = RoomCode::from_str("aaaaa-bbb-z-y-x-123[abxyz]");
-        assert_eq!(room.encrypted_name, "aaaaabbbzyx");
+        assert_eq!(room.encrypted_name, "aaaaa-bbb-z-y-x");
         assert_eq!(room.sector_id, 123);
         assert_eq!(room.stored_checksum, "abxyz");
     }
@@ -136,5 +164,11 @@ mod test {
 
         let room = RoomCode::from_str("not-a-real-room-404[oarel]");
         assert_eq!(room.calc_checksum(), "oarel");
+    }
+
+    #[test]
+    fn aoc2016_04_decrypt() {
+        let room = RoomCode::from_str("qzmt-zixmtkozy-ivhz-343[abxyz]");
+        assert_eq!(room.decrypt_name(), "very encrypted name");
     }
 }
