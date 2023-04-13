d61b51ea9b0c2282ed481e6045f159c4b73580d3
Sergey Chelak <sergey.chelak@gmail.com>
2023-04-13 13:57:14 +0300

    solved 2016/05 pt 1

--- src/aoc2016/day_05.rs
+++ src/aoc2016/day_05.rs
@@ -1,22 +1,34 @@
 use crate::solution::Solution;
-
+use md5;
 use std::io;
 
 pub struct AoC2016_05 {
-    door_id: String
+    door_id: String,
 }
 
 impl AoC2016_05 {
     pub fn new() -> io::Result<Self> {
         Ok(Self {
-            door_id: String::from("ojvtpuvg")
+            door_id: String::from("ojvtpuvg"),
         })
     }
 }
 
 impl Solution for AoC2016_05 {
-    // fn part_one(&self) -> String {
-    // }
+    fn part_one(&self) -> String {
+        let mut password = String::new();
+        for i in 0..usize::MAX {
+            let input = format!("{}{i}", self.door_id);
+            let hash = format!("{:x}", md5::compute(input));
+            if hash.starts_with("00000") {
+                password.push_str(&hash[5..6]);
+                if password.len() == 8 {
+                    return password;
+                }
+            }
+        }
+        "Not found".to_string()
+    }
 
     // fn part_two(&self) -> String {
     // }
@@ -33,7 +45,7 @@ mod test {
     #[test]
     fn aoc2016_05_correctness() -> io::Result<()> {
         let sol = AoC2016_05::new()?;
-        assert_eq!(sol.part_one(), "");
+        assert_eq!(sol.part_one(), "4543c154");
         assert_eq!(sol.part_two(), "");
         Ok(())
     }
