9ab5fdfee6c96547f77dc934337d65942655f448
Sergey Chelak <sergey.chelak@gmail.com>
2023-04-13 14:31:50 +0300

    solved 2016/05 pt 2

--- src/aoc2016/day_05.rs
+++ src/aoc2016/day_05.rs
@@ -30,8 +30,30 @@ impl Solution for AoC2016_05 {
         "Not found".to_string()
     }
 
-    // fn part_two(&self) -> String {
-    // }
+    fn part_two(&self) -> String {
+        let len = 8;
+        let mut password = vec!['\0'; len];
+        for i in 0..usize::MAX {
+            let input = format!("{}{i}", self.door_id);
+            let hash = format!("{:x}", md5::compute(input));
+            if !hash.starts_with("00000") {
+                continue;
+            }
+            let hash = hash.chars().collect::<Vec<char>>();
+            if let Some(index) = hash[5].to_digit(10) {
+                let index = index as usize;
+                if index < len && password[index] == '\0' {
+                    password[index] = hash[6];
+
+                    println!("{password:?}");
+                    if !password.contains(&'\0') {
+                        return password.iter().collect();
+                    }
+                }
+            }
+        }
+        "Not found".to_string()
+    }
 
     fn description(&self) -> String {
         "AoC 2016/Day 5: How About a Nice Game of Chess?".to_string()
@@ -46,7 +68,7 @@ mod test {
     fn aoc2016_05_correctness() -> io::Result<()> {
         let sol = AoC2016_05::new()?;
         assert_eq!(sol.part_one(), "4543c154");
-        assert_eq!(sol.part_two(), "");
+        assert_eq!(sol.part_two(), "1050cbbd");
         Ok(())
     }
 }
