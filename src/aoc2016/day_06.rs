aec683335fc65331dd6329f046739ffa81952f2e
Sergey Chelak <sergey.chelak@gmail.com>
2023-04-14 22:29:00 +0300

    solved 2016/06 part 1

--- src/aoc2016/day_06.rs
+++ src/aoc2016/day_06.rs
@@ -17,8 +17,9 @@ impl AoC2016_06 {
 }
 
 impl Solution for AoC2016_06 {
-    // fn part_one(&self) -> String {
-    // }
+    fn part_one(&self) -> String {
+        correct_message(&self.lines)
+    }
 
     // fn part_two(&self) -> String {
     // }
@@ -26,6 +27,33 @@ impl Solution for AoC2016_06 {
     fn description(&self) -> String {
         "AoC 2016/Day 6: Signals and Noise".to_string()
     }
+}
+
+fn correct_message(lines: &Vec<String>) -> String {
+    let len = lines[0].len();
+    let mut freq_matrix = vec![vec![0; 26]; len];
+    for line in lines {
+        let chars = line.chars().into_iter().collect::<Vec<char>>();
+        for i in 0..chars.len() {
+            let pos = chars[i] as u8 - b'a';
+            freq_matrix[i][pos as usize] += 1;
+        }
+    }
+    freq_matrix
+        .iter()
+        .map(|freq| {
+            let mut idx = 0;
+            let mut max = 0;
+            for i in 0..freq.len() {
+                if freq[i] > max {
+                    max = freq[i];
+                    idx = i;
+                }
+            }
+            idx
+        })
+        .map(|val| (val as u8 + b'a') as char)
+        .collect::<String>()
 }
 
 #[cfg(test)]
@@ -42,8 +70,36 @@ mod test {
     #[test]
     fn aoc2016_06_correctness() -> io::Result<()> {
         let sol = AoC2016_06::new()?;
-        assert_eq!(sol.part_one(), "");
+        assert_eq!(sol.part_one(), "qrqlznrl");
         assert_eq!(sol.part_two(), "");
         Ok(())
+    }
+
+    #[test]
+    fn aoc2016_06_demo_case1() {
+        let lines = [
+            "eedadn",
+            "drvtee",
+            "eandsr",
+            "raavrd",
+            "atevrs",
+            "tsrnev",
+            "sdttsa",
+            "rasrtv",
+            "nssdts",
+            "ntnada",
+            "svetve",
+            "tesnvt",
+            "vntsnd",
+            "vrdear",
+            "dvrsen",
+            "enarar",
+        ].iter()
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+        let sol = AoC2016_06 {
+            lines
+        };
+        assert_eq!(sol.part_one(), "easter");
     }
 }
