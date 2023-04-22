8824a7b92fcaf9ce1b2dca60327ce8be9acf0ac9
Sergey Chelak <sergey.chelak@gmail.com>
2023-04-22 23:59:31 +0300

    solved 2016/14

--- src/aoc2016/day_14.rs
+++ src/aoc2016/day_14.rs
@@ -2,6 +2,119 @@ use crate::solution::Solution;
 
 use std::io;
 
+struct HashInfo {
+    symbol: char,
+    reps: usize,
+    index: usize,
+}
+
+fn hash(salt: &str, index: usize) -> String {
+    let input = format!("{}{index}", salt);
+    format!("{:x}", md5::compute(input))
+}
+
+fn stretched_hash(salt: &str, index: usize) -> String {
+    let mut input = hash(salt, index);
+    for _ in 0..2016 {
+        input = format!("{:x}", md5::compute(input));
+    }
+    input
+}
+
+type FnHasher = dyn Fn(&str, usize) -> String;
+
+struct HashGenerator {
+    salt: String,
+    hasher: &'static FnHasher,
+    _index: usize,
+}
+
+impl HashGenerator {
+    fn with_salt(salt: &str, hasher: &'static FnHasher) -> Self {
+        Self {
+            salt: salt.to_string(),
+            hasher,
+            _index: 0,
+        }
+    }
+}
+
+impl Iterator for HashGenerator {
+    type Item = HashInfo;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        for index in self._index..usize::MAX {
+            let hash = (self.hasher)(&self.salt, index);
+            let (reps, ch) = longest_char_sequence(&hash);
+            if reps > 2 {
+                self._index = index + 1;
+                return Some(HashInfo{
+                    symbol: ch,
+                    reps,
+                    index,
+                });
+            }
+        }
+        None
+    }
+}
+
+fn longest_char_sequence(s: &str) -> (usize, char) {
+    let mut count = 0;
+    let mut prev = '\0';
+    let mut res_count = 0;
+    let mut res_char = '\0';
+    for ch in s.chars() {
+        if ch == prev {
+            count += 1;
+            if res_count < count {
+                res_count = count;
+                res_char = prev;
+            }
+        } else {
+            count = 1;
+        }
+        prev = ch;
+    }
+    (res_count, res_char)
+}
+
+fn find_key_index(salt: &str, hasher: &'static FnHasher) -> Option<usize> {
+    let distance = 1001;
+    let mut generator = HashGenerator::with_salt(salt, hasher);
+    let mut hashes: Vec<HashInfo> = Vec::with_capacity(2 * distance);
+    if let Some(info) = generator.next() {
+        hashes.push(info);
+    }
+    // look for the pairs
+    let mut count = 0usize;
+    let mut number = 0usize;
+    loop {
+        let symbol = hashes[number].symbol;
+        let index = hashes[number].index;
+        // fill hashes
+        for info in generator.by_ref() {
+            let info_idx = info.index;
+            hashes.push(info);
+            if info_idx - index > distance {
+                break;
+            }
+        }
+        let current_hash = &hashes[number];
+        for info in hashes.iter().skip(number + 1) {
+            assert_ne!(info.index, current_hash.index);
+            if info.symbol == symbol && info.reps > 4 && info.index - index <= distance {
+                count += 1;
+                break;
+            }
+        }
+        if count == 64 {
+            break Some(current_hash.index);
+        }
+        number += 1;
+    }
+}
+
 pub struct AoC2016_14 {
     salt: String
 }
@@ -15,11 +128,17 @@ impl AoC2016_14 {
 }
 
 impl Solution for AoC2016_14 {
-    // fn part_one(&self) -> String {
-    // }
+    fn part_one(&self) -> String {
+        find_key_index(&self.salt, &hash)
+            .expect("Correspoding hash index not found")
+            .to_string()
+    }
 
-    // fn part_two(&self) -> String {
-    // }
+    fn part_two(&self) -> String {
+        find_key_index(&self.salt, &stretched_hash)
+            .expect("Correspoding hash index not found")
+            .to_string()
+    }
 
     fn description(&self) -> String {
         "AoC 2016/Day 14: One-Time Pad".to_string()
@@ -33,8 +152,28 @@ mod test {
     #[test]
     fn aoc2016_14_correctness() -> io::Result<()> {
         let sol = AoC2016_14::new()?;
-        assert_eq!(sol.part_one(), "");
-        assert_eq!(sol.part_two(), "");
+        assert_eq!(sol.part_one(), "15035");
+        assert_eq!(sol.part_two(), "19968");
         Ok(())
+    }
+
+    #[test]
+    fn aoc2016_14_longest_char_sequence() {
+        assert_eq!(longest_char_sequence("cc38887a5"), (3, '8'));
+        assert_eq!(longest_char_sequence("abbccc"), (3, 'c'));
+        assert_eq!(longest_char_sequence("aaaabbbccd"), (4, 'a'));
+        //assert_eq!(longest_char_sequence(""), (, ''));
+    }
+
+    // #[test]
+    // fn aoc2016_14_example() {
+    //     let index = find_key_index("abc");
+    //     assert_eq!(index.unwrap(), 22728);
+    // }
+
+    #[test]
+    fn aoc2016_14_calc_stretched_hash() {
+        let hash = stretched_hash("abc", 0);
+        assert_eq!(hash, "a107ff634856bb300138cac6568c0f24");
     }
 }
