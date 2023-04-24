fabc422247c137ccd109f8b95e8675d95d19ce70
Sergey Chelak <sergey.chelak@gmail.com>
2023-04-25 00:59:49 +0300

    solved 2016/19 part #1

--- src/aoc2016/day_19.rs
+++ src/aoc2016/day_19.rs
@@ -8,21 +8,40 @@ pub struct AoC2016_19 {
 
 impl AoC2016_19 {
     pub fn new() -> io::Result<Self> {
-        Ok(Self {
-            elves: 3012210,
-        })
+        Ok(Self { elves: 3012210 })
     }
 }
 
 impl Solution for AoC2016_19 {
-    // fn part_one(&self) -> String {
-    // }
+    fn part_one(&self) -> String {
+        play_white_elephant(self.elves).to_string()
+    }
 
     // fn part_two(&self) -> String {
     // }
 
     fn description(&self) -> String {
         "AoC 2016/Day 19: An Elephant Named Joseph".to_string()
+    }
+}
+
+fn play_white_elephant(participants: usize) -> usize {
+    let mut table = vec![1; participants];
+    let mut ptr = 0usize;
+    loop {
+        let mut next = (ptr + 1) % participants;
+        while table[next] == 0 {
+            next = (next + 1) % participants;
+        }
+        if next == ptr {
+            break next + 1;
+        }
+        table[ptr] += table[next];
+        table[next] = 0;
+        ptr = next;
+        while table[ptr] == 0 {
+            ptr = (ptr + 1) % participants;
+        }
     }
 }
 
@@ -36,5 +55,10 @@ mod test {
         assert_eq!(sol.part_one(), "");
         assert_eq!(sol.part_two(), "");
         Ok(())
+    }
+
+    #[test]
+    fn aoc2016_19_example() {
+        assert_eq!(play_white_elephant(5), 3);
     }
 }
