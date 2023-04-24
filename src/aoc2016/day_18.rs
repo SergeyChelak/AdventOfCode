ab380ed078dea52589d7c75f2c414ce803717eda
Sergey Chelak <sergey.chelak@gmail.com>
2023-04-25 00:16:02 +0300

    solved 2016/18 part #2

--- src/aoc2016/day_18.rs
+++ src/aoc2016/day_18.rs
@@ -17,42 +17,39 @@ impl AoC2016_18 {
         let row = s.chars().map(|ch| ch == '.').collect::<Vec<bool>>();
         Self { row }
     }
+
+    fn safe_cells_count(&self, rows: usize) -> usize {
+        let mut row = self.row.clone();
+        let mut count = 0;
+        for _ in 0..rows {
+            count += row.iter().filter(|&item| *item).count();
+            row = next_row(&row);
+        }
+        count
+    }
 }
 
 impl Solution for AoC2016_18 {
     fn part_one(&self) -> String {
-        let mut row = self.row.clone();
-        let mut count = 0;
-        for _ in 0..40 {
-            count += safe_count(&row);
-            row = next_row(&row);
-        }
-        count.to_string()
+        self.safe_cells_count(40).to_string()
     }
 
-    // fn part_two(&self) -> String {
-    // }
+    fn part_two(&self) -> String {
+        self.safe_cells_count(400000).to_string()
+    }
 
     fn description(&self) -> String {
         "AoC 2016/Day 18: Like a Rogue".to_string()
     }
 }
 
-fn safe_count(row: &Vec<bool>) -> usize {
-    row.iter().filter(|&item| *item).count()
-}
-
-fn next_row(row: &Vec<bool>) -> Vec<bool> {
+fn next_row(row: &[bool]) -> Vec<bool> {
     let len = row.len();
     let mut next = vec![false; len];
     for i in 0..len {
         let left = if i > 0 { row[i - 1] } else { true };
-        let center = row[i];
         let right = if i < len - 1 { row[i + 1] } else { true };
-        let is_trap = !left && !center && right
-            || left && !center && !right
-            || !left && center && right
-            || left && center && !right;
+        let is_trap = !left && right || left && !right;
         next[i] = !is_trap;
     }
     next
@@ -73,7 +70,7 @@ mod test {
     fn aoc2016_18_correctness() -> io::Result<()> {
         let sol = AoC2016_18::new()?;
         assert_eq!(sol.part_one(), "2016");
-        assert_eq!(sol.part_two(), "");
+        assert_eq!(sol.part_two(), "19998750");
         Ok(())
     }
 }
