a0eaa83a43bf6b3f75a7630246110cafa53590a7
Sergey Chelak <sergey.chelak@gmail.com>
2023-04-26 01:29:14 +0300

    solved 2016/20 part #2

--- src/aoc2016/day_20.rs
+++ src/aoc2016/day_20.rs
@@ -9,41 +9,63 @@ pub struct AoC2016_20 {
 
 impl AoC2016_20 {
     pub fn new() -> io::Result<Self> {
-        let ranges = read_file_as_lines("input/aoc2016_20")?
+        let mut ranges = read_file_as_lines("input/aoc2016_20")?
             .iter()
             .map(|s| {
-                let (from, to) = s.split_once('-')
+                let (from, to) = s
+                    .split_once('-')
                     .expect("Ranges should be separated with '-' delimiter");
-                let from = from.parse::<usize>()
+                let from = from
+                    .parse::<usize>()
                     .expect("Low range value should be integer");
-                let to = to.parse::<usize>()
+                let to = to
+                    .parse::<usize>()
                     .expect("High range value should be integer");
                 (from, to)
             })
             .collect::<Vec<(usize, usize)>>();
         Ok(Self {
-            ranges
+            ranges: Self::merge_ranges(&mut ranges),
         })
+    }
+
+    fn merge_ranges(ranges: &mut [(usize, usize)]) -> Vec<(usize, usize)> {
+        ranges.sort_by(|(a, _), (b, _)| a.cmp(b));
+        let mut merged = Vec::new();
+        let (mut from, mut to) = (ranges[0].0, ranges[0].1);
+        for (a, b) in ranges.iter() {
+            if *a <= to + 1 {
+                to = to.max(*b);
+            } else {
+                merged.push((from, to));
+                from = *a;
+                to = *b;
+            }
+        }
+        merged.push((from, to));
+        merged
     }
 }
 
 impl Solution for AoC2016_20 {
     fn part_one(&self) -> String {
-        let mut i = 0;
-        'outer: while i <= 4_294_967_295 {
-            for (a, b) in &self.ranges {
-                if *a <= i && i <= *b {
-                    i += 1;
-                    continue 'outer;
-                }                
+        match self.ranges.len() {
+            0 => 0,
+            _ => {
+                let (l, h) = self.ranges[0];
+                if l == 0 {
+                    h + 1
+                } else {
+                    l - 1
+                }
             }
-            break;
         }
-        i.to_string()
+        .to_string()
     }
 
-    // fn part_two(&self) -> String {
-    // }
+    fn part_two(&self) -> String {
+        (4294967296 - self.ranges.iter().map(|(l, h)| *h - *l + 1).sum::<usize>()).to_string()
+    }
 
     fn description(&self) -> String {
         "AoC 2016/Day 20: Firewall Rules".to_string()
@@ -65,7 +87,7 @@ mod test {
     fn aoc2016_20_correctness() -> io::Result<()> {
         let sol = AoC2016_20::new()?;
         assert_eq!(sol.part_one(), "31053880");
-        assert_eq!(sol.part_two(), "");
+        assert_eq!(sol.part_two(), "117");
         Ok(())
     }
 }
