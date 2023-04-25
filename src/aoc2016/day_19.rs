59b98bfc89ada2a3edda6bd2bc35e92c58ff96fc
Sergey Chelak <sergey.chelak@gmail.com>
2023-04-25 21:04:12 +0300

    stub for 2016/19 part 2

--- src/aoc2016/day_19.rs
+++ src/aoc2016/day_19.rs
@@ -14,18 +14,19 @@ impl AoC2016_19 {
 
 impl Solution for AoC2016_19 {
     fn part_one(&self) -> String {
-        play_white_elephant(self.elves).to_string()
+        play_white_elephant_adjacent(self.elves).to_string()
     }
 
-    // fn part_two(&self) -> String {
-    // }
+    fn part_two(&self) -> String {
+        play_white_elephant_cross(self.elves).to_string()
+    }
 
     fn description(&self) -> String {
         "AoC 2016/Day 19: An Elephant Named Joseph".to_string()
     }
 }
 
-fn play_white_elephant(participants: usize) -> usize {
+fn play_white_elephant_adjacent(participants: usize) -> usize {
     let mut table = vec![1; participants];
     let mut ptr = 0usize;
     loop {
@@ -45,6 +46,14 @@ fn play_white_elephant(participants: usize) -> usize {
     }
 }
 
+fn play_white_elephant_cross(participants: usize) -> usize {
+    let mut table = vec![1; participants];
+    let mut ptr = 0usize;
+    loop {
+        todo!()
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -52,13 +61,18 @@ mod test {
     #[test]
     fn aoc2016_19_correctness() -> io::Result<()> {
         let sol = AoC2016_19::new()?;
-        assert_eq!(sol.part_one(), "");
+        assert_eq!(sol.part_one(), "1830117");
         assert_eq!(sol.part_two(), "");
         Ok(())
     }
 
     #[test]
-    fn aoc2016_19_example() {
-        assert_eq!(play_white_elephant(5), 3);
+    fn aoc2016_19_example1() {
+        assert_eq!(play_white_elephant_adjacent(5), 3);
+    }
+
+    #[test]
+    fn aoc2016_19_example2() {
+        assert_eq!(play_white_elephant_cross(5), 2);
     }
 }
