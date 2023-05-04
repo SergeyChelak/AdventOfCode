4b35252b6f9b8e5267f5dafd55834fd834061e28
Sergey Chelak <sergey.chelak@gmail.com>
2023-05-04 21:52:45 +0300

    converted 2d array to map

--- src/aoc2016/day_22.rs
+++ src/aoc2016/day_22.rs
@@ -2,19 +2,19 @@ use crate::solution::Solution;
 use crate::utils::*;
 
 use std::collections::hash_map::DefaultHasher;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 use std::hash::{Hash, Hasher};
 use std::io;
 use std::num::ParseIntError;
 
 #[derive(Default, Copy, Clone, Hash)]
-struct Node {
+struct StorageInfo {
     is_target: bool,
     size: usize,
     used: usize,
 }
 
-impl Node {
+impl StorageInfo {
     fn avail(&self) -> usize {
         self.size - self.used
     }
@@ -29,140 +29,57 @@ impl Node {
     }
 }
 
-struct InputItem {
+#[derive(Copy, Clone, Hash, Eq, PartialEq)]
+struct Position {
     x: usize,
     y: usize,
-    node: Node,
 }
 
-impl InputItem {
-    fn parse(s: &str) -> Self {
-        let tokens = s.split_whitespace().collect::<Vec<&str>>();
-        let (x, y) = {
-            let name = tokens[0].split('-').collect::<Vec<&str>>();
-            let x = name[1][1..]
-                .parse::<usize>()
-                .expect("Node x value should be integer");
-            let y = name[2][1..]
-                .parse::<usize>()
-                .expect("Node y value should be integer");
-            (x, y)
-        };
-        let value_of = |index: usize| -> Result<usize, ParseIntError> {
-            let len = tokens[index].len();
-            tokens[index][0..len - 1].parse::<usize>()
-        };
+// find zero node location
+// build node's grid
+// mark target node
+// reduce node's grid to movable/empty/target/permanent
+// search with hashing empty position + target data position
 
-        let size = value_of(1).expect("Size should be integer value");
-        let used = value_of(2).expect("Used should be integer value");
-        let usage = Node {
-            is_target: false,
-            size,
-            used,
-        };
-        Self { x, y, node: usage }
-    }
-}
+// fn make_grid(nodes: &[Node]) -> Grid {
+//     let max_x = 1 + nodes
+//         .iter()
+//         .map(|node| node.x)
+//         .max()
+//         .expect("Max X should be computable");
+//     let max_y = 1 + nodes
+//         .iter()
+//         .map(|node| node.y)
+//         .max()
+//         .expect("Max Y should be computable");
+//     let mut grid = vec![vec![StorageInfo::default(); max_x]; max_y];
+//     nodes.iter().for_each(|node| {
+//         grid[node.y][node.x] = node.node;
+//     });
+//     grid[0][max_x - 1].is_target = true;
+//     grid
+// }
 
-type Grid = Vec<Vec<Node>>;
-type GridSize = (usize, usize);
+// fn grid_hash(grid: &Grid) -> u64 {
+//     let mut hasher = DefaultHasher::new();
+//     grid.hash(&mut hasher);
+//     hasher.finish()
+// }
 
-fn make_grid(nodes: &[InputItem]) -> (Grid, GridSize) {
-    let max_x = 1 + nodes
-        .iter()
-        .map(|node| node.x)
-        .max()
-        .expect("Max X should be computable");
-    let max_y = 1 + nodes
-        .iter()
-        .map(|node| node.y)
-        .max()
-        .expect("Max Y should be computable");
-    let mut grid = vec![vec![Node::default(); max_x]; max_y];
-    nodes.iter().for_each(|node| {
-        grid[node.y][node.x] = node.node;
-    });
-    (grid, (max_y, max_x))
-}
-
-fn grid_hash(grid: &Grid) -> u64 {
-    let mut hasher = DefaultHasher::new();
-    grid.hash(&mut hasher);
-    hasher.finish()
-}
-
-fn find_fewest_steps(initial: &Grid) -> usize {
-    let mut steps = 0usize;
-    let mut visited = HashSet::new();
-    let mut grids = Vec::new();
-    {
-        if initial[0][0].is_target {
-            return 0;
-        }
-        let hash = grid_hash(&initial);
-        visited.insert(hash);
-        grids.push(initial.clone());
-    }
-    'main: while !grids.is_empty() {
-        steps += 1;
-        println!("Step = {steps}");
-        let mut next_grids = Vec::new();
-        for grid in grids {
-            for i in 0..grid.len() {
-                for j in 0..grid[i].len() {
-                    let mut positions = Vec::with_capacity(4);
-                    if i > 0 {
-                        positions.push((i - 1, j));
-                    }
-                    if i < grid.len() - 1 {
-                        positions.push((i + 1, j));
-                    }
-                    if j > 0 {
-                        positions.push((i, j - 1));
-                    }
-                    if j < grid[i].len() - 1 {
-                        positions.push((i, j + 1));
-                    }
-                    for (x, y) in positions {
-                        if grid[i][j].is_fit(&grid[x][y]) {
-                            let mut next = grid.clone();
-                            next[x][y].used += next[i][j].used;
-                            next[i][j].used = 0;
-                            next[x][y].is_target = next[i][j].is_target;
-                            next[i][j].is_target = false;
-                            if next[0][0].is_target {
-                                break 'main;
-                            }
-                            let hash = grid_hash(&next);
-                            if visited.contains(&hash) {
-                                continue;
-                            }
-                            visited.insert(hash);
-                            next_grids.push(next);
-                        }
-                    }
-                }
-            }
-        }
-        grids = next_grids;
-    }
-    steps
-}
-
-fn _print_grid(grid: &Grid) {
-    for row in grid {
-        for item in row {
-            print!("{}/{}\t", item.used, item.size);
-            if item.is_target {
-                print!("#");
-            }
-        }
-        println!();
-    }
-}
+// fn _print_grid(grid: &Grid) {
+//     for row in grid {
+//         for item in row {
+//             print!("{}/{}\t", item.used, item.size);
+//             if item.is_target {
+//                 print!("#");
+//             }
+//         }
+//         println!();
+//     }
+// }
 
 pub struct AoC2016_22 {
-    nodes: Vec<InputItem>,
+    nodes: HashMap<Position, StorageInfo>,
 }
 
 impl AoC2016_22 {
@@ -172,23 +89,53 @@ impl AoC2016_22 {
     }
 
     fn with_lines(lines: &[String]) -> Self {
-        let nodes = lines
+        let mut nodes = HashMap::new();
+        lines
             .iter()
-            .map(|s| InputItem::parse(s))
-            .collect::<Vec<InputItem>>();
+            .for_each(|s| {
+                let (pos, info) = Self::parse_node_info(s);
+                nodes.insert(pos, info);
+            });
         Self { nodes }
+    }
+
+    fn parse_node_info(s: &str) -> (Position, StorageInfo) {
+        let tokens = s.split_whitespace().collect::<Vec<&str>>();
+        let position = {
+            let name = tokens[0].split('-').collect::<Vec<&str>>();
+            let x = name[1][1..]
+                .parse::<usize>()
+                .expect("Node x value should be integer");
+            let y = name[2][1..]
+                .parse::<usize>()
+                .expect("Node y value should be integer");
+            Position { x, y }
+        };
+        let value_of = |index: usize| -> Result<usize, ParseIntError> {
+        let len = tokens[index].len();
+            tokens[index][0..len - 1].parse::<usize>()
+        };
+
+        let size = value_of(1).expect("Size should be integer value");
+        let used = value_of(2).expect("Used should be integer value");
+        let info = StorageInfo {
+            is_target: false,
+            size,
+            used,
+        };
+        (position, info)
     }
 }
 
 impl Solution for AoC2016_22 {
     fn part_one(&self) -> String {
         let mut count = 0usize;
-        for i in 0..self.nodes.len() {
-            if self.nodes[i].node.is_empty() {
+        for (i, (_, info_i)) in self.nodes.iter().enumerate() {
+            if info_i.is_empty() {
                 continue;
             }
-            for j in 0..self.nodes.len() {
-                if i != j && self.nodes[i].node.is_fit(&self.nodes[j].node) {
+            for (j, (_, info_j)) in self.nodes.iter().enumerate() {
+                if i != j && info_i.is_fit(&info_j) {
                     count += 1;
                 }
             }
@@ -197,11 +144,7 @@ impl Solution for AoC2016_22 {
     }
 
     fn part_two(&self) -> String {
-        let (mut grid, size) = make_grid(&self.nodes);
-        println!("Size = {size:?}");
-        grid[0][size.1 - 1].is_target = true;
-        // print_grid(&grid);
-        find_fewest_steps(&grid).to_string()
+        "".to_string()
     }
 
     fn description(&self) -> String {
@@ -222,11 +165,11 @@ mod test {
 
     #[test]
     fn aoc2016_22_parse() {
-        let node = InputItem::parse("/dev/grid/node-x2-y5     87T   65T    22T   74%");
-        assert_eq!(node.x, 2);
-        assert_eq!(node.y, 5);
-        assert_eq!(node.node.size, 87);
-        assert_eq!(node.node.used, 65);
+        let (pos, info) = AoC2016_22::parse_node_info("/dev/grid/node-x2-y5     87T   65T    22T   74%");
+        assert_eq!(pos.x, 2);
+        assert_eq!(pos.y, 5);
+        assert_eq!(info.size, 87);
+        assert_eq!(info.used, 65);
     }
 
     #[test]
