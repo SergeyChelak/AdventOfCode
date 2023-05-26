557ea2745b57fe12ba9a8101606441bfa579f9c7
Sergey Chelak <sergey.chelak@gmail.com>
2023-05-27 01:11:31 +0300

    solved 2017/04/pt 2

--- src/aoc2017/day_04.rs
+++ src/aoc2017/day_04.rs
@@ -25,8 +25,14 @@ impl Solution for AoC2017_04 {
             .to_string()
     }
 
-    // fn part_two(&self) -> String {
-    // }
+    fn part_two(&self) -> String {
+        self.phrases
+            .iter()
+            .filter(|s| has_no_anagrams(s))
+            .map(|_| 1)
+            .sum::<usize>()
+            .to_string()
+    }
 
     fn description(&self) -> String {
         "AoC 2017/Day 4: High-Entropy Passphrases".to_string()
@@ -45,6 +51,39 @@ fn has_duplicates(s: &str) -> bool {
     true
 }
 
+fn has_no_anagrams(s: &str) -> bool {
+    let words = s.split_whitespace().collect::<Vec<&str>>();
+    let len = words.len();
+    for i in 0..len - 1 {
+        for j in i + 1..len {
+            if is_anagram(words[i], words[j]) {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+fn is_anagram(w1: &str, w2: &str) -> bool {
+    let f1 = footprint(w1);
+    let f2 = footprint(w2);
+    for (&a, &b) in f1.iter().zip(f2.iter()) {
+        if a != b {
+            return false;
+        }
+    }
+    true
+}
+
+fn footprint(word: &str) -> [u32; 26] {
+    let mut arr = [0; 26];
+    word.chars().for_each(|ch| {
+        let index = ch as u8 - b'a';
+        arr[index as usize] += 1;
+    });
+    arr
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -60,7 +99,7 @@ mod test {
     fn aoc2017_04_correctness() -> io::Result<()> {
         let sol = AoC2017_04::new()?;
         assert_eq!(sol.part_one(), "383");
-        assert_eq!(sol.part_two(), "");
+        assert_eq!(sol.part_two(), "265");
         Ok(())
     }
 }
