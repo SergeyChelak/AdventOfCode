16d399c440ef260a529feb6f99e9f77629522833
Sergey Chelak <sergey.chelak@gmail.com>
2023-05-26 20:00:07 +0300

    solved 2017/02/pt 2

--- src/aoc2017/day_02.rs
+++ src/aoc2017/day_02.rs
@@ -6,12 +6,29 @@ use std::io;
 type Value = u32;
 type Grid = Vec<Vec<Value>>;
 
-fn checksum(grid: &Grid) -> Value {
+fn checksum_min_max(grid: &Grid) -> Value {
     grid.iter()
         .map(|row| {
             let max = row.iter().max().expect("Row shouldn't be empty");
             let min = row.iter().min().expect("Row shouldn't be empty");
             max - min
+        })
+        .sum()
+}
+
+fn checksum_mod(grid: &Grid) -> Value {
+    grid.iter()
+        .map(|row| {
+            let len = row.len();
+            for i in 0..len - 1 {
+                for j in i + 1..len {
+                    let (a, b) = (row[i].min(row[j]), row[i].max(row[j]));
+                    if b % a == 0 {
+                        return b / a;
+                    }
+                }
+            }
+            0
         })
         .sum()
 }
@@ -41,11 +58,12 @@ impl AoC2017_02 {
 
 impl Solution for AoC2017_02 {
     fn part_one(&self) -> String {
-        checksum(&self.grid).to_string()
+        checksum_min_max(&self.grid).to_string()
     }
 
-    // fn part_two(&self) -> String {
-    // }
+    fn part_two(&self) -> String {
+        checksum_mod(&self.grid).to_string()
+    }
 
     fn description(&self) -> String {
         "AoC 2017/Day 2: Corruption Checksum".to_string()
@@ -66,14 +84,14 @@ mod test {
     #[test]
     fn aoc2017_02_checksum() {
         let grid = vec![vec![5, 1, 9, 5], vec![7, 5, 3], vec![2, 4, 6, 8]];
-        assert_eq!(checksum(&grid), 18)
+        assert_eq!(checksum_min_max(&grid), 18)
     }
 
     #[test]
     fn aoc2017_02_correctness() -> io::Result<()> {
         let sol = AoC2017_02::new()?;
         assert_eq!(sol.part_one(), "44216");
-        assert_eq!(sol.part_two(), "");
+        assert_eq!(sol.part_two(), "320");
         Ok(())
     }
 }
