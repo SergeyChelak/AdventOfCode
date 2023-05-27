51951a4c1a58f57e60b36ba81fda6ada5e661f7a
Sergey Chelak <sergey.chelak@gmail.com>
2023-05-28 00:53:39 +0300

    added stub for 2017/07

---
+++ src/aoc2017/day_07.rs
@@ -0,0 +1,82 @@
+use crate::solution::Solution;
+use crate::utils::*;
+
+use std::io;
+
+struct Node {
+    name: String,
+    weight: u32,
+    children: Vec<String>,
+}
+
+impl Node {
+    fn parse(s: &str) -> Self {
+        let parts = s.split(" -> ").collect::<Vec<&str>>();
+        let (name, weight) = parts[0]
+            .split_once(" ")
+            .expect("Node should contain name and weight");
+        let name = name.to_string();
+        let weight = weight[1..weight.len() - 1]
+            .parse::<u32>()
+            .expect("Node weight should be integer");
+        let children = if parts.len() > 1 {
+            parts[1]
+                .split(", ")
+                .map(|s| s.to_string())
+                .collect::<Vec<String>>()
+        } else {
+            vec![]
+        };
+        Self {
+            name,
+            weight,
+            children,
+        }
+    }
+}
+
+pub struct AoC2017_07 {
+    nodes: Vec<Node>,
+}
+
+impl AoC2017_07 {
+    pub fn new() -> io::Result<Self> {
+        let nodes = read_file_as_lines("input/aoc2017_07")?
+            .iter()
+            .map(|s| Node::parse(s))
+            .collect::<Vec<Node>>();
+        Ok(Self { nodes })
+    }
+}
+
+impl Solution for AoC2017_07 {
+    // fn part_one(&self) -> String {
+    // }
+
+    // fn part_two(&self) -> String {
+    // }
+
+    fn description(&self) -> String {
+        "AoC 2017/Day 7: Recursive Circus".to_string()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn aoc2017_07_input_load_test() -> io::Result<()> {
+        let sol = AoC2017_07::new()?;
+        assert!(!sol.nodes.is_empty());
+        Ok(())
+    }
+
+    #[test]
+    fn aoc2017_07_correctness() -> io::Result<()> {
+        let sol = AoC2017_07::new()?;
+        assert_eq!(sol.part_one(), "");
+        assert_eq!(sol.part_two(), "");
+        Ok(())
+    }
+}
