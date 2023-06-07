37862063ac692df6e0eea0e2f9d1bb87db8913d3
Sergey Chelak <sergey.chelak@gmail.com>
2023-06-08 02:38:16 +0300

    refactoring & solving 2017/11/part 2

--- src/aoc2017/day_11.rs
+++ src/aoc2017/day_11.rs
@@ -25,17 +25,6 @@ impl Edge {
             _ => panic!("Unexpected value '{s}'"),
         }
     }
-
-    // fn inverse(&self) -> Self {
-    //     match self {
-    //         Self::North => Self::South,
-    //         Self::South => Self::North,
-    //         Self::SouthWest => Self::NorthEast,
-    //         Self::NorthEast => Self::SouthWest,
-    //         Self::NorthWest => Self::SouthEast,
-    //         Self::SouthEast => Self::NorthWest,
-    //     }
-    // }
 }
 
 struct Container {
@@ -44,8 +33,73 @@ struct Container {
     delta_edge: Option<Edge>,
 }
 
+fn distance(steps: &[Edge]) -> usize {
+    let mut arr = [0usize; 6];
+    steps.iter().for_each(|edge| {
+        arr[*edge as usize] += 1;
+    });
+    let mut sum = arr.iter().sum();
+    loop {
+        [
+            Container {
+                edge_a: Edge::North,
+                edge_b: Edge::South,
+                delta_edge: None,
+            },
+            Container {
+                edge_a: Edge::NorthEast,
+                edge_b: Edge::SouthWest,
+                delta_edge: None,
+            },
+            Container {
+                edge_a: Edge::SouthEast,
+                edge_b: Edge::NorthWest,
+                delta_edge: None,
+            },
+            Container {
+                edge_a: Edge::NorthWest,
+                edge_b: Edge::South,
+                delta_edge: Some(Edge::SouthWest),
+            },
+            Container {
+                edge_a: Edge::SouthEast,
+                edge_b: Edge::North,
+                delta_edge: Some(Edge::NorthEast),
+            },
+            Container {
+                edge_a: Edge::SouthWest,
+                edge_b: Edge::North,
+                delta_edge: Some(Edge::NorthWest),
+            },
+            Container {
+                edge_a: Edge::NorthEast,
+                edge_b: Edge::South,
+                delta_edge: Some(Edge::SouthEast),
+            },
+        ]
+        .iter()
+        .for_each(|container| {
+            let idx_a = container.edge_a as usize;
+            let idx_b = container.edge_b as usize;
+
+            let val = arr[idx_a].min(arr[idx_b]);
+            arr[idx_a] -= val;
+            arr[idx_b] -= val;
+            if let Some(edge) = container.delta_edge {
+                arr[edge as usize] += val;
+            }
+        });
+        let new_sum = arr.iter().sum::<usize>();
+        if new_sum == sum {
+            break;
+        }
+        sum = new_sum;
+    }
+    sum
+}
+
 pub struct AoC2017_11 {
-    edges: Vec<Edge>,
+    steps: Vec<Edge>,
 }
 
 impl AoC2017_11 {
@@ -55,79 +109,23 @@ impl AoC2017_11 {
     }
 
     fn with_input(s: &str) -> Self {
-        let edges = s.trim().split(',').map(|val| Edge::with_str(val)).collect();
-        Self { edges }
+        let steps = s.trim().split(',').map(Edge::with_str).collect();
+        Self { steps }
     }
 }
 
 impl Solution for AoC2017_11 {
     fn part_one(&self) -> String {
-        let mut arr = [0usize; 6];
-        self.edges.iter().for_each(|edge| {
-            arr[*edge as usize] += 1;
-        });
-        let mut sum = arr.iter().sum();
-        loop {
-            [
-                Container {
-                    edge_a: Edge::North,
-                    edge_b: Edge::South,
-                    delta_edge: None,
-                },
-                Container {
-                    edge_a: Edge::NorthEast,
-                    edge_b: Edge::SouthWest,
-                    delta_edge: None,
-                },
-                Container {
-                    edge_a: Edge::SouthEast,
-                    edge_b: Edge::NorthWest,
-                    delta_edge: None,
-                },
-                Container {
-                    edge_a: Edge::NorthWest,
-                    edge_b: Edge::South,
-                    delta_edge: Some(Edge::SouthWest),
-                },
-                Container {
-                    edge_a: Edge::SouthEast,
-                    edge_b: Edge::North,
-                    delta_edge: Some(Edge::NorthEast),
-                },
-                Container {
-                    edge_a: Edge::SouthWest,
-                    edge_b: Edge::North,
-                    delta_edge: Some(Edge::NorthWest),
-                },
-                Container {
-                    edge_a: Edge::NorthEast,
-                    edge_b: Edge::South,
-                    delta_edge: Some(Edge::SouthEast),
-                },
-            ]
-            .iter()
-            .for_each(|container| {
-                let idx_a = container.edge_a as usize;
-                let idx_b = container.edge_b as usize;
-
-                let val = arr[idx_a].min(arr[idx_b]);
-                arr[idx_a] -= val;
-                arr[idx_b] -= val;
-                if let Some(edge) = container.delta_edge {
-                    arr[edge as usize] += val;
-                }
-            });
-            let new_sum = arr.iter().sum::<usize>();
-            if new_sum == sum {
-                break;
-            }
-            sum = new_sum;
-        }
-        sum.to_string()
+        distance(&self.steps).to_string()
     }
 
-    // fn part_two(&self) -> String {
-    // }
+    fn part_two(&self) -> String {
+        let mut max_steps = 0usize;
+        for l in 1..=self.steps.len() {
+            max_steps = max_steps.max(distance(&self.steps[0..l]));
+        }
+        max_steps.to_string()
+    }
 
     fn description(&self) -> String {
         "AoC 2017/Day 11: Hex Ed".to_string()
@@ -141,7 +139,7 @@ mod test {
     #[test]
     fn aoc2017_11_input_load_test() -> io::Result<()> {
         let sol = AoC2017_11::new()?;
-        assert!(!sol.edges.is_empty());
+        assert!(!sol.steps.is_empty());
         Ok(())
     }
 
@@ -149,7 +147,7 @@ mod test {
     fn aoc2017_11_correctness() -> io::Result<()> {
         let sol = AoC2017_11::new()?;
         assert_eq!(sol.part_one(), "773");
-        assert_eq!(sol.part_two(), "");
+        assert_eq!(sol.part_two(), "1560");
         Ok(())
     }
 }
