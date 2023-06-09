354e1984252edbf895049e39a64c43b908b76be7
Sergey Chelak <sergey.chelak@gmail.com>
2023-06-09 21:22:33 +0300

    parsing input data

--- src/aoc2017/day_12.rs
+++ src/aoc2017/day_12.rs
@@ -1,18 +1,45 @@
 use crate::solution::Solution;
 use crate::utils::*;
 
+use std::collections::HashMap;
 use std::io;
 
+type Graph = HashMap<usize, usize>;
+
 pub struct AoC2017_12 {
-    // place required fields here
+    graph: Graph,
 }
 
 impl AoC2017_12 {
     pub fn new() -> io::Result<Self> {
-        _ = read_file_as_lines("input/aoc2017_12")?;
-        Ok(Self {
-            // initialize solution
-        })
+        let mut graph = Graph::new();
+        read_file_as_lines("input/aoc2017_12")?
+            .iter()
+            .map(|s| Self::parse_line(s))
+            .for_each(|(node, connections)| {
+                connections.iter().for_each(|&elem| {
+                    graph.insert(node, elem);
+                    graph.insert(elem, node);
+                })
+            });
+        Ok(Self { graph })
+    }
+
+    fn parse_line(s: &str) -> (usize, Vec<usize>) {
+        let (node, connections) = s
+            .split_once(" <-> ")
+            .expect("Items should be separated with  <->");
+        let node = node
+            .parse::<usize>()
+            .expect("Integer expected as node number");
+        let connections = connections
+            .split(", ")
+            .map(|x| {
+                x.parse::<usize>()
+                    .expect("Connected node number integer number")
+            })
+            .collect::<Vec<usize>>();
+        (node, connections)
     }
 }
 
@@ -35,6 +62,7 @@ mod test {
     #[test]
     fn aoc2017_12_input_load_test() -> io::Result<()> {
         let sol = AoC2017_12::new()?;
+        assert!(!sol.graph.is_empty());
         Ok(())
     }
 
