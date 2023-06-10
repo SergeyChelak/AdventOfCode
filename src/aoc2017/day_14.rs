eca7601b1d71b7862f74b3b0e7dcae92d21afabb
Sergey Chelak <sergey.chelak@gmail.com>
2023-06-10 21:46:39 +0300

    solved 2017/15/part 2

--- src/aoc2017/day_14.rs
+++ src/aoc2017/day_14.rs
@@ -35,12 +35,64 @@ impl Solution for AoC2017_14 {
             .to_string()
     }
 
-    // fn part_two(&self) -> String {
-    // }
+    fn part_two(&self) -> String {
+        let mut matrix = make_matrix(&self.input);
+        let mut regions = 0;
+        for i in 0..matrix.len() {
+            for j in 0..matrix[i].len() {
+                if matrix[i][j] == 0 {
+                    continue;
+                }
+                fill_region(&mut matrix, i, j);
+                regions += 1;
+            }
+        }
+        regions.to_string()
+    }
 
     fn description(&self) -> String {
         "AoC 2017/Day 14: Disk Defragmentation".to_string()
     }
+}
+
+type Matrix = Vec<Vec<i32>>;
+
+fn make_matrix(input: &str) -> Matrix {
+    (0..128)
+        .map(|i| format!("{}-{i}", input))
+        .map(|s| s.knot_hash())
+        .map(|hash| hash2binary(&hash))
+        .collect()
+}
+
+fn fill_region(matrix: &mut Matrix, row: usize, col: usize) {
+    if matrix[row][col] == 0 {
+        return;
+    }
+    matrix[row][col] = 0;
+    if row > 0 {
+        fill_region(matrix, row - 1, col);
+    }
+    if row < matrix.len() - 1 {
+        fill_region(matrix, row + 1, col);
+    }
+    if col > 0 {
+        fill_region(matrix, row, col - 1);
+    }
+    if col < matrix[row].len() - 1 {
+        fill_region(matrix, row, col + 1);
+    }
+}
+
+fn hash2binary(s: &str) -> Vec<i32> {
+    let mut arr = Vec::new();
+    s.chars().filter_map(|ch| ch.to_digit(16)).for_each(|x| {
+        for offset in (0..4).rev() {
+            let val = if x & (1 << offset) > 0 { 1 } else { 0 };
+            arr.push(val);
+        }
+    });
+    arr
 }
 
 #[cfg(test)]
@@ -56,10 +108,18 @@ mod test {
     }
 
     #[test]
+    fn aoc2017_14_example2() {
+        let sol = AoC2017_14 {
+            input: "flqrgnkx".to_string(),
+        };
+        assert_eq!(sol.part_two(), "1242");
+    }
+
+    #[test]
     fn aoc2017_14_correctness() -> io::Result<()> {
         let sol = AoC2017_14::new()?;
         assert_eq!(sol.part_one(), "8106");
-        assert_eq!(sol.part_two(), "");
+        assert_eq!(sol.part_two(), "1164");
         Ok(())
     }
 }
