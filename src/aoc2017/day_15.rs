82c02cd7fa5615021da7f7590c3e94428b797129
Sergey Chelak <sergey.chelak@gmail.com>
2023-06-11 00:38:09 +0300

    solved 2017/15/part 2

--- src/aoc2017/day_15.rs
+++ src/aoc2017/day_15.rs
@@ -7,6 +7,7 @@ type Value = u64;
 struct Generator {
     value: Value,
     factor: Value,
+    filter: Value,
 }
 
 impl Generator {
@@ -14,6 +15,7 @@ impl Generator {
         Self {
             value,
             factor: 16807,
+            filter: 4,
         }
     }
 
@@ -21,11 +23,21 @@ impl Generator {
         Self {
             value,
             factor: 48271,
+            filter: 8,
         }
     }
 
     fn next(&mut self) {
         self.value = self.value * self.factor % 2147483647;
+    }
+
+    fn next_filtered(&mut self) {
+        loop {
+            self.next();
+            if self.value % self.filter == 0 {
+                break;
+            }
+        }
     }
 }
 
@@ -41,27 +53,34 @@ impl AoC2017_15 {
             start_b: 516,
         })
     }
-}
 
-impl Solution for AoC2017_15 {
-    fn part_one(&self) -> String {
+    fn count_matches(&self, max_pairs: usize, next: &impl Fn(&mut Generator)) -> i32 {
         let mut gen_a = Generator::gen_a(self.start_a);
         let mut gen_b = Generator::gen_b(self.start_b);
         let mut count = 0;
-        for _ in 0..40_000_000 {
+        for _ in 0..max_pairs {
             let val_a = gen_a.value & 0xffff;
             let val_b = gen_b.value & 0xffff;
             if val_a == val_b {
                 count += 1;
             }
-            gen_a.next();
-            gen_b.next();
+            next(&mut gen_a);
+            next(&mut gen_b);
         }
-        count.to_string()
+        count
+    }
+}
+
+impl Solution for AoC2017_15 {
+    fn part_one(&self) -> String {
+        self.count_matches(40_000_000, &|gen: &mut Generator| gen.next())
+            .to_string()
     }
 
-    // fn part_two(&self) -> String {
-    // }
+    fn part_two(&self) -> String {
+        self.count_matches(5_000_000, &|gen: &mut Generator| gen.next_filtered())
+            .to_string()
+    }
 
     fn description(&self) -> String {
         "AoC 2017/Day 15: Dueling Generators".to_string()
@@ -85,7 +104,7 @@ mod test {
     fn aoc2017_15_correctness() -> io::Result<()> {
         let sol = AoC2017_15::new()?;
         assert_eq!(sol.part_one(), "594");
-        assert_eq!(sol.part_two(), "");
+        assert_eq!(sol.part_two(), "328");
         Ok(())
     }
 }
