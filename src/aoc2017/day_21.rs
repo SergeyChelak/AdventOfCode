b0134c113e06260ca4efd43339458dc62908cc42
Sergey Chelak <sergey.chelak@gmail.com>
2023-06-20 23:32:31 +0300

    draft solution

--- src/aoc2017/day_21.rs
+++ src/aoc2017/day_21.rs
@@ -7,45 +7,47 @@ use std::io;
 type Value = char;
 type Matrix = Vec<Vec<Value>>;
 
-trait MatrixOps {
-    fn initial() -> Self;
-
-    fn as_string(&self) -> String;
-
-    fn from_str(s: &str) -> Self;
-
-    fn rotate(&self) -> Self;
+fn matrix_from_str(s: &str) -> Matrix {
+    s.split('/')
+        .map(|x| x.chars().collect::<Vec<Value>>())
+        .collect::<Matrix>()
 }
 
-impl MatrixOps for Matrix {
-    fn initial() -> Self {
-        Self::from_str(".#./..#/###")
-    }
+fn matrix_to_string(matrix: &Matrix) -> String {
+    matrix
+        .iter()
+        .map(|arr| arr.iter().collect::<String>())
+        .collect::<Vec<String>>()
+        .join("/")
+}
 
-    fn as_string(&self) -> String {
-        self.iter()
-            .map(|arr| arr.iter().collect::<String>())
-            .collect::<Vec<String>>()
-            .join("/")
-    }
+fn flip_horizontal(matrix: &Matrix) -> Matrix {
+    let mut matrix = matrix.clone();
+    matrix.iter_mut().for_each(|row| row.reverse());
+    matrix
+}
 
-    fn from_str(s: &str) -> Self {
-        s.split('/')
-            .map(|x| x.chars().collect::<Vec<Value>>())
-            .collect::<Matrix>()
-    }
+fn flip_vertical(matrix: &Matrix) -> Matrix {
+    matrix.clone().into_iter().rev().collect::<Matrix>()
+}
 
-    fn rotate(&self) -> Self {
-        let mut matrix = self.clone().into_iter().rev().collect::<Matrix>();
-        for i in 0..matrix.len() {
-            for j in i + 1..matrix[i].len() {
-                let tmp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = tmp;
-            }
+fn transpose(matrix: &Matrix) -> Matrix {
+    let n = matrix.len();
+    let m = matrix[0].len();
+    let mut result = vec![vec!['\0'; n]; m];
+    for i in 0..n {
+        for j in 0..m {
+            result[j][i] = matrix[i][j];
         }
-        matrix
     }
+    result
+}
+
+enum Operation {
+    Nothing,
+    Transpose,
+    FlipVertical,
+    FlipHorizontal,
 }
 
 pub struct AoC2017_21 {
@@ -63,11 +65,66 @@ impl AoC2017_21 {
             });
         Ok(Self { rules })
     }
+
+    fn enhance_art(&self, matrix: &mut Matrix) {
+        let len = matrix.len();
+        let step = if len % 2 == 0 {
+            2
+        } else if len % 3 == 0 {
+            3
+        } else {
+            panic!("Unexpected step size")
+        };
+        let ops = [
+            Operation::Nothing,
+            Operation::FlipHorizontal,
+            Operation::FlipVertical,
+            Operation::FlipHorizontal,
+            Operation::Transpose,
+            Operation::FlipHorizontal,
+            Operation::FlipVertical,
+            Operation::FlipHorizontal,
+        ];
+        for r in (0..len).step_by(step) {
+            for c in (0..len).step_by(step) {
+                let mut inner = vec![vec!['\0'; step]; step];
+                for i in 0..step {
+                    for j in 0..step {
+                        inner[i][j] = matrix[i + r][j + c];
+                    }
+                }
+                for op in &ops {
+                    inner = match op {
+                        Operation::Nothing => inner,
+                        Operation::Transpose => transpose(&inner),
+                        Operation::FlipHorizontal => flip_horizontal(&inner),
+                        Operation::FlipVertical => flip_vertical(&inner),
+                    };
+                    let key = matrix_to_string(&inner);
+                    if let Some(pattern) = self.rules.get(&key) {
+                        println!("Match found: {}", pattern);
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
 
 impl Solution for AoC2017_21 {
-    // fn part_one(&self) -> String {
-    // }
+    fn part_one(&self) -> String {
+        let mut matrix = matrix_from_str(".#./..#/###");
+        (0..5).for_each(|_| self.enhance_art(&mut matrix));
+        matrix
+            .iter()
+            .map(|x| {
+                x.iter()
+                    .map(|v| if *v == '#' { 1 } else { 0 })
+                    .sum::<usize>()
+            })
+            .sum::<usize>()
+            .to_string()
+    }
 
     // fn part_two(&self) -> String {
     // }
