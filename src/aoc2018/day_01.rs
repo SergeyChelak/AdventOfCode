06200258b934b2bc4d1c1dc160445b0675f9be56
Sergey Chelak <sergey.chelak@gmail.com>
2023-07-05 22:42:23 +0300

    solved 2018/01

--- src/aoc2018/day_01.rs
+++ src/aoc2018/day_01.rs
@@ -4,20 +4,24 @@ use crate::utils::*;
 use std::io;
 
 pub struct AoC2018_01 {
-    // place required fields here
+    input: Vec<i32>,
 }
 
 impl AoC2018_01 {
     pub fn new() -> io::Result<Self> {
-        Ok(Self {
-            // initialize solution
-        })
+        let (input, _): (Vec<_>, Vec<_>) = read_file_as_lines("input/aoc2018_01")?
+            .iter()
+            .map(|s| s.parse::<i32>())
+            .partition(Result::is_ok);
+        let input = input.into_iter().map(Result::unwrap).collect();
+        Ok(Self { input })
     }
 }
 
 impl Solution for AoC2018_01 {
-    // fn part_one(&self) -> String {
-    // }
+    fn part_one(&self) -> String {
+        self.input.iter().sum::<i32>().to_string()
+    }
 
     // fn part_two(&self) -> String {
     // }
@@ -34,13 +38,14 @@ mod test {
     #[test]
     fn aoc2018_01_input_load_test() -> io::Result<()> {
         let sol = AoC2018_01::new()?;
+        assert!(!sol.input.is_empty());
         Ok(())
     }
 
     #[test]
     fn aoc2018_01_correctness() -> io::Result<()> {
         let sol = AoC2018_01::new()?;
-        assert_eq!(sol.part_one(), "");
+        assert_eq!(sol.part_one(), "543");
         assert_eq!(sol.part_two(), "");
         Ok(())
     }
--- src/aoc2018/mod.rs
+++ src/aoc2018/mod.rs
@@ -3,6 +3,78 @@ use super::solution::Solution;
 mod day_01;
 use day_01::*;
 
+// mod day_02;
+// use day_02::*;
+
+// mod day_03;
+// use day_03::*;
+
+// mod day_04;
+// use day_04::*;
+
+// mod day_05;
+// use day_05::*;
+
+// mod day_06;
+// use day_06::*;
+
+// mod day_07;
+// use day_07::*;
+
+// mod day_08;
+// use day_08::*;
+
+// mod day_09;
+// use day_09::*;
+
+// mod day_10;
+// use day_10::*;
+
+// mod day_11;
+// use day_11::*;
+
+// mod day_12;
+// use day_12::*;
+
+// mod day_13;
+// use day_13::*;
+
+// mod day_14;
+// use day_14::*;
+
+// mod day_15;
+// use day_15::*;
+
+// mod day_16;
+// use day_16::*;
+
+// mod day_17;
+// use day_17::*;
+
+// mod day_18;
+// use day_18::*;
+
+// mod day_19;
+// use day_19::*;
+
+// mod day_20;
+// use day_20::*;
+
+// mod day_21;
+// use day_21::*;
+
+// mod day_22;
+// use day_22::*;
+
+// mod day_23;
+// use day_23::*;
+
+// mod day_24;
+// use day_24::*;
+
+// mod day_25;
+// use day_25::*;
+
 pub fn all_days() -> std::io::Result<Vec<Box<dyn Solution>>> {
     Ok(vec![
         Box::new(AoC2018_01::new()?),
