48edf4abb1f107db5b72226f122e9229225b4dea
Sergey Chelak <sergey.chelak@gmail.com>
2023-07-09 00:48:01 +0300

    parsing input

--- src/aoc2018/day_04.rs
+++ src/aoc2018/day_04.rs
@@ -3,18 +3,60 @@ use crate::utils::*;
 
 use std::io;
 
+use chrono::NaiveDateTime;
+
+enum Event {
+    Begin(NaiveDateTime, usize),
+    WakeUp(NaiveDateTime),
+    Asleep(NaiveDateTime),
+}
+
+impl Event {
+    fn from_str(s: &str) -> Self {
+        let index = s.find(']').expect("Closing square bracket not found");
+        let formatted_date = &s[1..index];
+        let date = NaiveDateTime::parse_from_str(formatted_date, "%Y-%m-%d %H:%M")
+            .expect("Failed to parse date");
+        let tokens = s[index + 1..].trim().split(' ').collect::<Vec<&str>>();
+        match tokens[0] {
+            "wakes" => Self::WakeUp(date),
+            "falls" => Self::Asleep(date),
+            "Guard" => {
+                let id = tokens[1][1..]
+                    .parse::<usize>()
+                    .expect("Failed to parse guard id");
+                Self::Begin(date, id)
+            }
+            _ => panic!("Unexpected token {}", tokens[0]),
+        }
+    }
+
+    fn duration(&self, other: &Self) -> i64 {
+        (self.time() - other.time()).num_minutes()
+    }
+
+    fn time(&self) -> NaiveDateTime {
+        match self {
+            Self::Asleep(time) => *time,
+            Self::WakeUp(time) => *time,
+            Self::Begin(time, _) => *time,
+        }
+    }
+}
+
 pub struct AoC2018_04 {
-    // place required fields here
+    records: Vec<Event>,
 }
 
 impl AoC2018_04 {
     pub fn new() -> io::Result<Self> {
         let mut records = read_file_as_lines("input/aoc2018_04")?;
         records.sort();
-        println!("{:?}", records);
-        Ok(Self {
-            // initialize solution
-        })
+        let records = records
+            .iter()
+            .map(|s| Event::from_str(s))
+            .collect::<Vec<Event>>();
+        Ok(Self { records })
     }
 }
 
@@ -37,6 +79,7 @@ mod test {
     #[test]
     fn aoc2018_04_input_load_test() -> io::Result<()> {
         let sol = AoC2018_04::new()?;
+        assert!(!sol.records.is_empty());
         Ok(())
     }
 
