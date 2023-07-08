6ff1e6a448477824cd2760a9f766e58e65f521e5
Sergey Chelak <sergey.chelak@gmail.com>
2023-07-09 01:35:52 +0300

    solved 2018/04/#1

--- src/aoc2018/day_04.rs
+++ src/aoc2018/day_04.rs
@@ -1,9 +1,10 @@
 use crate::solution::Solution;
 use crate::utils::*;
 
+use std::collections::HashMap;
 use std::io;
 
-use chrono::NaiveDateTime;
+use chrono::{NaiveDateTime, Timelike};
 
 enum Event {
     Begin(NaiveDateTime, usize),
@@ -30,19 +31,9 @@ impl Event {
             _ => panic!("Unexpected token {}", tokens[0]),
         }
     }
-
-    fn duration(&self, other: &Self) -> i64 {
-        (self.time() - other.time()).num_minutes()
-    }
-
-    fn time(&self) -> NaiveDateTime {
-        match self {
-            Self::Asleep(time) => *time,
-            Self::WakeUp(time) => *time,
-            Self::Begin(time, _) => *time,
-        }
-    }
 }
+
+type Stat = [usize; 60];
 
 pub struct AoC2018_04 {
     records: Vec<Event>,
@@ -61,8 +52,40 @@ impl AoC2018_04 {
 }
 
 impl Solution for AoC2018_04 {
-    // fn part_one(&self) -> String {
-    // }
+    fn part_one(&self) -> String {
+        let mut timeline: HashMap<usize, Stat> = HashMap::new();
+        let mut guard_id = 0;
+        let mut start_asleep: Option<u32> = None;
+        for event in &self.records {
+            match event {
+                Event::Asleep(time) => start_asleep = Some(time.minute()),
+                Event::WakeUp(time) => {
+                    let entry = timeline.entry(guard_id).or_insert([0usize; 60]);
+                    let start =
+                        start_asleep.expect("Asleep time should appear before wake up") as usize;
+                    let stop = time.minute() as usize;
+                    for i in start..stop {
+                        entry[i] += 1;
+                    }
+                }
+                Event::Begin(_, id) => guard_id = *id,
+            };
+        }
+        let (&id, time) = timeline
+            .iter()
+            .max_by(|(_, a), (_, b)| {
+                let sum_a: usize = a.iter().sum();
+                let sum_b: usize = b.iter().sum();
+                sum_a.cmp(&sum_b)
+            })
+            .expect("Timeline is empty");
+        let best_minute = time
+            .iter()
+            .enumerate()
+            .max_by(|(_, &a), (_, &b)| a.cmp(&b))
+            .expect("Timeline isn't filled");
+        (id * best_minute.0).to_string()
+    }
 
     // fn part_two(&self) -> String {
     // }
@@ -86,8 +109,36 @@ mod test {
     #[test]
     fn aoc2018_04_correctness() -> io::Result<()> {
         let sol = AoC2018_04::new()?;
-        assert_eq!(sol.part_one(), "");
+        assert_eq!(sol.part_one(), "101194");
         assert_eq!(sol.part_two(), "");
         Ok(())
+    }
+
+    #[test]
+    fn aoc2018_04_example1() {
+        let records = vec![
+            "[1518-11-01 00:00] Guard #10 begins shift",
+            "[1518-11-01 00:05] falls asleep",
+            "[1518-11-01 00:25] wakes up",
+            "[1518-11-01 00:30] falls asleep",
+            "[1518-11-01 00:55] wakes up",
+            "[1518-11-01 23:58] Guard #99 begins shift",
+            "[1518-11-02 00:40] falls asleep",
+            "[1518-11-02 00:50] wakes up",
+            "[1518-11-03 00:05] Guard #10 begins shift",
+            "[1518-11-03 00:24] falls asleep",
+            "[1518-11-03 00:29] wakes up",
+            "[1518-11-04 00:02] Guard #99 begins shift",
+            "[1518-11-04 00:36] falls asleep",
+            "[1518-11-04 00:46] wakes up",
+            "[1518-11-05 00:03] Guard #99 begins shift",
+            "[1518-11-05 00:45] falls asleep",
+            "[1518-11-05 00:55] wakes up",
+        ]
+        .iter()
+        .map(|s| Event::from_str(s))
+        .collect::<Vec<Event>>();
+        let sol = AoC2018_04 { records };
+        assert_eq!(sol.part_one(), "240");
     }
 }
