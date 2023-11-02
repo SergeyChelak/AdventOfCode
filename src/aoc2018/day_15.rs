b7afe14d4d8842ce2595621e2e77eda1986af44e
Sergey Chelak <sergey.chelak@gmail.com>
2023-11-02 12:56:16 +0200

    parse input

--- src/aoc2018/day_15.rs
+++ src/aoc2018/day_15.rs
@@ -3,16 +3,45 @@ use crate::utils::*;
 
 use std::io;
 
+#[derive(Clone, Copy, Debug)]
+enum Elem {
+    Elf(i32),
+    Goblin(i32),
+    Wall,
+    Empty,
+}
+
+// Each unit, either Goblin or Elf, has 3 attack power and starts with 200 hit points.
+const INITIAL_HIT_POINTS: i32 = 200;
+const ATTACK_POWER: i32 = 3;
+
 pub struct AoC2018_15 {
-    // place required fields here
+    maze: Vec<Vec<Elem>>,
 }
 
 impl AoC2018_15 {
     pub fn new() -> io::Result<Self> {
-        _ = read_file_as_lines("input/aoc2018_15")?;
-        Ok(Self {
-            // initialize solution
-        })
+        let input = read_file_as_lines("input/aoc2018_15")?;
+        let maze = Self::parse_maze(&input);
+        Ok(Self { maze })
+    }
+
+    fn parse_maze(input: &[String]) -> Vec<Vec<Elem>> {
+        let mut maze = Vec::new();
+        input.iter().for_each(|line| {
+            let row = line
+                .chars()
+                .map(|ch| match ch {
+                    '#' => Elem::Wall,
+                    '.' => Elem::Empty,
+                    'E' => Elem::Elf(INITIAL_HIT_POINTS),
+                    'G' => Elem::Goblin(INITIAL_HIT_POINTS),
+                    _ => panic!("Unexpected char '{}'", ch),
+                })
+                .collect::<Vec<Elem>>();
+            maze.push(row);
+        });
+        maze
     }
 }
 
@@ -35,6 +64,7 @@ mod test {
     #[test]
     fn aoc2018_15_input_load_test() -> io::Result<()> {
         let sol = AoC2018_15::new()?;
+        assert!(!sol.maze.is_empty());
         Ok(())
     }
 
