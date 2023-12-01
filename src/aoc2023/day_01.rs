b213e1855b1a24461d8243bbb712ceb78bba22a2
Sergey Chelak <sergey.chelak@gmail.com>
2023-12-01 12:12:40 +0200

    solved part 2

--- src/aoc2023/day_01.rs
+++ src/aoc2023/day_01.rs
@@ -1,6 +1,7 @@
 use crate::solution::Solution;
 use crate::utils::*;
 
+use std::char::from_digit;
 use std::io;
 
 pub struct AoC2023_01 {
@@ -23,8 +24,13 @@ impl Solution for AoC2023_01 {
             .to_string()
     }
 
-    // fn part_two(&self) -> String {
-    // }
+    fn part_two(&self) -> String {
+        self.input
+            .iter()
+            .map(|x| get_spelled_digit(x))
+            .sum::<u32>()
+            .to_string()
+    }
 
     fn description(&self) -> String {
         "AoC 2023/Day 1: Trebuchet?!".to_string()
@@ -38,6 +44,22 @@ fn get_digit(s: &str) -> u32 {
         .collect::<Vec<u32>>();
     digits.first().expect("digit should be present (1)") * 10
         + digits.last().expect("digit should be present (2)")
+}
+
+fn get_spelled_digit(s: &str) -> u32 {
+    let words = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    let mut output = s.chars().collect::<Vec<char>>();
+    for (digit, w) in words.iter().enumerate() {
+        s.match_indices(w).map(|(idx, _)| idx).for_each(|pos| {
+            let ch = from_digit((digit + 1) as u32, 10)
+                .expect("Impossible value during converting to char");
+            output[pos] = ch;
+        });
+    }
+    let out = String::from_iter(output);
+    get_digit(&out)
 }
 
 #[cfg(test)]
@@ -55,7 +77,23 @@ mod test {
     fn aoc2023_01_correctness() -> io::Result<()> {
         let sol = AoC2023_01::new()?;
         assert_eq!(sol.part_one(), "54644");
-        assert_eq!(sol.part_two(), "");
+        assert_eq!(sol.part_two(), "53348");
         Ok(())
+    }
+
+    #[test]
+    fn aoc2023_01_ex_2() {
+        let arr = [
+            ("two1nine", 29),
+            ("eightwothree", 83),
+            ("abcone2threexyz", 13),
+            ("xtwone3four", 24),
+            ("4nineeightseven2", 42),
+            ("zoneight234", 14),
+            ("7pqrstsixteen", 76),
+        ];
+        for (inp, out) in arr {
+            assert_eq!(get_spelled_digit(inp), out)
+        }
     }
 }
