bc6faf9734bfd44e1287d6e4b9f87ac4b9490cd8
Sergey Chelak <sergey.chelak@gmail.com>
2023-12-03 13:38:17 +0200

    solved part 2

--- src/aoc2023/day_03.rs
+++ src/aoc2023/day_03.rs
@@ -1,7 +1,11 @@
 use crate::solution::Solution;
 use crate::utils::*;
 
+use std::collections::HashSet;
 use std::io;
+use std::ops::Range;
+
+type Position = (usize, usize);
 
 pub struct AoC2023_03 {
     input: Vec<Vec<char>>,
@@ -45,8 +49,21 @@ impl Solution for AoC2023_03 {
         total.to_string()
     }
 
-    // fn part_two(&self) -> String {
-    // }
+    fn part_two(&self) -> String {
+        let mut total = 0;
+        for (row, arr) in self.input.iter().enumerate() {
+            for (col, val) in arr.iter().enumerate() {
+                if *val != '*' {
+                    continue;
+                }
+                let positions = adjacent_digits(&self.input, row, col);
+                if positions.len() == 2 {
+                    total += positions[0] * positions[1];
+                }
+            }
+        }
+        total.to_string()
+    }
 
     fn description(&self) -> String {
         "AoC 2023/Day 3: Gear Ratios".to_string()
@@ -54,41 +71,85 @@ impl Solution for AoC2023_03 {
 }
 
 fn has_adjacent_symbol(matrix: &[Vec<char>], row: usize, col: usize) -> bool {
+    adjacent_positions(matrix, row, col)
+        .iter()
+        .map(|(r, c)| matrix[*r][*c])
+        .filter(|ch| !matches!(ch, '.' | '0'..='9'))
+        .count()
+        > 0
+}
+
+fn adjacent_digits(matrix: &[Vec<char>], row: usize, col: usize) -> Vec<u32> {
+    adjacent_positions(matrix, row, col)
+        .into_iter()
+        .filter(|(r, c)| matrix[*r][*c].is_numeric())
+        .map(|(r, c)| (r, calc_digit_range(&matrix[r], c)))
+        .collect::<HashSet<_>>()
+        .iter()
+        .map(|(r, range)| {
+            String::from_iter(&matrix[*r][range.start..=range.end])
+                .parse::<u32>()
+                .expect("Failed to parse adjacent number")
+        })
+        .collect::<Vec<u32>>()
+}
+
+fn adjacent_positions(matrix: &[Vec<char>], row: usize, col: usize) -> Vec<Position> {
     let up = row > 0;
     let down = row < matrix.len() - 1;
     let left = col > 0;
     let right = col < matrix[row].len() - 1;
 
-    let mut chars = Vec::with_capacity(8);
+    let mut positions = Vec::with_capacity(8);
     if up {
-        chars.push(matrix[row - 1][col]);
+        positions.push((row - 1, col));
     }
     if down {
-        chars.push(matrix[row + 1][col]);
+        positions.push((row + 1, col));
     }
     if left {
-        chars.push(matrix[row][col - 1]);
+        positions.push((row, col - 1));
     }
     if right {
-        chars.push(matrix[row][col + 1]);
+        positions.push((row, col + 1));
     }
     if up && left {
-        chars.push(matrix[row - 1][col - 1]);
+        positions.push((row - 1, col - 1));
     }
     if up && right {
-        chars.push(matrix[row - 1][col + 1]);
+        positions.push((row - 1, col + 1));
     }
     if down && left {
-        chars.push(matrix[row + 1][col - 1]);
+        positions.push((row + 1, col - 1));
     }
     if down && right {
-        chars.push(matrix[row + 1][col + 1]);
+        positions.push((row + 1, col + 1));
     }
-    chars
-        .iter()
-        .filter(|ch| !matches!(ch, '.' | '0'..='9'))
-        .count()
-        > 0
+    positions
+}
+
+fn calc_digit_range(input: &[char], pos: usize) -> Range<usize> {
+    let mut begin = pos;
+    while begin > 0 {
+        if input[begin - 1].is_numeric() {
+            begin -= 1;
+        } else {
+            break;
+        }
+    }
+    let mut end = pos;
+    while end < input.len() - 1 {
+        if input[end + 1].is_numeric() {
+            end += 1;
+        } else {
+            break;
+        }
+    }
+    // String::from_iter(&input[begin..end])
+    //     .parse::<u32>()
+    //     .expect("Failed to parse adjacent number")
+
+    begin..end
 }
 
 #[cfg(test)]
@@ -104,7 +165,18 @@ mod test {
 
     #[test]
     fn aoc2023_03_ex1() {
-        let input = [
+        let sol = AoC2023_03 { input: input() };
+        assert_eq!(sol.part_one(), "4361");
+    }
+
+    #[test]
+    fn aoc2023_03_ex2() {
+        let sol = AoC2023_03 { input: input() };
+        assert_eq!(sol.part_two(), "467835");
+    }
+
+    fn input() -> Vec<Vec<char>> {
+        [
             "467..114..",
             "...*......",
             "..35..633.",
@@ -118,16 +190,14 @@ mod test {
         ]
         .iter()
         .map(|s| s.chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-        let sol = AoC2023_03 { input };
-        assert_eq!(sol.part_one(), "4361");
+        .collect::<Vec<_>>()
     }
 
     #[test]
     fn aoc2023_03_correctness() -> io::Result<()> {
         let sol = AoC2023_03::new()?;
         assert_eq!(sol.part_one(), "527364");
-        assert_eq!(sol.part_two(), "");
+        assert_eq!(sol.part_two(), "79026871");
         Ok(())
     }
 }
