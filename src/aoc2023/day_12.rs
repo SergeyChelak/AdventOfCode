b76f270ad4b22f80eb444ab2fd528edf20951460
Sergey Chelak <sergey.chelak@gmail.com>
2023-12-12 15:01:21 +0200

    solved part 1

--- src/aoc2023/day_12.rs
+++ src/aoc2023/day_12.rs
@@ -1,31 +1,105 @@
 use crate::solution::Solution;
 use crate::utils::*;
 
+use std::collections::{HashSet, VecDeque};
 use std::io;
+
+enum PatternMatch {
+    Yes,
+    No,
+    Possible,
+}
 
 #[derive(Debug)]
 struct GroupInfo {
-    pattern: String,
-    damaged_lengths: Vec<usize>,
+    spring: Vec<char>,
+    pattern: Vec<usize>,
 }
 
 impl From<&str> for GroupInfo {
     fn from(value: &str) -> Self {
-        let (pattern, lengths) = value.split_once(' ').expect("Separator not found");
-        let damaged_lengths = lengths
+        let (springs, pattern_str) = value.split_once(' ').expect("Separator not found");
+        let pattern = pattern_str
             .split(',')
             .map(|s| s.parse::<usize>().expect("Length should be integer"))
             .collect::<Vec<_>>();
         Self {
-            pattern: pattern.to_string(),
-            damaged_lengths,
+            spring: springs.chars().collect(),
+            pattern,
         }
     }
 }
 
 impl GroupInfo {
     fn arrangements_count(&self) -> usize {
-        todo!()
+        let mut backtrack = HashSet::new();
+        let mut state = VecDeque::from([self.spring.clone()]);
+        while !state.is_empty() {
+            let spring = state.pop_front().expect("state isn't expected to be empty");
+            match self.matching(&spring) {
+                PatternMatch::Yes => {
+                    backtrack.insert(spring);
+                }
+                PatternMatch::Possible => {
+                    let Some(idx) = spring.iter().position(|ch| *ch == '?') else {
+                        let s = String::from_iter(spring);
+                        println!(">>> {s}");
+                        panic!("'?' is expected");
+                    };
+                    let mut next = spring.clone();
+                    next[idx] = '.';
+                    state.push_back(next.clone());
+                    next[idx] = '#';
+                    state.push_back(next);
+                }
+                _ => {}
+            }
+        }
+        backtrack.len()
+    }
+
+    fn matching(&self, spring: &Vec<char>) -> PatternMatch {
+        let mut inp_pattern = Vec::new();
+        let mut acc = 0usize;
+        let mut is_completed = true;
+        for ch in spring {
+            match ch {
+                '?' => {
+                    is_completed = false;
+                    break;
+                }
+                '.' => {
+                    if acc > 0 {
+                        inp_pattern.push(acc);
+                    }
+                    acc = 0;
+                }
+                '#' => {
+                    acc += 1;
+                }
+                ch => panic!("Unexpected char {ch}"),
+            }
+        }
+        if is_completed && acc > 0 {
+            inp_pattern.push(acc);
+        }
+        let size = inp_pattern.len();
+        if is_completed && size != self.pattern.len() {
+            return PatternMatch::No;
+        }
+        if size > self.pattern.len() {
+            return PatternMatch::No;
+        }
+        for (a, b) in self.pattern[0..size].iter().zip(inp_pattern.iter()) {
+            if *a != *b {
+                return PatternMatch::No;
+            }
+        }
+        if is_completed {
+            PatternMatch::Yes
+        } else {
+            PatternMatch::Possible
+        }
     }
 }
 
@@ -113,7 +187,7 @@ mod test {
     #[test]
     fn aoc2023_12_correctness() -> io::Result<()> {
         let sol = AoC2023_12::new()?;
-        assert_eq!(sol.part_one(), "");
+        assert_eq!(sol.part_one(), "7361");
         assert_eq!(sol.part_two(), "");
         Ok(())
     }
