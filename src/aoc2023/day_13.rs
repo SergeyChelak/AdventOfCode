90c35d31dbdfe038921acf8d3752706f616ee184
Sergey Chelak <sergey.chelak@gmail.com>
2023-12-14 00:28:15 +0200

    solved part 1

--- src/aoc2023/day_13.rs
+++ src/aoc2023/day_13.rs
@@ -1,24 +1,42 @@
 use crate::solution::Solution;
-use crate::utils::*;
 
+use std::fs::read_to_string;
 use std::io;
 
 pub struct AoC2023_13 {
-    // place required fields here
+    blocks: Vec<String>,
 }
 
 impl AoC2023_13 {
     pub fn new() -> io::Result<Self> {
-        let _ = read_file_as_chars("input/aoc2023_13")?;
-        Ok(Self {
-            // initialize solution
-        })
+        let contents = read_to_string("input/aoc2023_13")?;
+        Ok(Self::with_chars(&contents))
+    }
+
+    fn with_chars(contents: &str) -> Self {
+        let blocks = contents
+            .split("\n\n")
+            .map(|s| s.to_string())
+            .collect::<Vec<_>>();
+        Self { blocks }
     }
 }
 
 impl Solution for AoC2023_13 {
-    // fn part_one(&self) -> String {
-    // }
+    fn part_one(&self) -> String {
+        let mut total_above = 0usize;
+        let mut total_left = 0usize;
+        for block in &self.blocks {
+            let lines = block.split_whitespace().collect::<Vec<_>>();
+            let chars = lines
+                .iter()
+                .map(|s| s.chars().collect::<Vec<_>>())
+                .collect::<Vec<_>>();
+            total_above += block_reflection_line(&chars);
+            total_left += block_reflection_line(&transpose(&chars));
+        }
+        (total_left + 100 * total_above).to_string()
+    }
 
     // fn part_two(&self) -> String {
     // }
@@ -28,6 +46,32 @@ impl Solution for AoC2023_13 {
     }
 }
 
+fn block_reflection_line(block: &[Vec<char>]) -> usize {
+    for i in 1..block.len() {
+        let above = block[..i].iter().rev().cloned().collect::<Vec<_>>();
+        let below = &block[i..];
+        let len = above.len().min(below.len());
+        let above = &above[..len];
+        let below = &below[..len];
+        if above == below {
+            return i;
+        }
+    }
+    0
+}
+
+fn transpose(inp: &[Vec<char>]) -> Vec<Vec<char>> {
+    let inp_rows = inp.len();
+    let inp_cols = inp[0].len();
+    let mut result = vec![vec!['\0'; inp_rows]; inp_cols];
+    for row in 0..inp_rows {
+        for col in 0..inp_cols {
+            result[col][row] = inp[row][col];
+        }
+    }
+    result
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -35,7 +79,32 @@ mod test {
     #[test]
     fn aoc2023_13_input_load_test() -> io::Result<()> {
         let sol = AoC2023_13::new()?;
+        assert!(!sol.blocks.is_empty());
         Ok(())
+    }
+
+    #[test]
+    fn aoc2023_13_ex1() {
+        let input = [
+            "#.##..##.",
+            "..#.##.#.",
+            "##......#",
+            "##......#",
+            "..#.##.#.",
+            "..##..##.",
+            "#.#.##.#.",
+            "",
+            "#...##..#",
+            "#....#..#",
+            "..##..###",
+            "#####.##.",
+            "#####.##.",
+            "..##..###",
+            "#....#..#",
+        ]
+        .join("\n");
+        let puzzle = AoC2023_13::with_chars(&input);
+        assert_eq!(puzzle.part_one(), "405")
     }
 
     #[test]
