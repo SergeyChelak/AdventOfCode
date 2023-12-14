2f25f7c77a74b86a1a46b87bcf27c579bdff99c5
Sergey Chelak <sergey.chelak@gmail.com>
2023-12-14 13:08:03 +0200

    solved part 1

--- src/aoc2023/day_14.rs
+++ src/aoc2023/day_14.rs
@@ -4,7 +4,7 @@ use crate::utils::*;
 use std::io;
 
 pub struct AoC2023_14 {
-    // place required fields here
+    input: Vec<Vec<char>>,
 }
 
 impl AoC2023_14 {
@@ -14,13 +14,19 @@ impl AoC2023_14 {
     }
 
     fn with_lines(lines: &[String]) -> Self {
-        todo!()
+        let input = lines
+            .iter()
+            .map(|row| row.chars().collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+        Self { input }
     }
 }
 
 impl Solution for AoC2023_14 {
-    // fn part_one(&self) -> String {
-    // }
+    fn part_one(&self) -> String {
+        let map = slide_north(&self.input);
+        total_load(&map).to_string()
+    }
 
     // fn part_two(&self) -> String {
     // }
@@ -30,6 +36,51 @@ impl Solution for AoC2023_14 {
     }
 }
 
+fn slide_north(input: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut result = input.clone();
+    for row in 1..input.len() {
+        for col in 0..input[row].len() {
+            if result[row][col] != 'O' {
+                continue;
+            }
+            let mut lift = 0;
+            for k in 1..=row {
+                if result[row - k][col] != '.' {
+                    break;
+                }
+                lift = k;
+            }
+            if lift > 0 {
+                result[row - lift][col] = 'O';
+                result[row][col] = '.'
+            }
+        }
+    }
+    result
+}
+
+fn _dump(input: &[Vec<char>]) {
+    for row in input {
+        for ch in row {
+            print!("{ch}");
+        }
+        println!();
+    }
+}
+
+fn total_load(input: &[Vec<char>]) -> usize {
+    let mut total = 0;
+    for col in 0..input[0].len() {
+        let len = input.len();
+        for row in 0..len {
+            if input[row][col] == 'O' {
+                total += len - row;
+            }
+        }
+    }
+    total
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -37,13 +88,35 @@ mod test {
     #[test]
     fn aoc2023_14_input_load_test() -> io::Result<()> {
         let sol = AoC2023_14::new()?;
+        assert!(!sol.input.is_empty());
         Ok(())
+    }
+
+    #[test]
+    fn aoc2023_14_ex1() {
+        let input = [
+            "O....#....",
+            "O.OO#....#",
+            ".....##...",
+            "OO.#O....O",
+            ".O.....O#.",
+            "O.#..O.#.#",
+            "..O..#O..O",
+            ".......O..",
+            "#....###..",
+            "#OO..#....",
+        ]
+        .iter()
+        .map(|s| s.to_string())
+        .collect::<Vec<_>>();
+        let puzzle = AoC2023_14::with_lines(&input);
+        assert_eq!(puzzle.part_one(), "136");
     }
 
     #[test]
     fn aoc2023_14_correctness() -> io::Result<()> {
         let sol = AoC2023_14::new()?;
-        assert_eq!(sol.part_one(), "");
+        assert_eq!(sol.part_one(), "109385");
         assert_eq!(sol.part_two(), "");
         Ok(())
     }
