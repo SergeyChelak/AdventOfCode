84071ba292d990b3aea9f70d6ab544e24807eefb
Sergey Chelak <sergey.chelak@gmail.com>
2023-12-14 11:57:37 +0200

    template for day 14

---
+++ src/aoc2023/day_14.rs
@@ -0,0 +1,50 @@
+use crate::solution::Solution;
+use crate::utils::*;
+
+use std::io;
+
+pub struct AoC2023_14 {
+    // place required fields here
+}
+
+impl AoC2023_14 {
+    pub fn new() -> io::Result<Self> {
+        let lines = read_file_as_lines("input/aoc2023_14")?;
+        Ok(Self::with_lines(&lines))
+    }
+
+    fn with_lines(lines: &[String]) -> Self {
+        todo!()
+    }
+}
+
+impl Solution for AoC2023_14 {
+    // fn part_one(&self) -> String {
+    // }
+
+    // fn part_two(&self) -> String {
+    // }
+
+    fn description(&self) -> String {
+        "AoC 2023/Day 14: Parabolic Reflector Dish".to_string()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn aoc2023_14_input_load_test() -> io::Result<()> {
+        let sol = AoC2023_14::new()?;
+        Ok(())
+    }
+
+    #[test]
+    fn aoc2023_14_correctness() -> io::Result<()> {
+        let sol = AoC2023_14::new()?;
+        assert_eq!(sol.part_one(), "");
+        assert_eq!(sol.part_two(), "");
+        Ok(())
+    }
+}
