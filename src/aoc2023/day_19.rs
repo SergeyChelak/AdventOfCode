2e5049332563b80f8512312e53b26113960857e8
Sergey Chelak <sergey.chelak@gmail.com>
2023-12-19 22:13:49 +0200

    code refactored

--- src/aoc2023/day_19.rs
+++ src/aoc2023/day_19.rs
@@ -23,99 +23,64 @@ impl From<&str> for Workflow {
     }
 }
 
-impl Workflow {
-    fn process(&self, part: &Part) -> &str {
-        for rule in &self.rules {
-            match rule {
-                Rule::Goto(next) => return next,
-                Rule::Compare(ch, cond, number, next) => {
-                    let value = part.value(*ch);
-                    let result = match cond {
-                        Condition::Greater => value > *number,
-                        Condition::Less => value < *number,
-                    };
-                    if result {
-                        return next;
-                    }
+enum Criteria {
+    Greater,
+    Less,
+}
+enum Rule {
+    Operation(Criteria, usize, Int, String),
+    Jump(String),
+}
+
+impl Rule {
+    fn exec(&self, part: &Part) -> Option<String> {
+        match self {
+            Rule::Jump(next) => Some(next.clone()),
+            Rule::Operation(criteria, idx, value, next) => {
+                let res = match criteria {
+                    Criteria::Greater => part[*idx] > *value,
+                    Criteria::Less => part[*idx] < *value,
+                };
+                if res {
+                    Some(next.clone())
+                } else {
+                    None
                 }
             }
         }
-        panic!("Unreachable")
     }
-}
-
-enum Rule {
-    Compare(char, Condition, Int, String),
-    Goto(String),
-}
-
-enum Condition {
-    Greater,
-    Less,
 }
 
 impl From<&str> for Rule {
     fn from(value: &str) -> Self {
         if let Some(idx) = value.find(':') {
             let chars = value.chars().collect::<Vec<_>>();
-            let condition = match chars[1] {
-                '>' => Condition::Greater,
-                '<' => Condition::Less,
-                ch => panic!("Unexpected condition {ch}"),
-            };
+            let operand = ch_to_idx(chars[0]);
             let number = value[2..idx]
                 .parse::<Int>()
                 .expect("Failed to parse value in the condition");
             let jump = value[idx + 1..].to_string();
-            Self::Compare(chars[0], condition, number, jump)
+            let criteria = match chars[1] {
+                '>' => Criteria::Greater,
+                '<' => Criteria::Less,
+                ch => panic!("Unexpected condition {ch}"),
+            };
+            Rule::Operation(criteria, operand, number, jump)
         } else {
-            Self::Goto(value.to_string())
+            Self::Jump(value.to_string())
         }
     }
 }
 
-#[derive(Debug)]
-struct Part {
-    x: Int,
-    m: Int,
-    a: Int,
-    s: Int,
-}
+type Part = [Int; 4];
 
-impl From<&str> for Part {
-    fn from(value: &str) -> Self {
-        let tokens = remove_first_and_last(value).split(',').collect::<Vec<_>>();
-        let (mut x, mut m, mut a, mut s) = (0, 0, 0, 0);
-        for token in tokens {
-            let (tag_name, tag_str_value) = token.split_once('=').expect("Incorrect part format");
-            let tag_value = tag_str_value
-                .parse::<Int>()
-                .expect("Part parameter value should be integer");
-            match tag_name {
-                "x" => x = tag_value,
-                "m" => m = tag_value,
-                "a" => a = tag_value,
-                "s" => s = tag_value,
-                _ => panic!("Unexpected tag name {tag_name}"),
-            }
-        }
-        Part { x, m, a, s }
-    }
-}
-
-impl Part {
-    fn rating(&self) -> Int {
-        self.x + self.m + self.a + self.s
-    }
-
-    fn value(&self, ch: char) -> Int {
-        match ch {
-            'x' => self.x,
-            'm' => self.m,
-            'a' => self.a,
-            's' => self.s,
-            _ => panic!("Unexpected char {ch} (1)"),
-        }
+fn ch_to_idx(ch: char) -> usize {
+    match ch {
+        'x' => 0,
+        'm' => 1,
+        'a' => 2,
+        's' => 3,
+        _ => panic!("Unexpected tag id {ch}"),
     }
 }
 
@@ -135,13 +100,30 @@ impl AoC2023_19 {
             .split_once("\n\n")
             .expect("Workflows should be separated with empty line");
 
-        let parts = parts.split_whitespace().map(Part::from).collect::<Vec<_>>();
+        let parts = parts
+            .split_whitespace()
+            .map(|x| Self::parse_part(x))
+            .collect::<Vec<_>>();
         let workflows = workflows
             .split_whitespace()
             .map(Workflow::from)
             .map(|elem| (elem.id.clone(), elem))
             .collect::<HashMap<_, _>>();
         Self { workflows, parts }
+    }
+
+    fn parse_part(value: &str) -> Part {
+        let tokens = remove_first_and_last(value).split(',').collect::<Vec<_>>();
+        let mut result = [0; 4];
+        for token in tokens {
+            let (tag_name, tag_str_value) = token.split_once('=').expect("Incorrect part format");
+            let tag_value = tag_str_value
+                .parse::<Int>()
+                .expect("Part parameter value should be integer");
+            let ch = tag_name.parse::<char>().expect("Incorrect tag name");
+            result[ch_to_idx(ch)] = tag_value;
+        }
+        result
     }
 }
 
@@ -152,21 +134,26 @@ impl Solution for AoC2023_19 {
             let mut id = "in".to_string();
             loop {
                 let workflow = self.workflows.get(&id).expect("workflow should be present");
-                match workflow.process(part) {
-                    "A" => {
-                        accepted.push(part);
+                for rule in &workflow.rules {
+                    if let Some(next) = rule.exec(part) {
+                        id = next;
                         break;
                     }
-                    "R" => {
-                        break;
-                    }
-                    next => {
-                        id = next.to_string();
-                    }
+                }
+                if id == "A" {
+                    accepted.push(part);
+                    break;
+                }
+                if id == "R" {
+                    break;
                 }
             }
         }
-        accepted.iter().map(|p| p.rating()).sum::<Int>().to_string()
+        accepted
+            .iter()
+            .map(|p| p.iter().sum::<Int>())
+            .sum::<Int>()
+            .to_string()
     }
 
     // fn part_two(&self) -> String {
