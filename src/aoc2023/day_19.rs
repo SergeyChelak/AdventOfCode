c730b8922650b0231ad56c71696d049a4ebb0cad
Sergey Chelak <sergey.chelak@gmail.com>
2023-12-19 12:56:57 +0200

    solved day 19 part 1

--- src/aoc2023/day_19.rs
+++ src/aoc2023/day_19.rs
@@ -4,6 +4,12 @@ use std::{collections::HashMap, io};
 
 type Int = i64;
 
+enum WorkflowResult {
+    Accept,
+    Reject,
+    Jump(String),
+}
+
 struct Workflow {
     id: String,
     rules: Vec<Rule>,
@@ -20,6 +26,35 @@ impl From<&str> for Workflow {
             .map(Rule::from)
             .collect::<Vec<_>>();
         Self { id, rules }
+    }
+}
+
+impl Workflow {
+    fn process(&self, part: &Part) -> WorkflowResult {
+        let goto_result = |s: &str| -> WorkflowResult {
+            match s {
+                "A" => WorkflowResult::Accept,
+                "R" => WorkflowResult::Reject,
+                _ => WorkflowResult::Jump(s.to_string()),
+            }
+        };
+
+        for rule in &self.rules {
+            match rule {
+                Rule::Goto(next) => return goto_result(&next),
+                Rule::Compare(ch, cond, number, next) => {
+                    let value = part.value(*ch);
+                    let result = match cond {
+                        Condition::Greater => value > *number,
+                        Condition::Less => value < *number,
+                    };
+                    if result {
+                        return goto_result(next);
+                    }
+                }
+            }
+        }
+        panic!("Unreachable")
     }
 }
 
@@ -53,6 +88,7 @@ impl From<&str> for Rule {
     }
 }
 
+#[derive(Debug)]
 struct Part {
     x: Int,
     m: Int,
@@ -78,6 +114,22 @@ impl From<&str> for Part {
             }
         }
         Part { x, m, a, s }
+    }
+}
+
+impl Part {
+    fn rating(&self) -> Int {
+        self.x + self.m + self.a + self.s
+    }
+
+    fn value(&self, ch: char) -> Int {
+        match ch {
+            'x' => self.x,
+            'm' => self.m,
+            'a' => self.a,
+            's' => self.s,
+            _ => panic!("Unexpected char {ch} (1)"),
+        }
     }
 }
 
@@ -108,8 +160,28 @@ impl AoC2023_19 {
 }
 
 impl Solution for AoC2023_19 {
-    // fn part_one(&self) -> String {
-    // }
+    fn part_one(&self) -> String {
+        let mut accepted: Vec<&Part> = Vec::new();
+        for part in &self.parts {
+            let mut id = "in".to_string();
+            loop {
+                let workflow = self.workflows.get(&id).expect("workflow should be present");
+                match workflow.process(part) {
+                    WorkflowResult::Accept => {
+                        accepted.push(part);
+                        break;
+                    }
+                    WorkflowResult::Reject => {
+                        break;
+                    }
+                    WorkflowResult::Jump(next) => {
+                        id = next;
+                    }
+                }
+            }
+        }
+        accepted.iter().map(|p| p.rating()).sum::<Int>().to_string()
+    }
 
     // fn part_two(&self) -> String {
     // }
@@ -132,9 +204,34 @@ mod test {
     }
 
     #[test]
+    fn aoc2023_19_ex1() {
+        let input = r#"
+px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}
+        "#;
+        let puzzle = AoC2023_19::with_str(input);
+        assert_eq!(puzzle.part_one(), "19114");
+    }
+
+    #[test]
     fn aoc2023_19_correctness() -> io::Result<()> {
         let sol = AoC2023_19::new()?;
-        assert_eq!(sol.part_one(), "");
+        assert_eq!(sol.part_one(), "446517");
         assert_eq!(sol.part_two(), "");
         Ok(())
     }
