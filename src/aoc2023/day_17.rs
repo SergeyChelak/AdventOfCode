b80cf7789a32f78578cfd409c60a59a572713a16
Sergey Chelak <sergey.chelak@gmail.com>
2023-12-19 11:11:28 +0200

    solved day 17 part 1

--- src/aoc2023/day_17.rs
+++ src/aoc2023/day_17.rs
@@ -1,9 +1,164 @@
 use crate::solution::Solution;
 use crate::utils::*;
 
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashMap};
 use std::io;
 
 type Int = u32;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
+struct Point {
+    row: usize,
+    col: usize,
+}
+
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    fn opposite(&self) -> Self {
+        use Direction::*;
+        match self {
+            Up => Down,
+            Down => Up,
+            Left => Right,
+            Right => Left,
+        }
+    }
+
+    fn is_opposite(&self, other: &Self) -> bool {
+        self.opposite() == *other
+    }
+}
+
+impl Point {
+    fn new(row: usize, col: usize) -> Self {
+        Self { row, col }
+    }
+}
+
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
+struct Node {
+    point: Point,
+    direction: Direction,
+    stripe: u8,
+}
+
+impl Node {
+    fn new(point: Point, direction: Direction, stripe: u8) -> Self {
+        Self {
+            point,
+            direction,
+            stripe,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
+struct QueueItem {
+    weight: Int,
+    node: Node,
+}
+
+impl Ord for QueueItem {
+    fn cmp(&self, other: &Self) -> Ordering {
+        // Notice that the we flip the ordering on costs.
+        // In case of a tie we compare positions - this step is necessary
+        // to make implementations of `PartialEq` and `Ord` consistent.
+        other
+            .weight
+            .cmp(&self.weight)
+            .then(self.node.cmp(&other.node))
+    }
+}
+
+// `PartialOrd` needs to be implemented as well.
+impl PartialOrd for QueueItem {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn valid_points(point: Point, rows: usize, cols: usize) -> HashMap<Direction, Point> {
+    let mut adj = HashMap::new();
+    let Point { row: r, col: c } = point;
+    if r > 0 {
+        adj.insert(Direction::Up, Point::new(r - 1, c));
+    }
+    if r < rows - 1 {
+        adj.insert(Direction::Down, Point::new(r + 1, c));
+    }
+    if c > 0 {
+        adj.insert(Direction::Left, Point::new(r, c - 1));
+    }
+    if c < cols - 1 {
+        adj.insert(Direction::Right, Point::new(r, c + 1));
+    }
+    adj
+}
+
+fn adjacent(map: &[Vec<Int>], node: Node) -> Vec<Node> {
+    let rows = map.len();
+    let cols = map[0].len();
+    let mut adj = Vec::new();
+    let stripe = node.stripe;
+    for (direction, point) in valid_points(node.point, rows, cols) {
+        if direction.is_opposite(&node.direction) {
+            continue;
+        }
+        if direction != node.direction {
+            adj.push(Node::new(point, direction, 1));
+        } else if stripe < 3 {
+            adj.push(Node::new(point, direction, 1 + stripe));
+        }
+    }
+    adj
+}
+
+fn dijkstra(map: &[Vec<Int>], start: Point, target: Point) -> Option<Int> {
+    // init
+    let mut weights: HashMap<Node, Int> = HashMap::new();
+    let mut queue: BinaryHeap<QueueItem> = BinaryHeap::new();
+    [
+        Node::new(start, Direction::Down, 0),
+        Node::new(start, Direction::Right, 0),
+    ]
+    .iter()
+    .for_each(|node| {
+        weights.insert(*node, 0);
+        queue.push(QueueItem {
+            node: *node,
+            weight: 0,
+        });
+    });
+    // main loop
+    while let Some(item) = queue.pop() {
+        let QueueItem { node, weight } = item;
+        if node.point == target {
+            return Some(weight);
+        }
+        for adj in adjacent(map, node) {
+            let new_weight = weight + map[adj.point.row][adj.point.col];
+            if let Some(old_weight) = weights.get(&adj) {
+                if new_weight >= *old_weight {
+                    continue;
+                }
+            }
+            weights.insert(adj, new_weight);
+            queue.push(QueueItem {
+                node: adj,
+                weight: new_weight,
+            });
+        }
+    }
+    None
+}
 
 pub struct AoC2023_17 {
     map: Vec<Vec<Int>>,
@@ -29,8 +184,14 @@ impl AoC2023_17 {
 }
 
 impl Solution for AoC2023_17 {
-    // fn part_one(&self) -> String {
-    // }
+    fn part_one(&self) -> String {
+        let start = Point::new(0, 0);
+        let target = Point::new(self.map.len() - 1, self.map[0].len() - 1);
+        let Some(val) = dijkstra(&self.map, start, target) else {
+            return "Not found".to_string();
+        };
+        val.to_string()
+    }
 
     // fn part_two(&self) -> String {
     // }
