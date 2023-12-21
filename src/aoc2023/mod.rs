06fb275350e6244b414f6f25277b60ff73c73476
Sergey Chelak <sergey.chelak@gmail.com>
2023-12-21 11:06:07 +0200

    template for day 21

--- src/aoc2023/mod.rs
+++ src/aoc2023/mod.rs
@@ -60,8 +60,8 @@ use day_19::*;
 mod day_20;
 use day_20::*;
 
-// mod day_21;
-// use day_21::*;
+mod day_21;
+use day_21::*;
 
 // mod day_22;
 // use day_22::*;
@@ -97,7 +97,7 @@ pub fn all_days() -> std::io::Result<Vec<Box<dyn Solution>>> {
         Box::new(AoC2023_18::new()?),
         Box::new(AoC2023_19::new()?),
         Box::new(AoC2023_20::new()?),
-        // Box::new(AoC2023_21::new()?),
+        Box::new(AoC2023_21::new()?),
         // Box::new(AoC2023_22::new()?),
         // Box::new(AoC2023_23::new()?),
         // Box::new(AoC2023_24::new()?),
@@ -106,5 +106,5 @@ pub fn all_days() -> std::io::Result<Vec<Box<dyn Solution>>> {
 }
 
 pub fn last_day() -> std::io::Result<Box<dyn Solution>> {
-    Ok(Box::new(AoC2023_20::new()?))
+    Ok(Box::new(AoC2023_21::new()?))
 }
