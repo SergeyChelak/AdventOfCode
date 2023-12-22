76416d4c6485626db208c1897401245906192dc0
Sergey Chelak <sergey.chelak@gmail.com>
2023-12-22 20:08:51 +0200

    parse input

--- src/aoc2023/day_22.rs
+++ src/aoc2023/day_22.rs
@@ -3,16 +3,59 @@ use crate::utils::*;
 
 use std::io;
 
+type Int = i32;
+
+#[derive(Debug, Clone)]
+struct Point3d {
+    x: Int,
+    y: Int,
+    z: Int,
+}
+
+impl Point3d {
+    fn from_csv(s: &str) -> Self {
+        let values = s
+            .split(',')
+            .map(|x| {
+                x.parse::<Int>()
+                    .expect("Integer value as coordinate is expected")
+            })
+            .collect::<Vec<_>>();
+        assert_eq!(values.len(), 3, "Incorrect number of coordinates");
+        Self {
+            x: values[0],
+            y: values[1],
+            z: values[2],
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Brick {
+    a: Point3d,
+    b: Point3d,
+}
+
 pub struct AoC2023_22 {
-    // place required fields here
+    bricks: Vec<Brick>,
 }
 
 impl AoC2023_22 {
     pub fn new() -> io::Result<Self> {
-        let _ = "input/aoc2023_22";
-        Ok(Self {
-            // initialize solution
-        })
+        let lines = read_file_as_lines("input/aoc2023_22")?;
+        Ok(Self::with_lines(&lines))
+    }
+
+    fn with_lines(lines: &[String]) -> Self {
+        let bricks = lines
+            .iter()
+            .map(|s| s.split_once('~').expect("Delimiter not found"))
+            .map(|(a, b)| Brick {
+                a: Point3d::from_csv(a),
+                b: Point3d::from_csv(b),
+            })
+            .collect::<Vec<_>>();
+        Self { bricks }
     }
 }
 
@@ -35,6 +78,7 @@ mod test {
     #[test]
     fn aoc2023_22_input_load_test() -> io::Result<()> {
         let sol = AoC2023_22::new()?;
+        assert!(!sol.bricks.is_empty());
         Ok(())
     }
 
