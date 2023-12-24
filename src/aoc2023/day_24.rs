d403acfa609e8907b42412cd74b107b1c8f85763
Sergey Chelak <sergey.chelak@gmail.com>
2023-12-24 13:31:00 +0200

    parse input

--- src/aoc2023/day_24.rs
+++ src/aoc2023/day_24.rs
@@ -3,8 +3,49 @@ use crate::utils::*;
 
 use std::io;
 
+type Int = i64;
+
+#[derive(Debug, Copy, Clone)]
+struct Vector3d {
+    x: Int,
+    y: Int,
+    z: Int,
+}
+
+impl From<&str> for Vector3d {
+    fn from(value: &str) -> Self {
+        let values = value
+            .split(", ")
+            .map(|s| s.parse::<Int>().expect("Numeric value is expected"))
+            .collect::<Vec<_>>();
+        assert!(value.len() > 2);
+        Self {
+            x: values[0],
+            y: values[1],
+            z: values[2],
+        }
+    }
+}
+
+struct Hailstone {
+    position: Vector3d,
+    velocity: Vector3d,
+}
+
+impl From<&str> for Hailstone {
+    fn from(value: &str) -> Self {
+        let (position, velocity) = value
+            .split_once(" @ ")
+            .expect("Failed to parse hailstone data");
+        Self {
+            position: Vector3d::from(position),
+            velocity: Vector3d::from(velocity),
+        }
+    }
+}
+
 pub struct AoC2023_24 {
-    // place required fields here
+    input: Vec<Hailstone>,
 }
 
 impl AoC2023_24 {
@@ -14,7 +55,11 @@ impl AoC2023_24 {
     }
 
     fn with_lines(lines: &[String]) -> Self {
-        todo!()
+        let input = lines
+            .iter()
+            .map(|s| Hailstone::from(s.as_str()))
+            .collect::<Vec<_>>();
+        Self { input }
     }
 }
 
@@ -37,6 +82,7 @@ mod test {
     #[test]
     fn aoc2023_24_input_load_test() -> io::Result<()> {
         let sol = AoC2023_24::new()?;
+        assert!(!sol.input.is_empty());
         Ok(())
     }
 
