d17908c95491939b82baea87c863cecbddd0822f
Sergey Chelak <sergey.chelak@gmail.com>
2024-08-13 15:24:46 +0300

    solved part 2

--- src/aoc2018/day_18.rs
+++ src/aoc2018/day_18.rs
@@ -1,9 +1,11 @@
 use crate::solution::Solution;
 use crate::utils::*;
 
+use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
 use std::io;
 
-#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Hash)]
 enum Acre {
     OpenGround,
     Trees,
@@ -33,18 +35,42 @@ impl AoC2018_18 {
         let input = parse_lines(&lines);
         Ok(Self { input })
     }
+
+    fn compute(&self, minutes: usize) -> usize {
+        let mut state = self.input.clone();
+        (0..minutes).for_each(|_| state = update(&state));
+        resource_value(&state)
+    }
 }
 
 impl Solution for AoC2018_18 {
     fn part_one(&self) -> String {
-        let mut state = self.input.clone();
-        (0..10).for_each(|_| state = update(&state));
-        let value = resource_value(&state);
-        value.to_string()
+        self.compute(10).to_string()
     }
 
-    // fn part_two(&self) -> String {
-    // }
+    fn part_two(&self) -> String {
+        let mut hashes = HashMap::<u64, usize>::new();
+        let minutes = 1_000_000_000;
+        let mut state = self.input.clone();
+        let mut values = Vec::new();
+        let mut offset = 0;
+        let mut period = 0;
+        for step in 0..minutes {
+            let hash = hash(&state);
+            if let Some(prev) = hashes.get(&hash) {
+                println!("found a loop at {}, prev step {}", step, prev);
+                period = step - prev;
+                offset = *prev;
+                break;
+            }
+            let value = resource_value(&state);
+            values.push(value);
+            hashes.insert(hash, step);
+            state = update(&state);
+        }
+        let pos = (minutes - offset) % period;
+        values[pos + offset].to_string()
+    }
 
     fn description(&self) -> String {
         "AoC 2018/Day 18: Settlers of The North Pole".to_string()
@@ -100,19 +126,19 @@ fn update(input: &Area) -> Area {
             if down && right {
                 adj.push(input[row + 1][col + 1])
             }
-            let (mut open, mut trees, mut lumberyard) = (0, 0, 0);
+            let (mut trees, mut lumberyard) = (0, 0);
             adj.iter().for_each(|x| match x {
                 Acre::Trees => trees += 1,
-                Acre::OpenGround => open += 1,
                 Acre::Lumberyard => lumberyard += 1,
+                _ => {}
             });
-            output[row][col] = next_acre(*value, open, trees, lumberyard);
+            output[row][col] = next_acre(*value, trees, lumberyard);
         }
     }
     output
 }
 
-fn next_acre(current: Acre, _open: usize, trees: usize, lumberyard: usize) -> Acre {
+fn next_acre(current: Acre, trees: usize, lumberyard: usize) -> Acre {
     match current {
         // An open acre will become filled with trees if three or more adjacent acres contained trees
         Acre::OpenGround if trees > 2 => Acre::Trees,
@@ -133,6 +159,12 @@ fn resource_value(input: &Area) -> usize {
 
 fn count(input: &Area, value: Acre) -> usize {
     input.iter().flatten().filter(|&x| *x == value).count()
+}
+
+fn hash(area: &Area) -> u64 {
+    let mut hasher = std::hash::DefaultHasher::new();
+    area.hash(&mut hasher);
+    hasher.finish()
 }
 
 #[cfg(test)]
@@ -175,7 +207,7 @@ mod test {
     fn aoc2018_18_correctness() -> io::Result<()> {
         let sol = AoC2018_18::new()?;
         assert_eq!(sol.part_one(), "480150");
-        assert_eq!(sol.part_two(), "");
+        assert_eq!(sol.part_two(), "233020");
         Ok(())
     }
 
