f705f9716eed479722ab878ae5df98cfb3f7a69c
Sergey Chelak <sergey.chelak@gmail.com>
2024-08-20 21:24:41 +0300

    simplified

--- src/aoc2018/day_22.rs
+++ src/aoc2018/day_22.rs
@@ -5,50 +5,28 @@ use std::io;
 
 type UInt = usize;
 type Coordinate = Point2d<UInt>;
+type CaveRegions = Vec<Vec<UInt>>;
 
-#[derive(Clone, Copy)]
-struct InputData {
+pub struct AoC2018_22 {
     depth: usize,
     target: Coordinate,
 }
 
-impl InputData {
-    fn new(depth: usize, x: UInt, y: UInt) -> Self {
+impl AoC2018_22 {
+    pub fn new() -> io::Result<Self> {
+        Ok(Self::with_parameters(9171, 7, 721))
+    }
+
+    fn with_parameters(depth: usize, x: UInt, y: UInt) -> Self {
         Self {
             depth,
             target: Coordinate::new(x, y),
         }
     }
-}
 
-type CaveRegions = Vec<Vec<UInt>>;
-
-struct Cave {
-    params: InputData,
-    regions: CaveRegions,
-}
-
-impl Cave {
-    fn new(params: InputData) -> Self {
-        Self {
-            params,
-            regions: Default::default(),
-        }
-    }
-
-    fn erosion_level(&self, val: UInt) -> UInt {
-        (val + self.params.depth) % 20183
-    }
-
-    fn calculate_geologic_index(&mut self) {
-        let (cols, rows) = {
-            let coord = self.params.target;
-            (coord.x + 1, coord.y + 1)
-        };
-        let (x, y) = {
-            let coord = self.params.target;
-            (coord.x, coord.y)
-        };
+    fn geologic_map(&self) -> CaveRegions {
+        let (cols, rows) = (self.target.x + 1, self.target.y + 1);
+        let (x, y) = (self.target.x, self.target.y);
         let mut regions = CaveRegions::with_capacity(rows);
         for row in 0..rows {
             let mut arr = Vec::with_capacity(cols);
@@ -73,12 +51,16 @@ impl Cave {
             }
             regions.push(arr);
         }
-        self.regions = regions
+        regions
     }
 
-    fn risk(&self) -> UInt {
+    fn erosion_level(&self, val: UInt) -> UInt {
+        (val + self.depth) % 20183
+    }
+
+    fn risk(&self, regions: &CaveRegions) -> UInt {
         let mut result = 0;
-        for row in self.regions.iter() {
+        for row in regions.iter() {
             for val in row {
                 let level = self.erosion_level(*val);
                 result += level % 3;
@@ -88,23 +70,10 @@ impl Cave {
     }
 }
 
-pub struct AoC2018_22 {
-    input: InputData,
-}
-
-impl AoC2018_22 {
-    pub fn new() -> io::Result<Self> {
-        Ok(Self {
-            input: InputData::new(9171, 7, 721),
-        })
-    }
-}
-
 impl Solution for AoC2018_22 {
     fn part_one(&self) -> String {
-        let mut cave = Cave::new(self.input);
-        cave.calculate_geologic_index();
-        cave.risk().to_string()
+        let map = self.geologic_map();
+        self.risk(&map).to_string()
     }
 
     // fn part_two(&self) -> String {
@@ -121,16 +90,14 @@ mod test {
 
     #[test]
     fn aoc2018_22_example1() {
-        let sol = AoC2018_22 {
-            input: InputData::new(510, 10, 10),
-        };
+        let sol = AoC2018_22::with_parameters(510, 10, 10);
         assert_eq!(sol.part_one(), "114")
     }
 
     #[test]
     fn aoc2018_22_correctness() -> io::Result<()> {
         let sol = AoC2018_22::new()?;
-        assert_eq!(sol.part_one(), "");
+        assert_eq!(sol.part_one(), "5786");
         assert_eq!(sol.part_two(), "");
         Ok(())
     }
