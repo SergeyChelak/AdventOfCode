ebaaf2e9448e34e598368c1c97b50f1d1cc82e2a
Sergey Chelak <sergey.chelak@gmail.com>
2024-12-03 09:53:00 +0000

    solved part 1

--- src/aoc2024/day_03.rs
+++ src/aoc2024/day_03.rs
@@ -1,31 +1,49 @@
-use crate::solution::Solution;
-use crate::utils::*;
+use regex::Regex;
 
+use crate::solution::Solution;
+
+use std::fs::read_to_string;
 use std::io;
 
 pub struct AoC2024_03 {
-    // place required fields here
+    input: String,
 }
 
 impl AoC2024_03 {
     pub fn new() -> io::Result<Self> {
-        let _ = "input/aoc2024_03";
-        Ok(Self {
-            // initialize solution
-        })
+        let input = read_to_string("input/aoc2024_03")?;
+        Ok(Self { input })
     }
 }
 
 impl Solution for AoC2024_03 {
-    // fn part_one(&self) -> String {
-    // }
+    fn part_one(&self) -> String {
+        let regex = Regex::new(r"mul\(\d*,\d*\)").expect("regex format should be valid");
+        let mut rest = self.input.as_str();
+        let mut sum = 0;
+        while let Some(reg_match) = regex.find(rest) {
+            let value = &rest[reg_match.start()..reg_match.end()];
+            sum += mul(value);
+            rest = &rest[reg_match.end()..];
+        }
+        sum.to_string()
+    }
 
     // fn part_two(&self) -> String {
     // }
 
     fn description(&self) -> String {
-        "".to_string()
+        "2024/Day 3: Mull It Over".to_string()
     }
+}
+
+fn mul(s: &str) -> i32 {
+    let (a, b) = s[4..s.len() - 1]
+        .split_once(',')
+        .expect("Invalid instruction {s}");
+    let a = a.parse::<i32>().expect("first argument isn't numeric");
+    let b = b.parse::<i32>().expect("second argument isn't numeric");
+    a * b
 }
 
 #[cfg(test)]
@@ -35,14 +53,24 @@ mod test {
     #[test]
     fn aoc2024_03_input_load_test() -> io::Result<()> {
         let sol = AoC2024_03::new()?;
+        assert!(!sol.input.is_empty());
         Ok(())
     }
 
     #[test]
     fn aoc2024_03_correctness() -> io::Result<()> {
         let sol = AoC2024_03::new()?;
-        assert_eq!(sol.part_one(), "");
+        assert_eq!(sol.part_one(), "171183089");
         assert_eq!(sol.part_two(), "");
         Ok(())
+    }
+
+    #[test]
+    fn aoc2024_03_case_1() {
+        let sol = AoC2024_03 {
+            input: r"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+                .to_string(),
+        };
+        assert_eq!(sol.part_one(), "161");
     }
 }
