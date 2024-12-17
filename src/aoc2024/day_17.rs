2994498b875fd6c84bd227f84b14821167e8226c
Sergey Chelak <sergey.chelak@gmail.com>
2024-12-17 22:14:06 +0200

    day 17, part 2

--- src/aoc2024/day_17.rs
+++ src/aoc2024/day_17.rs
@@ -1,11 +1,11 @@
 use crate::solution::Solution;
 
 use std::fs::read_to_string;
-use std::io;
+use std::{io, usize};
 
 type Int = usize;
 
-#[derive(Default)]
+#[derive(Default, Clone)]
 struct Machine {
     ra: Int,
     rb: Int,
@@ -54,7 +54,7 @@ impl Machine {
         // The denominator is found by raising 2 to the power of the instruction's combo operand.
         // (So, an operand of 2 would divide A by 4 (2^2); an operand of 5 would divide A by 2^B.)
         // The result of the division operation is truncated to an integer and then written to the A register.
-        self.ra /= 1 << self.combo_value(operand);
+        self.ra >>= self.combo_value(operand);
     }
 
     fn bxl(&mut self, operand: Int) {
@@ -92,12 +92,12 @@ impl Machine {
 
     fn bdv(&mut self, operand: Int) {
         // The bdv instruction (opcode 6) works exactly like the adv instruction except that the result is stored in the B register. (The numerator is still read from the A register.)
-        self.rb = self.ra / (1 << self.combo_value(operand));
+        self.rb = self.ra >> self.combo_value(operand);
     }
 
     fn cdv(&mut self, operand: Int) {
         // The cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored in the C register. (The numerator is still read from the A register.)
-        self.rc = self.ra / (1 << self.combo_value(operand));
+        self.rc = self.ra >> self.combo_value(operand);
     }
 
     fn formatted_output(&self) -> String {
@@ -166,12 +166,85 @@ impl Solution for AoC2024_17 {
         machine.formatted_output()
     }
 
-    // fn part_two(&self) -> String {
-    // }
+    fn part_two(&self) -> String {
+        let original = Machine::from(self.input.as_str());
+        let target = original.memory;
+
+        let mut backtrack = vec![0];
+        let mut results = Vec::new();
+
+        while let Some(mut acc) = backtrack.pop() {
+            let mut has_next = true;
+            let mut acc_next = acc;
+            while has_next {
+                has_next = false;
+                for num in 0..8 {
+                    acc_next = acc + num;
+                    let arr = function(acc_next);
+                    match validate(&arr, &target) {
+                        MatchResult::Equal => results.push(acc_next),
+                        MatchResult::Similar => backtrack.push(acc_next << 3),
+                        _ => {}
+                    };
+                }
+                acc = acc_next << 3;
+            }
+        }
+
+        results.iter().min().expect("Result not found").to_string()
+    }
 
     fn description(&self) -> String {
         "2024/Day 17: Chronospatial Computer".to_string()
     }
+}
+
+enum MatchResult {
+    Similar,
+    Equal,
+    Wrong,
+}
+
+fn validate(arr: &[usize], target: &[usize]) -> MatchResult {
+    if arr.len() > target.len() {
+        return MatchResult::Wrong;
+    }
+
+    let is_equals = arr
+        .iter()
+        .rev()
+        .zip(target.iter().rev())
+        .all(|(a, b)| a == b);
+
+    if !is_equals {
+        return MatchResult::Wrong;
+    }
+
+    if arr.len() == target.len() {
+        MatchResult::Equal
+    } else {
+        MatchResult::Similar
+    }
+}
+
+fn function(x: usize) -> Vec<usize> {
+    let mut ra = x;
+    let mut rb;
+    let mut rc;
+    let mut out = Vec::new();
+    loop {
+        rb = ra % 8;
+        rb ^= 1;
+        rc = ra >> rb;
+        rb ^= 5;
+        rb ^= rc;
+        out.push(rb % 8);
+        ra >>= 3;
+        if ra == 0 {
+            break;
+        }
+    }
+    out
 }
 
 #[cfg(test)]
@@ -246,8 +319,21 @@ Program: 0,1,5,4,3,0";
     #[test]
     fn aoc2024_17_correctness_part_2() -> io::Result<()> {
         let sol = make_solution()?;
-        assert_eq!(sol.part_two(), "");
+        assert_eq!(sol.part_two(), "164279024971453");
         Ok(())
+    }
+
+    #[test]
+    fn aoc2024_17_case_2() {
+        let input = "Register A: 2024
+Register B: 0
+Register C: 0
+
+Program: 0,3,5,4,3,0";
+        let puzzle = AoC2024_17 {
+            input: input.to_string(),
+        };
+        assert_eq!(puzzle.part_two(), "117440");
     }
 
     fn make_solution() -> io::Result<AoC2024_17> {
