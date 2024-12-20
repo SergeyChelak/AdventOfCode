278b922dab21a04f13b0bdcb5615b2c4cf3df959
Sergey Chelak <sergey.chelak@gmail.com>
2024-12-20 04:19:07 +0200

    day 19, part 2

--- src/aoc2024/day_19.rs
+++ src/aoc2024/day_19.rs
@@ -5,8 +5,8 @@ use std::fs::read_to_string;
 use std::io;
 
 pub struct AoC2024_19 {
-    pattern: Vec<String>,
-    design: Vec<String>,
+    patterns: Vec<String>,
+    designs: Vec<String>,
 }
 
 impl AoC2024_19 {
@@ -30,52 +30,55 @@ impl AoC2024_19 {
             .filter(|s| !s.is_empty())
             .collect::<Vec<_>>();
 
-        Self { pattern, design }
+        Self {
+            patterns: pattern,
+            designs: design,
+        }
     }
 }
 
 impl Solution for AoC2024_19 {
     fn part_one(&self) -> String {
-        let mut available = HashMap::new();
-        for pattern in &self.pattern {
-            available.insert(pattern.clone(), true);
-        }
-
         let mut total = 0usize;
-        for design in &self.design {
-            if is_possible(design, &mut available) {
+        let mut memo = HashMap::new();
+        for design in &self.designs {
+            if path_count(design, &self.patterns, &mut memo) > 0 {
                 total += 1;
             }
         }
-
         total.to_string()
     }
 
-    // fn part_two(&self) -> String {
-    // }
+    fn part_two(&self) -> String {
+        let mut total = 0usize;
+        let mut memo = HashMap::new();
+        for design in &self.designs {
+            total += path_count(design, &self.patterns, &mut memo);
+        }
+        total.to_string()
+    }
 
     fn description(&self) -> String {
         "Day 19: Linen Layout".to_string()
     }
 }
 
-fn is_possible(design: &str, available: &mut HashMap<String, bool>) -> bool {
-    if let Some(val) = available.get(design) {
+fn path_count(design: &str, patterns: &[String], memo: &mut HashMap<String, usize>) -> usize {
+    if design.is_empty() {
+        return 1;
+    }
+
+    if let Some(val) = memo.get(design) {
         return *val;
     }
 
-    for i in 1..design.len() {
-        let prefix = &design[..i];
-        let suffix = &design[i..];
-        // println!("p: {}, s: {}", prefix, suffix);
-        if is_possible(prefix, available) && is_possible(suffix, available) {
-            available.insert(design.to_string(), true);
-            return true;
-        }
+    let mut result = 0;
+    for pattern in patterns.iter().filter(|p| design.starts_with(*p)) {
+        result += path_count(&design[pattern.len()..], patterns, memo);
     }
 
-    available.insert(design.to_string(), false);
-    false
+    memo.insert(design.to_string(), result);
+    result
 }
 
 #[cfg(test)]
@@ -85,8 +88,9 @@ mod test {
     #[test]
     fn aoc2024_19_input_load_test() -> io::Result<()> {
         let sol = make_solution()?;
-        assert!(!sol.pattern.is_empty());
-        assert!(!sol.design.is_empty());
+        assert!(!sol.patterns.is_empty());
+        assert!(!sol.designs.is_empty());
+        assert_eq!(400, sol.designs.len());
         Ok(())
     }
 
@@ -100,7 +104,7 @@ mod test {
     #[test]
     fn aoc2024_19_correctness_part_2() -> io::Result<()> {
         let sol = make_solution()?;
-        assert_eq!(sol.part_two(), "");
+        assert_eq!(sol.part_two(), "623924810770264");
         Ok(())
     }
 
@@ -112,6 +116,12 @@ mod test {
     fn aoc2024_19_case_1() {
         let sol = make_test_solution();
         assert_eq!(sol.part_one(), "6");
+    }
+
+    #[test]
+    fn aoc2024_19_case_2() {
+        let sol = make_test_solution();
+        assert_eq!(sol.part_two(), "16");
     }
 
     fn make_test_solution() -> AoC2024_19 {
