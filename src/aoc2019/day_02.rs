a00c08d984a8934a371b75b6014baca4b4254e89
Sergey Chelak <sergey.chelak@gmail.com>
2025-04-10 00:14:25 +0300

    solved 2019 day 2, part 1

---
+++ src/aoc2019/day_02.rs
@@ -0,0 +1,102 @@
+use crate::solution::Solution;
+
+use std::fs::read_to_string;
+use std::io;
+
+type Int = usize;
+
+pub struct AoC2019_02 {
+    input: Vec<Int>,
+}
+
+impl AoC2019_02 {
+    pub fn new() -> io::Result<Self> {
+        let input = read_to_string("input/aoc2019_02")?;
+        Ok(Self::with_str(&input))
+    }
+
+    fn with_str(input: &str) -> Self {
+        let input = input
+            .trim()
+            .split(',')
+            .map(|s| s.parse::<Int>().expect("Invalid input"))
+            .collect::<Vec<_>>();
+        Self { input }
+    }
+}
+
+impl Solution for AoC2019_02 {
+    fn part_one(&self) -> String {
+        let mut data = self.input.clone();
+        data[1] = 12;
+        data[2] = 2;
+        execute(&mut data);
+        data[0].to_string()
+    }
+
+    // fn part_two(&self) -> String {
+    // }
+
+    fn description(&self) -> String {
+        "Day 2: 1202 Program Alarm".to_string()
+    }
+}
+
+fn execute(data: &mut [Int]) {
+    let size = data.len();
+    let mut index = 0usize;
+    while index + 4 < size {
+        let slice = &data[index..index + 4];
+        let opcode = slice[0];
+        let first_at = slice[1];
+        let first = data[first_at];
+        let second_at = slice[2];
+        let second = data[second_at];
+        let result_at = slice[3];
+        data[result_at] = match opcode {
+            1 => first + second,
+            2 => first * second,
+            99 => break,
+            x => panic!("Unexpected opcode {x}"),
+        };
+        index += 4;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn aoc2019_02_input_load_test() -> io::Result<()> {
+        let sol = make_solution()?;
+        assert!(!sol.input.is_empty());
+        Ok(())
+    }
+
+    #[test]
+    fn aoc2019_02_case_1() {
+        let puzzle = AoC2019_02::with_str("1,9,10,3,2,3,11,0,99,30,40,50");
+        let mut data = puzzle.input.clone();
+        execute(&mut data);
+        assert_eq!(data[0], 3500)
+    }
+
+    #[test]
+    fn aoc2019_02_correctness_part_1() -> io::Result<()> {
+        let sol = make_solution()?;
+        assert_eq!(sol.part_one(), "4690667");
+        Ok(())
+    }
+
+    #[test]
+    fn aoc2019_02_correctness_part_2() -> io::Result<()> {
+        let sol = make_solution()?;
+        assert_eq!(sol.part_two(), "");
+        Ok(())
+    }
+
+    fn make_solution() -> io::Result<AoC2019_02> {
+        AoC2019_02::new()
+    }
+}
--- src/aoc2019/mod.rs
+++ src/aoc2019/mod.rs
@@ -3,8 +3,8 @@ use super::solution::{PuzzleFactory, PuzzleFactoryMethod};
 mod day_01;
 use day_01::*;
 
-// mod day_02;
-// use day_02::*;
+mod day_02;
+use day_02::*;
 
 // mod day_03;
 // use day_03::*;
@@ -78,7 +78,7 @@ use day_01::*;
 pub fn puzzle_factory_2019() -> PuzzleFactory {
     let producers: Vec<&'static PuzzleFactoryMethod> = vec![
         &|| Ok(Box::new(AoC2019_01::new()?)),
-        // &|| Ok(Box::new(AoC2019_02::new()?)),
+        &|| Ok(Box::new(AoC2019_02::new()?)),
         // &|| Ok(Box::new(AoC2019_03::new()?)),
         // &|| Ok(Box::new(AoC2019_04::new()?)),
         // &|| Ok(Box::new(AoC2019_05::new()?)),
