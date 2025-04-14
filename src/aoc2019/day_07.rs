4a3bc4fb70bc2b547c12920e9f3e05e521be130c
Sergey Chelak <sergey.chelak@gmail.com>
2025-04-14 18:53:48 +0300

    2019 day 7 part 2

--- src/aoc2019/day_07.rs
+++ src/aoc2019/day_07.rs
@@ -5,7 +5,7 @@ use crate::{
 use std::fs::read_to_string;
 use std::io;
 
-use super::intcode_computer::{self, Int};
+use super::intcode_computer::{self, Int, InterruptReason};
 
 pub struct AoC2019_07 {
     input: intcode_computer::Memory,
@@ -22,19 +22,52 @@ impl AoC2019_07 {
         Self { input }
     }
 
-    fn amplifier_output(&self, input: Int, phase: Int) -> Int {
-        let mut computer = IntcodeComputer::with_memory(self.input.clone());
-        computer.push_input(input);
-        computer.push_input(phase);
-        computer.run();
-        computer.output()
+    fn thruster_output(&self, phases: &[Int]) -> Int {
+        let amplifier_output = |input: Int, phase: Int| -> Int {
+            let mut computer = IntcodeComputer::with_memory(self.input.clone());
+            computer.push_input(phase);
+            computer.push_input(input);
+            computer.run();
+            computer.output()
+        };
+        let mut value = 0;
+        for phase in phases {
+            value = amplifier_output(value, *phase);
+        }
+        value
     }
 
-    fn thruster_output(&self, initial_input: Int, phases: &[Int]) -> Int {
-        let mut value = initial_input;
-        for phase in phases {
-            let output = self.amplifier_output(value, *phase);
-            value = output;
+    fn thruster_feedback_output(&self, phases: &[Int]) -> Int {
+        let mut computers = phases
+            .iter()
+            .map(|phase| {
+                let mut computer = IntcodeComputer::with_memory(self.input.clone());
+                computer.push_input(*phase);
+                computer
+            })
+            .collect::<Vec<_>>();
+
+        let mut value = 0;
+        loop {
+            let mut halted = false;
+            for (i, _) in phases.iter().enumerate() {
+                let comp = computers.get_mut(i).expect("Computer not exists");
+                comp.push_input(value);
+                let reason = comp.run();
+                match reason {
+                    InterruptReason::Halted => {
+                        halted = true;
+                    }
+                    InterruptReason::WrongInstruction { code, pc } => {
+                        panic!("Comp #{i} issued wrong instruction {} at {}", code, pc);
+                    }
+                    InterruptReason::WaitForInput => {}
+                }
+                value = comp.output();
+            }
+            if halted {
+                break;
+            }
         }
         value
     }
@@ -43,14 +76,19 @@ impl AoC2019_07 {
 impl Solution for AoC2019_07 {
     fn part_one(&self) -> String {
         PermutationIterator::from_array(&[0, 1, 2, 3, 4])
-            .map(|arr| self.thruster_output(0, &arr))
+            .map(|arr| self.thruster_output(&arr))
             .max()
             .expect("Not found")
             .to_string()
     }
 
-    // fn part_two(&self) -> String {
-    // }
+    fn part_two(&self) -> String {
+        PermutationIterator::from_array(&[5, 6, 7, 8, 9])
+            .map(|arr| self.thruster_feedback_output(&arr))
+            .max()
+            .expect("Not found")
+            .to_string()
+    }
 
     fn description(&self) -> String {
         "Day 7: Amplification Circuit".to_string()
@@ -72,7 +110,7 @@ mod test {
     fn aoc2019_07_output_check_1() {
         let program = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0";
         let puzzle = AoC2019_07::with_str(program);
-        let out = puzzle.thruster_output(0, &[4, 3, 2, 1, 0]);
+        let out = puzzle.thruster_output(&[4, 3, 2, 1, 0]);
         assert_eq!(out, 43210)
     }
 
@@ -80,7 +118,7 @@ mod test {
     fn aoc2019_07_output_check_2() {
         let program = "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0";
         let puzzle = AoC2019_07::with_str(program);
-        let out = puzzle.thruster_output(0, &[0, 1, 2, 3, 4]);
+        let out = puzzle.thruster_output(&[0, 1, 2, 3, 4]);
         assert_eq!(out, 54321)
     }
 
@@ -88,8 +126,25 @@ mod test {
     fn aoc2019_07_output_check_3() {
         let program = "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0";
         let puzzle = AoC2019_07::with_str(program);
-        let out = puzzle.thruster_output(0, &[1, 0, 4, 3, 2]);
+        let out = puzzle.thruster_output(&[1, 0, 4, 3, 2]);
         assert_eq!(out, 65210)
+    }
+
+    #[test]
+    fn aoc2019_07_feedback_output_check_1() {
+        let program =
+            "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5";
+        let puzzle = AoC2019_07::with_str(program);
+        let out = puzzle.thruster_feedback_output(&[9, 8, 7, 6, 5]);
+        assert_eq!(out, 139629729)
+    }
+
+    #[test]
+    fn aoc2019_07_feedback_output_check_2() {
+        let program = "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10";
+        let puzzle = AoC2019_07::with_str(program);
+        let out = puzzle.thruster_feedback_output(&[9, 7, 8, 5, 6]);
+        assert_eq!(out, 18216)
     }
 
     #[test]
@@ -102,7 +157,7 @@ mod test {
     #[test]
     fn aoc2019_07_correctness_part_2() -> io::Result<()> {
         let sol = make_solution()?;
-        assert_eq!(sol.part_two(), "");
+        assert_eq!(sol.part_two(), "4215746");
         Ok(())
     }
 
--- src/aoc2019/intcode_computer.rs
+++ src/aoc2019/intcode_computer.rs
@@ -14,19 +14,21 @@ enum OpCode {
     Hlt,
 }
 
-impl From<Int> for OpCode {
-    fn from(value: Int) -> Self {
+impl TryFrom<Int> for OpCode {
+    type Error = String;
+
+    fn try_from(value: Int) -> Result<Self, Self::Error> {
         match value {
-            1 => OpCode::Add,
-            2 => OpCode::Mul,
-            3 => OpCode::Inp,
-            4 => OpCode::Out,
-            5 => OpCode::Jit,
-            6 => OpCode::Jif,
-            7 => OpCode::Lt,
-            8 => OpCode::Eq,
-            99 => OpCode::Hlt,
-            _ => panic!("Unexpected opcode {value}"),
+            1 => Ok(OpCode::Add),
+            2 => Ok(OpCode::Mul),
+            3 => Ok(OpCode::Inp),
+            4 => Ok(OpCode::Out),
+            5 => Ok(OpCode::Jit),
+            6 => Ok(OpCode::Jif),
+            7 => Ok(OpCode::Lt),
+            8 => Ok(OpCode::Eq),
+            99 => Ok(OpCode::Hlt),
+            _ => Err(format!("Unexpected opcode {value}")),
         }
     }
 }
@@ -48,6 +50,13 @@ impl From<Int> for Mode {
 }
 
 #[derive(Debug, Clone, Copy)]
+pub enum InterruptReason {
+    Halted,
+    WaitForInput,
+    WrongInstruction { code: Int, pc: usize },
+}
+
+#[derive(Debug, Clone, Copy)]
 struct Instruction {
     op_code: OpCode,
     mode_arg1: Mode,
@@ -55,11 +64,13 @@ struct Instruction {
     // mode_arg3: Mode,
 }
 
-impl From<Int> for Instruction {
-    fn from(value: Int) -> Self {
+impl TryFrom<Int> for Instruction {
+    type Error = String;
+
+    fn try_from(value: Int) -> Result<Self, Self::Error> {
         assert!(value >= 0);
         let mut code = value;
-        let op_code = OpCode::from(value % 100);
+        let op_code = OpCode::try_from(value % 100)?;
         code /= 100;
         let mut modes = Vec::with_capacity(3);
         for _ in 0..3 {
@@ -67,12 +78,12 @@ impl From<Int> for Instruction {
             modes.push(mode);
             code /= 10;
         }
-        Self {
+        Ok(Self {
             op_code,
             mode_arg1: modes[0],
             mode_arg2: modes[1],
             // mode_arg3: modes[2],
-        }
+        })
     }
 }
 
@@ -107,9 +118,16 @@ impl IntcodeComputer {
         self.output
     }
 
-    pub fn run(&mut self) {
+    pub fn run(&mut self) -> InterruptReason {
         loop {
-            let instr = Instruction::from(self.consume());
+            let instruction_pc = self.pc;
+            let code = self.consume();
+            let Ok(instr) = Instruction::try_from(code) else {
+                return InterruptReason::WrongInstruction {
+                    code,
+                    pc: instruction_pc,
+                };
+            };
             match instr.op_code {
                 OpCode::Add => {
                     let left = self.consume_read(instr.mode_arg1);
@@ -122,16 +140,21 @@ impl IntcodeComputer {
                     self.consume_write(left * right);
                 }
                 OpCode::Inp => {
+                    if self.input.is_empty() {
+                        // redo instruction
+                        self.pc = instruction_pc;
+                        return InterruptReason::WaitForInput;
+                    };
                     let val = self.consume();
                     assert!(val >= 0);
-                    self.memory[val as usize] = self.input.pop().expect("Input is empty");
+                    self.memory[val as usize] = self.input.remove(0)
                 }
                 OpCode::Out => {
                     let val = self.consume();
                     assert!(val >= 0);
                     self.output = self.memory[val as usize];
                 }
-                OpCode::Hlt => break,
+                OpCode::Hlt => return InterruptReason::Halted,
                 OpCode::Jit => {
                     let value = self.consume_read(instr.mode_arg1);
                     let addr = self.consume_read(instr.mode_arg2);
