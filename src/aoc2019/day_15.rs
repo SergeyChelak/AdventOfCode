e6aed05edf94446919176517e207bcd1bccea8e6
Sergey Chelak <sergey.chelak@gmail.com>
2025-04-22 14:44:41 +0300

    2019 day 15 part 1

---
+++ src/aoc2019/day_15.rs
@@ -0,0 +1,209 @@
+use crate::solution::Solution;
+use crate::utils::{Direction, Point2d};
+
+use std::collections::HashMap;
+use std::fs::read_to_string;
+use std::io;
+
+use super::intcode_computer::*;
+
+pub struct AoC2019_15 {
+    input: Memory,
+}
+
+impl AoC2019_15 {
+    pub fn new() -> io::Result<Self> {
+        let input = read_to_string("input/aoc2019_15")?;
+        Ok(Self::with_str(&input))
+    }
+
+    fn with_str(input: &str) -> Self {
+        Self {
+            input: parse_program(input),
+        }
+    }
+}
+
+impl Solution for AoC2019_15 {
+    fn part_one(&self) -> String {
+        let data = traverse_environment(&self.input);
+        let Some(distance) = data.oxygen_position.and_then(|p| data.distance_map.get(&p)) else {
+            return "Not found".to_string();
+        };
+        distance.to_string()
+    }
+
+    // fn part_two(&self) -> String {
+    // }
+
+    fn description(&self) -> String {
+        "Day 15: Oxygen System".to_string()
+    }
+}
+
+type Position = Point2d<isize>;
+
+impl Position {
+    fn move_by(&self, direction: &Direction) -> Self {
+        let x = self.x;
+        let y = self.y;
+        match direction {
+            Direction::Up => Position::new(x, y - 1),
+            Direction::Down => Position::new(x, y + 1),
+            Direction::Left => Position::new(x - 1, y),
+            Direction::Right => Position::new(x + 1, y),
+        }
+    }
+}
+
+const DIRECTION_NORTH: Int = 1;
+const DIRECTION_SOUTH: Int = 2;
+const DIRECTION_WEST: Int = 3;
+const DIRECTION_EAST: Int = 4;
+
+impl From<Direction> for Int {
+    fn from(value: Direction) -> Self {
+        match value {
+            Direction::Up => DIRECTION_NORTH,
+            Direction::Down => DIRECTION_SOUTH,
+            Direction::Left => DIRECTION_WEST,
+            Direction::Right => DIRECTION_EAST,
+        }
+    }
+}
+
+const TILE_UNDEFINED: Int = -1;
+const TILE_WALL: Int = 0;
+const TILE_FREE: Int = 1;
+const TILE_OXYGEN: Int = 2;
+
+type TileMap = HashMap<Position, Int>;
+type DistanceMap = HashMap<Position, Int>;
+
+struct Environment {
+    map: TileMap,
+    distance_map: DistanceMap,
+    oxygen_position: Option<Position>,
+}
+
+fn traverse_environment(input: &[Int]) -> Environment {
+    let mut computer = IntcodeComputer::with_size(10 * 1024);
+    computer.load_program(input);
+
+    let mut position = Position::new(0, 0);
+    let mut path: Vec<Direction> = Vec::new();
+
+    let mut oxygen_position: Option<Position> = None;
+
+    let mut map = TileMap::new();
+    let mut distance_map: HashMap<Position, Int> = HashMap::new();
+    map.insert(position, TILE_FREE);
+
+    loop {
+        let mut is_backtracking = false;
+        // has adjacent?
+        if let Some(dir) = Direction::all().iter().find(|dir| {
+            let p = position.move_by(dir);
+            let distance = distance_map.get(&p).unwrap_or(&Int::MAX);
+            *distance > path.len() as Int + 1
+        }) {
+            path.push(*dir);
+        } else if let Some(dir) = path.pop() {
+            // backtrack
+            path.push(dir.reverse());
+            is_backtracking = true;
+        }
+        let Some(dir) = path.pop() else {
+            break;
+        };
+        computer.push_input(dir.into());
+        let status = computer.run();
+        assert!(matches!(status, ExecutionStatus::WaitForInput));
+        let Some(output) = computer.pop_output() else {
+            panic!("Empty output")
+        };
+
+        let next = position.move_by(&dir);
+        map.insert(next, output);
+        if output != TILE_WALL {
+            position = next;
+            if !is_backtracking {
+                path.push(dir);
+            }
+        }
+        distance_map.insert(next, path.len() as Int);
+
+        if output == TILE_OXYGEN {
+            oxygen_position = Some(next)
+        }
+    }
+    Environment {
+        map,
+        distance_map,
+        oxygen_position,
+    }
+}
+
+fn dump(map: &TileMap, position: Position) {
+    let Some(min_x) = map.keys().map(|k| k.x).min() else {
+        return;
+    };
+    let Some(max_x) = map.keys().map(|k| k.x).max() else {
+        return;
+    };
+    let Some(min_y) = map.keys().map(|k| k.y).min() else {
+        return;
+    };
+    let Some(max_y) = map.keys().map(|k| k.y).max() else {
+        return;
+    };
+
+    for y in min_y..=max_y {
+        for x in min_x..=max_x {
+            let p = Position::new(x, y);
+            if p == position {
+                print!("D");
+                continue;
+            }
+            let val = map.get(&p).unwrap_or(&TILE_UNDEFINED);
+            let ch = match *val {
+                TILE_WALL => '#',
+                TILE_FREE => '.',
+                TILE_OXYGEN => 'O',
+                _ => ' ',
+            };
+            print!("{ch}")
+        }
+        println!()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn aoc2019_15_input_load_test() -> io::Result<()> {
+        let sol = make_solution()?;
+        assert!(!sol.input.is_empty());
+        Ok(())
+    }
+
+    #[test]
+    fn aoc2019_15_correctness_part_1() -> io::Result<()> {
+        let sol = make_solution()?;
+        assert_eq!(sol.part_one(), "222");
+        Ok(())
+    }
+
+    #[test]
+    fn aoc2019_15_correctness_part_2() -> io::Result<()> {
+        let sol = make_solution()?;
+        assert_eq!(sol.part_two(), "");
+        Ok(())
+    }
+
+    fn make_solution() -> io::Result<AoC2019_15> {
+        AoC2019_15::new()
+    }
+}
--- src/aoc2019/mod.rs
+++ src/aoc2019/mod.rs
@@ -44,8 +44,8 @@ use day_13::*;
 mod day_14;
 use day_14::*;
 
-// mod day_15;
-// use day_15::*;
+mod day_15;
+use day_15::*;
 
 // mod day_16;
 // use day_16::*;
@@ -93,7 +93,7 @@ pub fn puzzle_factory_2019() -> PuzzleFactory {
         &|| Ok(Box::new(AoC2019_12::new()?)),
         &|| Ok(Box::new(AoC2019_13::new()?)),
         &|| Ok(Box::new(AoC2019_14::new()?)),
-        // &|| Ok(Box::new(AoC2019_15::new()?)),
+        &|| Ok(Box::new(AoC2019_15::new()?)),
         // &|| Ok(Box::new(AoC2019_16::new()?)),
         // &|| Ok(Box::new(AoC2019_17::new()?)),
         // &|| Ok(Box::new(AoC2019_18::new()?)),
