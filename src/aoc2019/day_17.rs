d0489e9bfcf948e800805f8ed7efa0b0b539d232
Sergey Chelak <sergey.chelak@gmail.com>
2025-04-25 01:14:42 +0300

    slightly improved

--- src/aoc2019/day_17.rs
+++ src/aoc2019/day_17.rs
@@ -55,19 +55,17 @@ impl Solution for AoC2019_17 {
         let Some(direction) = map.get(&pos).map(|x| Direction::from(*x)) else {
             panic!("Failed to determine direction");
         };
-        let path = build_path(&map, pos, direction);
-        format_path(&path);
+        let path = {
+            let stack = build_path(&map, pos, direction);
+            extract_path(&stack)
+        };
         //
+        let input = build_movement_instructions(&path);
         let mut computer = IntcodeComputer::with_size(10 * 1024);
         let mut program = self.input.clone();
         program[0] = 2;
         computer.load_program(&program);
-
-        computer.push_input_str("A,B,B,C,B,C,B,C,A,A\n");
-        computer.push_input_str("L,6,R,8,L,4,R,8,L,12\n");
-        computer.push_input_str("L,12,R,10,L,4\n");
-        computer.push_input_str("L,12,L,6,L,4,L,4\n");
-        computer.push_input_str("n\n");
+        computer.push_input_str(&input);
 
         let status = computer.run();
         assert!(matches!(status, ExecutionStatus::Halted));
@@ -82,21 +80,67 @@ impl Solution for AoC2019_17 {
     }
 }
 
-type Point = Point2d<Int>;
-type ScaffoldMap = HashMap<Point, Int>;
+fn build_movement_instructions(input: &[String]) -> String {
+    let path = input.join(",");
 
-fn format_path(path: &[StackNode]) {
-    let arr = path
+    let mut dictionary = HashSet::<String>::new();
+
+    for from in 0..input.len() - 1 {
+        for len in 1..input.len() - from {
+            let pattern = input[from..from + len].join(",");
+            if dictionary.contains(&pattern) {
+                continue;
+            }
+            if !(3..=20).contains(&pattern.len()) {
+                continue;
+            }
+            let count = path.matches(&pattern).count();
+            if count > 1 {
+                dictionary.insert(pattern);
+            }
+        }
+    }
+
+    let patterns = dictionary.into_iter().collect::<Vec<_>>();
+    let allowed_set = [',', 'A', 'B', 'C'].into_iter().collect::<HashSet<_>>();
+    for (a, a_pattern) in patterns.iter().enumerate() {
+        let compressed = path.replace(a_pattern, "A");
+        for (b, b_pattern) in patterns.iter().enumerate().skip(a) {
+            let compressed = compressed.replace(b_pattern, "B");
+            for c_pattern in patterns.iter().skip(b) {
+                let compressed = compressed.replace(c_pattern, "C");
+                let is_valid = compressed.chars().all(|ch| allowed_set.contains(&ch));
+                if is_valid {
+                    return [
+                        compressed,
+                        a_pattern.clone(),
+                        b_pattern.clone(),
+                        c_pattern.clone(),
+                        "n\n".to_string(),
+                    ]
+                    .join("\n");
+                }
+            }
+        }
+    }
+
+    unreachable!()
+}
+
+fn extract_path(stack: &[StackNode]) -> Vec<String> {
+    stack
         .iter()
+        .skip(1)
         .map(|x| match x.path_element {
             PathElement::Direct => x.distance.to_string(),
             PathElement::Left => "L".to_string(),
             PathElement::Right => "R".to_string(),
         })
-        .collect::<Vec<_>>();
-    let s = arr.join(",");
-    println!("{s}");
+        .collect::<Vec<_>>()
 }
+
+type Point = Point2d<Int>;
+type ScaffoldMap = HashMap<Point, Int>;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum PathElement {
