d832a3937f7ad28470bc3a3f6ad5d09d3c4f95cd
Sergey Chelak <sergey.chelak@gmail.com>
2025-09-25 01:55:08 +0300

    refactored

--- src/aoc2020/day_12.rs
+++ src/aoc2020/day_12.rs
@@ -6,12 +6,52 @@ use std::io;
 type Int = isize;
 type Position = Point2d<Int>;
 
-struct Ship {
+enum Command {
+    North,
+    South,
+    East,
+    West,
+    Left,
+    Right,
+    Forward,
+}
+
+struct Instruction {
+    command: Command,
+    param: Int,
+}
+
+impl Instruction {
+    fn new(command: Command, param: Int) -> Self {
+        Self { command, param }
+    }
+}
+
+impl From<&str> for Instruction {
+    fn from(value: &str) -> Self {
+        let (cmd, param) = value.split_at(1);
+        let param = param
+            .parse::<Int>()
+            .expect("Invalid instruction format: {value}");
+        match cmd {
+            "N" => Self::new(Command::North, param),
+            "S" => Self::new(Command::South, param),
+            "E" => Self::new(Command::East, param),
+            "W" => Self::new(Command::West, param),
+            "L" => Self::new(Command::Left, param),
+            "R" => Self::new(Command::Right, param),
+            "F" => Self::new(Command::Forward, param),
+            _ => unreachable!(),
+        }
+    }
+}
+
+struct SimpleNavigation {
     dir: Direction,
     pos: Position,
 }
 
-impl Ship {
+impl SimpleNavigation {
     fn new() -> Self {
         Self {
             dir: Direction::Right,
@@ -19,18 +59,16 @@ impl Ship {
         }
     }
 
-    fn movement(&mut self, instr: &str) {
-        let (cmd, value) = instr.split_at(1);
-        let value = value.parse::<Int>().expect("Invalid instruction format");
-        match cmd {
-            "N" => self.pos.y -= value,
-            "S" => self.pos.y += value,
-            "E" => self.pos.x += value,
-            "W" => self.pos.x -= value,
-            "L" => self.turn_direction(Direction::turn_left, value),
-            "R" => self.turn_direction(Direction::turn_right, value),
-            "F" => self.move_position(value),
-            _ => unreachable!(),
+    fn movement(&mut self, instr: &Instruction) {
+        let value = instr.param;
+        match instr.command {
+            Command::North => self.pos.y -= value,
+            Command::South => self.pos.y += value,
+            Command::East => self.pos.x += value,
+            Command::West => self.pos.x -= value,
+            Command::Left => self.turn_direction(Direction::turn_left, value),
+            Command::Right => self.turn_direction(Direction::turn_right, value),
+            Command::Forward => self.move_in_direction(value),
         }
     }
 
@@ -41,7 +79,7 @@ impl Ship {
         }
     }
 
-    fn move_position(&mut self, value: Int) {
+    fn move_in_direction(&mut self, value: Int) {
         match self.dir {
             Direction::Up => self.pos.y -= value,
             Direction::Down => self.pos.y += value,
@@ -49,34 +87,46 @@ impl Ship {
             Direction::Right => self.pos.x += value,
         }
     }
+}
 
-    fn dist(&self) -> Int {
-        self.pos.x.abs() + self.pos.y
+impl Position {
+    fn manhattan_distance(&self) -> Int {
+        self.x.abs() + self.y.abs()
     }
 }
 
 pub struct AoC2020_12 {
-    input: Vec<String>,
+    input: Vec<Instruction>,
 }
 
 impl AoC2020_12 {
     pub fn new() -> io::Result<Self> {
         let lines = read_file_as_lines("input/aoc2020_12")?;
-        // Ok(Self::parse(&lines))
-        Ok(Self { input: lines })
+        Ok(Self::parse(&lines))
     }
 
-    // fn parse<T: AsRef<str>>(lines: &[T]) -> Self {
-    //     todo!()
-    // }
+    fn parse<T: AsRef<str>>(lines: &[T]) -> Self {
+        let input = lines
+            .iter()
+            .map(|x| x.as_ref())
+            .map(Instruction::from)
+            .collect();
+        Self { input }
+        //        wp (10, 1)  p (0, 0)
+        // f10        10, 1    100, 10
+        // n3         10, 4    100, 10
+        // f7         10, 4    170, 38
+        // r90        4, 10    170, 38
+        // f11        4, 10    214, 72
+    }
 }
 
 impl Solution for AoC2020_12 {
     fn part_one(&self) -> String {
-        let mut ship = Ship::new();
+        let mut ship = SimpleNavigation::new();
         self.input.iter().for_each(|instr| ship.movement(instr));
 
-        ship.dist().to_string()
+        ship.pos.manhattan_distance().to_string()
     }
 
     // fn part_two(&self) -> String {
