e4adce4d9b4440d66afc6806fb6a80e26d1d6b47
Sergey Chelak <sergey.chelak@gmail.com>
2025-10-03 21:33:01 +0300

    2020 day 20 part 2

--- src/aoc2020/day_20.rs
+++ src/aoc2020/day_20.rs
@@ -1,6 +1,6 @@
 use crate::{
     solution::Solution,
-    utils::{not_found, Vec2},
+    utils::{not_found, Diminishable, Point2d, Transformable2d, Vec2},
 };
 
 use std::{
@@ -14,20 +14,43 @@ struct TileRaw {
 }
 
 impl TileRaw {
-    fn edges(&self) -> Vec<TileEdge> {
-        let mut collection = HashSet::new();
-        let mut current = TileEdge::from(self);
-        for _ in 0..4 {
-            let next = current.rotated90();
-            let flip_ud = next.flipped_up_down();
-            let flip_lf = next.flipped_left_right();
-            collection.insert(next.clone());
-            collection.insert(flip_lf);
-            collection.insert(flip_ud);
+    fn edges(&self) -> HashMap<TileEdge, Vec<Transformation>> {
+        let mut collection = HashMap::new();
 
-            current = next;
+        let modify = |inp: &TileEdge,
+                      op: Transformation,
+                      store: &mut HashMap<TileEdge, Vec<Transformation>>|
+         -> TileEdge {
+            let val = match op {
+                Transformation::Rotate => inp.rotated90(),
+                Transformation::FlipVertical => inp.flipped_up_down(),
+            };
+
+            let mut arr = store.get(inp).cloned().unwrap_or(vec![]);
+            arr.push(op);
+            store.insert(val.clone(), arr);
+            val
+        };
+
+        let mut edges = TileEdge::from(self);
+        for _ in 0..4 {
+            edges = modify(&edges, Transformation::Rotate, &mut collection); //current.rotated90();
+            _ = modify(&edges, Transformation::FlipVertical, &mut collection); // next.flipped_up_down();
         }
-        collection.into_iter().collect()
+
+        {
+            for (k, v) in collection.iter() {
+                let x = TileRaw {
+                    id: 0,
+                    data: transforms(&self.data, v),
+                };
+                let e = TileEdge::from(&x);
+
+                assert_eq!(e, *k);
+            }
+        }
+
+        collection
     }
 
     fn col_chars(&self, col: usize) -> Vec<char> {
@@ -55,6 +78,12 @@ struct TileEdge {
     left: u16,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum Transformation {
+    Rotate,
+    FlipVertical,
+}
+
 impl TileEdge {
     fn rotated90(&self) -> Self {
         Self {
@@ -74,14 +103,14 @@ impl TileEdge {
         }
     }
 
-    fn flipped_left_right(&self) -> Self {
-        Self {
-            top: reverse_bits(self.top),
-            right: self.left,
-            bottom: reverse_bits(self.bottom),
-            left: self.right,
-        }
-    }
+    // fn flipped_left_right(&self) -> Self {
+    //     Self {
+    //         top: reverse_bits(self.top),
+    //         right: self.left,
+    //         bottom: reverse_bits(self.bottom),
+    //         left: self.right,
+    //     }
+    // }
 }
 
 impl From<&TileRaw> for TileEdge {
@@ -187,12 +216,12 @@ struct Disposition {
     edges: TileEdge,
 }
 
-struct Matrix {
+struct TileLayout {
     store: Vec2<Option<Disposition>>,
     dim: usize,
 }
 
-impl Matrix {
+impl TileLayout {
     fn new(dim: usize) -> Self {
         Self {
             store: vec![vec![None; dim]; dim],
@@ -214,6 +243,14 @@ impl Matrix {
     fn set(&mut self, pos: usize, value: Disposition) {
         let (row, col) = self.encode_pos(pos);
         self.store[row][col] = Some(value);
+    }
+
+    fn get(&self, pos: usize) -> Option<&Disposition> {
+        if self.at_end(pos) {
+            return None;
+        }
+        let (row, col) = self.encode_pos(pos);
+        self.store[row][col].as_ref()
     }
 
     fn remove(&mut self, pos: usize) {
@@ -271,15 +308,16 @@ impl Matrix {
     }
 }
 
+#[derive(Default)]
 struct TileDataStore {
     edges: HashMap<usize, Vec<TileEdge>>,
+    transformations: HashMap<(usize, TileEdge), Vec<Transformation>>,
+    data: HashMap<usize, Vec2<char>>,
 }
 
 impl TileDataStore {
     fn new() -> Self {
-        TileDataStore {
-            edges: HashMap::new(),
-        }
+        Default::default()
     }
 
     fn with(raw_tiles: &[TileRaw]) -> Self {
@@ -290,7 +328,15 @@ impl TileDataStore {
 
     fn fill_store(&mut self, raw_tiles: &[TileRaw]) {
         raw_tiles.iter().for_each(|raw| {
-            self.edges.insert(raw.id, raw.edges());
+            let id = raw.id;
+            self.data.insert(id, raw.data.clone());
+
+            let edges = raw.edges();
+            self.edges.insert(id, edges.keys().cloned().collect());
+
+            for (k, v) in edges.into_iter() {
+                self.transformations.insert((id, k), v);
+            }
         });
     }
 
@@ -313,15 +359,21 @@ impl TileDataStore {
         }
         result
     }
-}
 
-pub struct AoC2020_20 {
-    tiles: Vec<TileRaw>,
+    fn transformations(&self, disp: &Disposition) -> &[Transformation] {
+        self.transformations
+            .get(&(disp.tile_id, disp.edges.clone()))
+            .expect("transformations can't be None")
+    }
+
+    fn data(&self, id: usize) -> &Vec2<char> {
+        self.data.get(&id).expect("data can't be missed")
+    }
 }
 
 fn dfs(
     tile_store: &TileDataStore,
-    matrix: &mut Matrix,
+    matrix: &mut TileLayout,
     pos: usize,
     in_use: &mut HashSet<usize>,
 ) -> bool {
@@ -346,6 +398,10 @@ fn dfs(
     false
 }
 
+pub struct AoC2020_20 {
+    tiles: Vec<TileRaw>,
+}
+
 impl AoC2020_20 {
     pub fn new() -> io::Result<Self> {
         let input = std::fs::read_to_string("input/aoc2020_20")?;
@@ -360,31 +416,148 @@ impl AoC2020_20 {
             .collect::<Vec<_>>();
         Self { tiles }
     }
-}
 
-impl Solution for AoC2020_20 {
-    fn part_one(&self) -> String {
+    fn make_layout(&self) -> (TileLayout, TileDataStore) {
         let dim = self.tiles.len().isqrt();
         assert_eq!(dim * dim, self.tiles.len());
 
         let tile_store = TileDataStore::with(&self.tiles);
-        let mut matrix = Matrix::new(dim);
-        let is_ok = dfs(&tile_store, &mut matrix, 0, &mut HashSet::new());
+        let mut layout = TileLayout::new(dim);
+        let is_ok = dfs(&tile_store, &mut layout, 0, &mut HashSet::new());
 
         assert!(is_ok);
 
-        matrix
+        (layout, tile_store)
+    }
+}
+
+impl Solution for AoC2020_20 {
+    fn part_one(&self) -> String {
+        self.make_layout()
+            .0
             .product()
             .map(|val| val.to_string())
             .unwrap_or(not_found())
     }
 
-    // fn part_two(&self) -> String {
-    // }
+    fn part_two(&self) -> String {
+        let (layout, tile_store) = self.make_layout();
+        let dim = self.tiles.len().isqrt();
+        let (mut image, pixels) = assemble_image(layout, tile_store, dim);
+
+        let mut result = pixels;
+        let pattern = build_pattern();
+        let cols = 1 + pattern.iter().map(|p| p.x).max().unwrap_or(0);
+        let rows = 1 + pattern.iter().map(|p| p.y).max().unwrap_or(0);
+        for _ in 0..4 {
+            for _ in 0..2 {
+                image = transform(&image, Transformation::FlipVertical);
+                let val = check_pattern(&image, &pattern, rows, cols);
+                result = result.min(pixels - val);
+            }
+            image = transform(&image, Transformation::Rotate);
+        }
+        result.to_string()
+    }
 
     fn description(&self) -> String {
         "Day 20: Jurassic Jigsaw".to_string()
     }
+}
+
+fn assemble_image(
+    layout: TileLayout,
+    tile_store: TileDataStore,
+    dim: usize,
+) -> (Vec2<char>, usize) {
+    let reduced_dim = TILE_SIZE - 2;
+    let image_dim = dim * reduced_dim;
+    let mut image = vec![vec!['x'; image_dim]; image_dim];
+
+    let mut pixels = 0;
+    for r in 0..dim {
+        for c in 0..dim {
+            let pos = r * dim + c;
+            let disp = layout.get(pos).expect("Can't be none");
+            let trans = tile_store.transformations(disp);
+            let data = transforms(tile_store.data(disp.tile_id), trans)
+                .diminished(1)
+                .expect("Failed to diminish data");
+
+            for (i, row) in data.iter().enumerate() {
+                for (j, val) in row.iter().enumerate() {
+                    assert_eq!(image[reduced_dim * r + i][reduced_dim * c + j], 'x');
+                    image[reduced_dim * r + i][reduced_dim * c + j] = *val;
+                    if *val == '#' {
+                        pixels += 1;
+                    }
+                }
+            }
+        }
+    }
+    // debug validation
+    {
+        let valid_fill = image.iter().all(|row| row.iter().all(|ch| *ch != 'x'));
+        assert!(valid_fill);
+    }
+
+    (image, pixels)
+}
+
+fn transforms<T: Clone>(data: &Vec2<T>, transformations: &[Transformation]) -> Vec2<T> {
+    transformations
+        .iter()
+        .fold(data.clone(), |acc, t| transform(&acc, *t))
+}
+
+fn transform<T: Clone>(data: &Vec2<T>, t: Transformation) -> Vec2<T> {
+    match t {
+        Transformation::Rotate => data.transposed().flipped_horizontally(),
+        Transformation::FlipVertical => data.flipped_vertically(),
+    }
+}
+
+fn check_pattern(
+    image: &Vec2<char>,
+    pattern: &[Point2d<usize>],
+    rows: usize,
+    cols: usize,
+) -> usize {
+    let mut sum = 0;
+
+    for r in 0..image.len() - rows {
+        for c in 0..image[r].len() - cols {
+            let tmp = pattern
+                .iter()
+                .map(|p| Point2d::new(p.x + c, p.y + r))
+                .collect::<Vec<_>>();
+            let is_match = tmp.iter().all(|p| image[p.y][p.x] == '#');
+            if is_match {
+                sum += pattern.len();
+            }
+        }
+    }
+    sum
+}
+
+fn build_pattern() -> Vec<Point2d<usize>> {
+    " x                  #
+      x#    ##    ##    ###
+      x #  #  #  #  #  #
+    "
+    .split('\n')
+    .map(|s| s.trim())
+    .filter(|s| !s.is_empty())
+    .enumerate()
+    .flat_map(|(r, row)| {
+        row.chars()
+            .skip(1)
+            .enumerate()
+            .filter(|(_, ch)| *ch == '#')
+            .map(|(c, _)| Point2d::new(c, r))
+            .collect::<Vec<_>>()
+    })
+    .collect::<Vec<_>>()
 }
 
 #[cfg(test)]
@@ -408,7 +581,7 @@ mod test {
     #[test]
     fn aoc2020_20_correctness_part_2() -> io::Result<()> {
         let sol = make_solution()?;
-        assert_eq!(sol.part_two(), "");
+        assert_eq!(sol.part_two(), "1901");
         Ok(())
     }
 
@@ -444,8 +617,99 @@ mod test {
         let flipped_ud = original.flipped_up_down();
         assert_eq!(original, flipped_ud.flipped_up_down());
 
-        let flipped_lr = original.flipped_left_right();
-        assert_eq!(original, flipped_lr.flipped_left_right());
+        // let flipped_lr = original.flipped_left_right();
+        // assert_eq!(original, flipped_lr.flipped_left_right());
+    }
+
+    #[test]
+    fn aoc2020_20_case1() {
+        let sol = test_solution();
+        assert_eq!(sol.part_one(), "20899048083289")
+    }
+
+    #[test]
+    fn aoc2020_20_case2() {
+        let sol = test_solution();
+        assert_eq!(sol.part_two(), "273")
+    }
+
+    #[test]
+    fn aoc2020_20_transform_match() {
+        let original = raw_tile();
+
+        let rot = TileRaw {
+            id: 0,
+            data: transform(&original.data, Transformation::Rotate),
+        };
+
+        let orig_edges = TileEdge::from(&original);
+        let rot_edges = TileEdge::from(&rot);
+
+        assert_eq!(rot_edges, orig_edges.rotated90());
+    }
+
+    #[test]
+    fn aoc2020_20_build_pattern() {
+        let pattern = build_pattern();
+        assert_eq!(15, pattern.len());
+        assert!(pattern.contains(&Point2d::new(0usize, 18)));
+        assert!(pattern.contains(&Point2d::new(1usize, 0)));
+        assert!(pattern.contains(&Point2d::new(1usize, 18)));
+        assert!(pattern.contains(&Point2d::new(1usize, 19)));
+        assert!(pattern.contains(&Point2d::new(2usize, 1)));
+        assert!(pattern.contains(&Point2d::new(2usize, 16)));
+    }
+
+    #[test]
+    fn aoc2020_20_pattern_matching() {
+        let mut inp = ".#.#..#.##...#.##..#####
+        ###....#.#....#..#......
+        ##.##.###.#.#..######...
+        ###.#####...#.#####.#..#
+        ##.#....#.##.####...#.##
+        ...########.#....#####.#
+        ....#..#...##..#.#.###..
+        .####...#..#.....#......
+        #..#.##..#..###.#.##....
+        #.####..#.####.#.#.###..
+        ###.#.#...#.######.#..##
+        #.####....##..########.#
+        ##..##.#...#...#.#.#.#..
+        ...#..#..#.#.##..###.###
+        .#.#....#.##.#...###.##.
+        ###.#...#..#.##.######..
+        .#.#.###.##.##.#..#.##..
+        .####.###.#...###.#..#.#
+        ..#.#..#..#.#.#.####.###
+        #..####...#.#.#.###.###.
+        #####..#####...###....##
+        #.##..#..#...#..####...#
+        .#.###..##..##..####.##.
+        ...###...##...#...#..###"
+            .trim()
+            .split('\n')
+            .filter(|s| !s.is_empty())
+            .map(|s| s.trim().chars().collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+
+        inp = inp
+            .transposed()
+            .flipped_horizontally()
+            .flipped_horizontally();
+
+        for r in inp.iter() {
+            for c in r.iter() {
+                print!("{c}");
+            }
+            println!();
+        }
+
+        let pattern = build_pattern();
+        let cols = pattern.iter().map(|p| p.x).max().unwrap_or(0);
+        let rows = pattern.iter().map(|p| p.y).max().unwrap_or(0);
+
+        let count = check_pattern(&inp, &pattern, rows, cols);
+        assert_eq!(30, count)
     }
 
     fn raw_tile() -> TileRaw {
@@ -464,8 +728,7 @@ mod test {
         )
     }
 
-    #[test]
-    fn aoc2020_20_case1() {
+    fn test_solution() -> AoC2020_20 {
         let inp = "Tile 2311:
         ..##.#..#.
         ##..#.....
@@ -574,8 +837,6 @@ mod test {
         ..#.......
         ..#.###...
 ";
-        let sol = AoC2020_20::parse(inp);
-
-        assert_eq!(sol.part_one(), "20899048083289")
+        AoC2020_20::parse(inp)
     }
 }
