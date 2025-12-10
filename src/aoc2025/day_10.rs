b8498cd48fa7f8e7d387ef84df2f8039dd071642
Sergey Chelak <sergey.chelak@gmail.com>
2025-12-10 21:37:04 +0200

    performance improved for pt 1

--- src/aoc2025/day_10.rs
+++ src/aoc2025/day_10.rs
@@ -1,11 +1,11 @@
 use crate::solution::Solution;
 use crate::utils::*;
 
-use std::collections::VecDeque;
+use std::collections::{HashMap, VecDeque};
 use std::io;
 
 struct MachineConfiguration {
-    indicators: Vec<bool>,
+    indicators: usize,
     buttons: Vec<Vec<usize>>,
     joltage: Vec<usize>,
 }
@@ -34,7 +34,7 @@ impl Solution for AoC2025_10 {
     fn part_one(&self) -> String {
         self.input
             .iter()
-            .map(|x| indicator_setup_presses(&x.indicators, &x.buttons))
+            .filter_map(|x| indicator_setup_presses(x.indicators, &x.buttons))
             .sum::<usize>()
             .to_string()
     }
@@ -47,44 +47,35 @@ impl Solution for AoC2025_10 {
     }
 }
 
-struct State {
-    indicators: Vec<bool>,
-    taps: usize,
+fn bitmap_buttons(buttons: &[Vec<usize>]) -> Vec<usize> {
+    buttons.iter().map(|arr| bitmap(arr)).collect()
 }
 
-impl State {
-    fn with(len: usize) -> Self {
-        Self {
-            indicators: vec![false; len],
-            taps: 0,
-        }
-    }
-
-    fn tap(&self, wiring: &[usize]) -> State {
-        let mut indicators = self.indicators.clone();
-        for &idx in wiring {
-            indicators[idx] = !indicators[idx];
-        }
-        Self {
-            indicators,
-            taps: self.taps + 1,
-        }
-    }
+fn bitmap(arr: &[usize]) -> usize {
+    arr.iter().fold(0, |acc, x| acc | (1 << *x))
 }
 
-fn indicator_setup_presses(target: &[bool], buttons: &[Vec<usize>]) -> usize {
+fn indicator_setup_presses(target: usize, buttons: &[Vec<usize>]) -> Option<usize> {
+    let bitmaps = bitmap_buttons(buttons);
     let mut queue = VecDeque::new();
-    queue.push_back(State::with(target.len()));
+    queue.push_back(0usize);
+    let mut count_map = HashMap::<usize, usize>::new();
+    count_map.insert(0usize, 0);
     while let Some(state) = queue.pop_back() {
-        for button in buttons {
-            let next_state = state.tap(button);
-            if next_state.indicators == target {
-                return next_state.taps;
+        let count = 1 + count_map.get(&state).expect("Value must be preserved");
+        for bitmap in &bitmaps {
+            let next = state ^ *bitmap;
+            if count_map.contains_key(&next) {
+                continue;
             }
-            queue.push_front(next_state);
+            if next == target {
+                return Some(count);
+            }
+            count_map.insert(next, count);
+            queue.push_front(next);
         }
     }
-    unreachable!()
+    None
 }
 
 impl From<&str> for MachineConfiguration {
@@ -112,11 +103,12 @@ fn parse_csv(s: &str) -> Vec<usize> {
         .collect()
 }
 
-fn parse_indicators(s: &str) -> Vec<bool> {
+fn parse_indicators(s: &str) -> usize {
     let mut iter = s.chars();
     iter.next();
     iter.next_back();
-    iter.map(|ch| ch == '#').collect::<Vec<_>>()
+    iter.rev()
+        .fold(0usize, |acc, ch| acc << 1 | if ch == '#' { 1 } else { 0 })
 }
 
 #[cfg(test)]
@@ -145,16 +137,17 @@ mod test {
     }
 
     #[test]
-    fn aoc2025_10_parse_indicators_test() {
-        let arr = parse_indicators("[.##.]");
-        assert_eq!(arr, [false, true, true, false]);
+    fn aoc2025_10_bitmap_button_test() {
+        assert_eq!(0b1000, bitmap(&[3]));
+        assert_eq!(0b1010, bitmap(&[1, 3]));
+        assert_eq!(0b101, bitmap(&[0, 2]));
     }
 
     #[test]
     fn aoc2025_10_parse_machine_config() {
         let input = "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}";
         let config = MachineConfiguration::from(input);
-        assert_eq!(config.indicators, [false, true, true, false]);
+        assert_eq!(config.indicators, 0b110);
         assert_eq!(config.joltage, [3, 5, 4, 7]);
         assert_eq!(config.buttons.len(), 6);
         assert_eq!(config.buttons[0], [3]);
