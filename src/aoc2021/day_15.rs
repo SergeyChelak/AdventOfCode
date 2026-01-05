7894faf1315ddae267e703ae3a9985cd316684c4
Sergey Chelak <sergey.chelak@gmail.com>
2026-01-05 21:30:49 +0200

    2021 day 15 part 2

--- src/aoc2021/day_15.rs
+++ src/aoc2021/day_15.rs
@@ -37,45 +37,80 @@ impl Solution for AoC2021_15 {
         let rows = self.input.len();
         let cols = self.input[0].len();
 
-        let mut dp = HashMap::<Point, Int>::new();
-        let start = Point::zero();
-        dp.insert(start, 0);
-
-        let mut queue = VecDeque::new();
-        queue.push_front(start);
-
-        while let Some(point) = queue.pop_back() {
-            let weight = dp.get(&point).copied().unwrap();
-
-            for adj in Direction::all()
-                .iter()
-                .filter_map(|dir| point.safe_moved_by(dir))
-                .filter(|p| p.x < cols && p.y < rows)
-            {
-                let tmp = weight + self.input[adj.y][adj.x];
-                let is_better = if let Some(adj_weight) = dp.get(&adj) {
-                    *adj_weight > tmp
-                } else {
-                    true
-                };
-                if is_better {
-                    dp.insert(adj, tmp);
-                    queue.push_front(adj);
+        let dp = risk(
+            |p| {
+                if p.y < rows && p.x < cols {
+                    return Some(self.input[p.y][p.x]);
                 }
-            }
-        }
-
+                None
+            },
+            Point::zero(),
+        );
         dp.get(&Point::new(cols - 1, rows - 1))
             .map(|x| x.to_string())
             .unwrap_or(not_found())
     }
 
-    // fn part_two(&self) -> String {
-    // }
+    fn part_two(&self) -> String {
+        let tile_rows = self.input.len();
+        let tile_cols = self.input[0].len();
+
+        let rows = 5 * tile_rows;
+        let cols = 5 * tile_cols;
+
+        let dp = risk(
+            |p| {
+                if p.y >= rows || p.x >= cols {
+                    return None;
+                }
+                let times_row = (p.y / tile_rows) as Int;
+                let times_col = (p.x / tile_cols) as Int;
+
+                let mut value = self.input[p.y % tile_rows][p.x % tile_cols];
+                value = (value - 1 + times_col + times_row) % 9 + 1;
+                Some(value)
+            },
+            Point::zero(),
+        );
+        dp.get(&Point::new(cols - 1, rows - 1))
+            .map(|x| x.to_string())
+            .unwrap_or(not_found())
+    }
 
     fn description(&self) -> String {
         "Day 15: Chiton".to_string()
     }
+}
+
+fn risk(area: impl Fn(&Point) -> Option<Int>, start: Point) -> HashMap<Point, Int> {
+    let mut dp = HashMap::<Point, Int>::new();
+    dp.insert(start, 0);
+
+    let mut queue = VecDeque::new();
+    queue.push_front(start);
+    while let Some(point) = queue.pop_back() {
+        let weight = dp.get(&point).copied().unwrap();
+        for (adj_p, adj_w) in Direction::all()
+            .iter()
+            .filter_map(|dir| point.safe_moved_by(dir))
+            .filter_map(|p| {
+                let w = area(&p)?;
+                Some((p, w))
+            })
+        {
+            let tmp = weight + adj_w;
+            let is_better = if let Some(adj_weight) = dp.get(&adj_p) {
+                *adj_weight > tmp
+            } else {
+                true
+            };
+            if is_better {
+                dp.insert(adj_p, tmp);
+                queue.push_front(adj_p);
+            }
+        }
+    }
+    dp
 }
 
 #[cfg(test)]
@@ -99,11 +134,33 @@ mod test {
     #[test]
     fn aoc2021_15_correctness_part_2() -> io::Result<()> {
         let sol = make_solution()?;
-        assert_eq!(sol.part_two(), "");
+        assert_eq!(sol.part_two(), "2844");
         Ok(())
+    }
+
+    #[test]
+    fn aoc2021_15_case2() {
+        let sol = make_test_solution();
+        assert_eq!(sol.part_two(), "315");
     }
 
     fn make_solution() -> io::Result<AoC2021_15> {
         AoC2021_15::new()
+    }
+
+    fn make_test_solution() -> AoC2021_15 {
+        let lines = [
+            "1163751742",
+            "1381373672",
+            "2136511328",
+            "3694931569",
+            "7463417111",
+            "1319128137",
+            "1359912421",
+            "3125421639",
+            "1293138521",
+            "2311944581",
+        ];
+        AoC2021_15::parse_lines(&lines)
     }
 }
