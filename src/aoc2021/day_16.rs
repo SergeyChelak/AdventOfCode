59b5192559b9bebc0170a2143abcd658e1e2fd9e
Sergey Chelak <sergey.chelak@gmail.com>
2026-01-06 13:37:12 +0200

    2021 day 16 part 2

--- src/aoc2021/day_16.rs
+++ src/aoc2021/day_16.rs
@@ -1,4 +1,4 @@
-use crate::solution::Solution;
+use crate::{solution::Solution, utils::not_found};
 
 use std::io;
 
@@ -9,9 +9,13 @@ pub struct AoC2021_16 {
 impl AoC2021_16 {
     pub fn new() -> io::Result<Self> {
         let data = std::fs::read_to_string("input/aoc2021_16")?;
-        Ok(Self {
-            input: str_to_chars(&data),
-        })
+        Ok(Self::with_str(&data))
+    }
+
+    fn with_str(input: &str) -> Self {
+        Self {
+            input: str_to_chars(input),
+        }
     }
 }
 
@@ -21,10 +25,7 @@ impl Solution for AoC2021_16 {
 
         let mut stack = vec![Transmission::with_chars(&self.input)];
         while let Some(mut tx) = stack.pop() {
-            while tx.has_more() {
-                let Some(version) = tx.consume_version_checked() else {
-                    break;
-                };
+            while let Some(version) = tx.consume_version_checked() {
                 sum += version;
                 let id = tx.consume_type_id();
                 match id {
@@ -47,11 +48,114 @@ impl Solution for AoC2021_16 {
         sum.to_string()
     }
 
-    // fn part_two(&self) -> String {
-    // }
+    fn part_two(&self) -> String {
+        let bitmap = chars_to_bitmap(&self.input);
+        let result = interpret(bitmap);
+        assert_eq!(1, result.len());
+        result.first().map(|x| x.to_string()).unwrap_or(not_found())
+    }
 
     fn description(&self) -> String {
         "Day 16: Packet Decoder".to_string()
+    }
+}
+
+#[derive(Debug)]
+enum Operator {
+    Sum,
+    Product,
+    Min,
+    Max,
+    Greater,
+    Less,
+    Equal,
+}
+
+impl From<Int> for Operator {
+    fn from(value: Int) -> Self {
+        match value {
+            0 => Self::Sum,
+            1 => Self::Product,
+            2 => Self::Min,
+            3 => Self::Max,
+            5 => Self::Greater,
+            6 => Self::Less,
+            7 => Self::Equal,
+            _ => unreachable!(),
+        }
+    }
+}
+
+enum Token {
+    Literal(Int),
+    Operation(Operator, usize),
+}
+
+fn interpret(bitmap: Vec<Int>) -> Vec<Int> {
+    let mut tx = Transmission::with_bitmap(bitmap);
+    let mut tokens = Vec::<Token>::new();
+    while tx.consume_version_checked().is_some() {
+        let id = tx.consume_type_id();
+        if id == 4 {
+            let literal = tx.consume_literal();
+            tokens.push(Token::Literal(literal));
+            continue;
+        }
+
+        let operator = Operator::from(id);
+        let is_amount = tx.consume_bool();
+        if is_amount {
+            let count = tx.consume_sub_packets_count();
+            tokens.push(Token::Operation(operator, count));
+            continue;
+        }
+        let len = tx.consume_length();
+        let packet = tx.consume_packet(len);
+        let inner = interpret(packet);
+        tokens.push(Token::Operation(operator, inner.len()));
+        inner
+            .into_iter()
+            .rev()
+            .map(Token::Literal)
+            .for_each(|t| tokens.push(t));
+    }
+    eval_prefix(tokens)
+}
+
+fn eval_prefix(mut tokens: Vec<Token>) -> Vec<Int> {
+    let mut stack = Vec::<Int>::new();
+
+    while let Some(token) = tokens.pop() {
+        match token {
+            Token::Literal(val) => stack.push(val),
+            Token::Operation(operator, count) => {
+                let params = stack.split_off(stack.len() - count);
+                let value = eval_operator(&operator, &params);
+                stack.push(value)
+            }
+        }
+    }
+    stack
+}
+
+fn eval_operator(operator: &Operator, params: &[Int]) -> Int {
+    match operator {
+        Operator::Sum => params.iter().sum::<Int>(),
+        Operator::Product => params.iter().product::<Int>(),
+        Operator::Min => *params.iter().min().expect("(min) params are empty"),
+        Operator::Max => *params.iter().max().expect("(max) params are empty"),
+        Operator::Greater => {
+            assert_eq!(params.len(), 2);
+            (params[1] > params[0]) as Int
+        }
+        Operator::Less => {
+            assert_eq!(params.len(), 2);
+            (params[1] < params[0]) as Int
+        }
+        Operator::Equal => {
+            assert_eq!(params.len(), 2);
+            (params[1] == params[0]) as Int
+        }
     }
 }
 
@@ -64,10 +168,7 @@ struct Transmission {
 
 impl Transmission {
     fn with_chars(input: &[char]) -> Self {
-        let bitmap = input
-            .iter()
-            .flat_map(|ch| transform(*ch))
-            .collect::<Vec<_>>();
+        let bitmap = chars_to_bitmap(input);
         Self::with_bitmap(bitmap)
     }
 
@@ -99,7 +200,7 @@ impl Transmission {
         if !self.has_more() {
             return None;
         }
-        if self.bitmap.len() - self.ptr < 3 {
+        if self.bitmap.len() - self.ptr < 6 {
             return None;
         }
         Some(self.consume_version())
@@ -136,6 +237,13 @@ impl Transmission {
     fn consume_sub_packets_count(&mut self) -> Int {
         self.consume_digit(11)
     }
+}
+
+fn chars_to_bitmap(chars: &[char]) -> Vec<Int> {
+    chars
+        .iter()
+        .flat_map(|ch| transform(*ch))
+        .collect::<Vec<_>>()
 }
 
 fn transform(ch: char) -> [Int; 4] {
@@ -185,7 +293,7 @@ mod test {
     #[test]
     fn aoc2021_16_correctness_part_2() -> io::Result<()> {
         let sol = make_solution()?;
-        assert_eq!(sol.part_two(), "");
+        assert_eq!(sol.part_two(), "18234816469452");
         Ok(())
     }
 
@@ -247,6 +355,25 @@ mod test {
         _ = tx_1.consume_version();
         assert_eq!(tx_1.consume_type_id(), 4);
         assert_eq!(tx_1.consume_literal(), 20);
+    }
+
+    #[test]
+    fn aoc2021_16_cases_2() {
+        let cases = [
+            ("C200B40A82", 3),
+            ("04005AC33890", 54),
+            ("880086C3E88112", 7),
+            ("CE00C43D881120", 9),
+            // cmp
+            ("D8005AC2A8F0", 1),
+            ("F600BC2D8F", 0),
+            ("9C005AC2F8F0", 0),
+            ("9C0141080250320F1802104A08", 1),
+        ];
+        for (inp, out) in cases {
+            let sol = AoC2021_16::with_str(inp);
+            assert_eq!(out.to_string(), sol.part_two());
+        }
     }
 
     fn make_solution() -> io::Result<AoC2021_16> {
